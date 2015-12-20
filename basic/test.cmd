8fa4068f5af3c0734acfd3fd4ab9c48ce8d2517d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-12-20 16:46:43 -0500

    removed unneeded jar from classpath

--- basic/test.cmd
+++ basic/test.cmd
@@ -2,8 +2,11 @@
 REM works
 call mvn clean package install
 set TARGET=%CD%\target
-
-java -cp target\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-2.44.0.jar;target\lib\*  com.mycompany.app.App
+set MAIN_APP_CLASS=com.mycompany.app.App
+set SELENIUM_VERSION=2.44.0
+REM adding selenium-standalone to the CLASSPATH is not necessary.
+REM java -cp %TARGET%\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS% 
+java -cp target\app-1.1-SNAPSHOT.jar;target\lib\*  com.mycompany.app.App
 
 goto :EOF
 
--- table_entry/test.cmd
+++ table_entry/test.cmd
@@ -1,11 +1,11 @@
 @echo OFF
-echo building
+REM works
 call mvn clean package install
-echo testing
 set TARGET=%CD%\target
 set MAIN_APP_CLASS=com.mycompany.app.App
 set SELENIUM_VERSION=2.44.0
-java -cp %TARGET%\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS% 
+REM adding selenium-standalone to the CLASSPATH is not necessary.
+REM java -cp %TARGET%\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS%
+java -cp %TARGET%\app-1.1-SNAPSHOT.jar;%TARGET%\lib\*  %MAIN_APP_CLASS%
 
 goto :EOF
-
