186340704c2dc8db19803a1e5f457fb1aec78c15
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-12-22 19:54:23 -0500

    intermediate revision - updated unit tests

--- protractor/src/test/java/com/jprotractor/unit/NgByTest.java
+++ protractor/src/test/java/com/jprotractor/unit/NgByTest.java
@@ -28,6 +28,30 @@ public class NgByTest {
 	}
 
 	@Test
+	public void testButtonText() throws Exception {
+		By by = NgBy.buttonText("go!", driver);
+		assertThat(by, notNullValue());
+		assertThat(by.toString(),
+				equalTo("By.buttonText: go!"));
+	}
+
+	@Test
+	public void testRepeater() throws Exception {
+		By by = NgBy.repeater("cust in Customers", driver);
+		assertThat(by, notNullValue());
+		assertThat(by.toString(),
+				equalTo("By.repeater: cust in Customers"));
+	}
+
+
+	@Test
+	public void testOptions() throws Exception {
+	        By by = NgBy.options("option in options", driver);
+		assertThat(by, notNullValue());
+		assertThat(by.toString(),
+				equalTo("By.options: option in options"));
+	}
+
 	public void testBindingModel() throws Exception {
 		By by = NgBy.binding("person.address", driver);
 		assertThat(by, notNullValue());
