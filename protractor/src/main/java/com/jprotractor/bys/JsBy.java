d4ffcae5cce4764fd022d0c042b18adf4ed3386b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-12-26 00:13:04 -0500

    final revision - replacing Files.readAllBytes(Paths.get("file:/.../target/classes/angularscript.js") with  getResourceAsStream("angularscript.js")

--- protractor/src/main/java/com/jprotractor/bys/JsBy.java
+++ protractor/src/main/java/com/jprotractor/bys/JsBy.java
@@ -6,12 +6,6 @@ import java.io.InputStreamReader;
 
 import java.io.IOException;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.SearchContext;
@@ -33,20 +27,12 @@ public abstract class JsBy extends By {
 	}
 
 	protected String getScriptContent() {
-		String script = "";
 		try {
-				System.err.println(NgBy.class.getClassLoader().getResource(getFilename()).toURI());
 				InputStream is = NgBy.class.getClassLoader().getResourceAsStream(getFilename());
-				BufferedReader br = new BufferedReader(new InputStreamReader(is));
-				String line;
-				while ((line = br.readLine()) != null) {
-					script = script + line;
-				}
-				is.close();
-				return script;
+				byte[] bytes = new byte[is.available()];
+				is.read(bytes);
+				return new String(bytes, "UTF-8");
 		} catch ( IOException e) {
-			throw new RuntimeException(e);
-		} catch ( URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
 	}
