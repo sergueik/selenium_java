d96cd3cd577508838514c5e70ab062ed5426a703
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-02-20 10:46:59 -0500

    intermediate revision - allowed configuring versions of java, maven etc. from the environment

--- utils/setup.cmd
+++ utils/setup.cmd
@@ -1,10 +1,9 @@
 @echo OFF
-set GROOVY_VERSION=2.3.8
-set JAVA_VERSION=1.6.0_45
-set JAVA_VERSION=1.7.0_65
-set ANT_VERSION=1.9.3
-
-set MAVEN_VERSION=3.2.1
+REM allow override from the environment
+if "%GROOVY_VERSION%"==""  set GROOVY_VERSION=2.3.8
+if "%JAVA_VERSION%"=="" set JAVA_VERSION=1.7.0_65
+if "%ANT_VERSION%"=="" set ANT_VERSION=1.9.3
+if "%MAVEN_VERSION%"=="" set MAVEN_VERSION=3.2.1
 set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
 set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
 
@@ -14,7 +13,7 @@ set MAVEN_OPTS=-Xms256m -Xmx512m
 
 set ANT_HOME=c:\java\apache-ant-%ANT_VERSION%
 
-PATH=%JAVA_HOME%\bin;%PATH%;%ANT_HOME%\bin;%GROOVY_HOME%\bin;%M2%
+PATH=%JAVA_HOME%\bin;%PATH%;%ANT_HOME%\bin;%GROOVY_HOME%\bin;%M2%;c:\tools\phantomjs\bin
 
 REM http://www.tutorialspoint.com/maven/maven_environment_setup.htm
 REM http://stackoverflow.com/questions/5134953/noclassdeffounderror-in-java
