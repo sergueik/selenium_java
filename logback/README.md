c8fbe82063a9e128af630e892991dd7d947245b5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-05 13:19:54 -0500

    intermediate revision -refactoring

---
+++ logback/README.md
@@ -0,0 +1,3 @@
+Info
+----
+[Logback example](http://sergii-kartashov.blogspot.com/2012/12/loggining-into-database-with-help.html) 
--- logback/test.cmd
+++ logback/test.cmd
@@ -5,8 +5,8 @@ set TARGET=%CD%\target
 set SELENIUM_HOME=C:\JAVA\SELENIUM
 set SELENIUM_VERSION=2.47.1
 
+REM combining multiple logback configurations do not work, only the last entry matters
 java ^
--Dlogback.configurationFile=src/logback.console.xml ^
 -Dlogback.configurationFile=src/logback.mysql.xml ^
 -cp target\app-1.1-SNAPSHOT.jar;%SELENIUM_HOME%\selenium-server-standalone-%SELENIUM_VERSION%.jar;target\lib\* ^
 com.mycompany.app.App
@@ -21,4 +21,6 @@ REM log4j.properties
 REM log4j.xml
 REM http://stackoverflow.com/questions/17548997/maven-include-resources-into-jar
 REM http://stackoverflow.com/questions/4311026/how-to-get-slf4j-hello-world-working-with-log4j
-REM When a console warning gives you a URL to look at, and the URL says Knowing the appropriate location to place log4j.properties or log4j.xml requires understanding the search strategy of the class loader in use.
+REM When a console warning gives you a URL to look at, and the URL says Knowing the appropriate 
+REM location to place log4j.properties or log4j.xml requires 
+REM understanding the search strategy of the class loader in use.
