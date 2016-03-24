b2ecb66cd82eb3b1482bb21aa6556acf16b131b6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-23 20:21:58 -0400

    intermediate revision - refactoring the data providers

--- excel/src/main/java/com/mycompany/app/App.java
+++ excel/src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,9 @@
 package com.mycompany.app;
 // https://gist.github.com/madan712/3912272
 // https://poi.apache.org/apidocs/org/apache/poi/xssf/usermodel/XSSFWorkbook.html
+import java.util.ArrayList;
+import java.util.List;
+
 import java.io.File;
 import java.io.InputStream;
 import java.io.FileInputStream;
@@ -52,106 +55,123 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
-public class App
-{
-public static void main(String[] args)
-{
-	try
+public class App {
+  public static void main(String[] args) {
+    try	{
+      String filename = "TestConfiguration.xls";
+      InputStream ExcelFileToRead = new FileInputStream(filename );
+      HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
 
-	{
-		String filename = "TestConfiguration.xls";
-		InputStream ExcelFileToRead = new FileInputStream(filename );
-		HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
+      HSSFSheet sheet = wb.getSheetAt(0);
+      HSSFRow row;
+      HSSFCell cell;
 
-		HSSFSheet sheet = wb.getSheetAt(0);
-		HSSFRow row;
-		HSSFCell cell;
+      Iterator rows = sheet.rowIterator();
 
-		Iterator rows = sheet.rowIterator();
+      while (rows.hasNext()) {
+        row = (HSSFRow) rows.next();
+        Iterator cells = row.cellIterator();
+        while (cells.hasNext()) {
+          cell = (HSSFCell) cells.next();
+          if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
+            System.out.print(cell.getStringCellValue()+"\t");
+          } else if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
+            System.out.print(cell.getNumericCellValue()+"\t");
+          }
+          else
+          {
+            // TODO: Boolean, Formula, Errors
+          }
+        }
+        System.out.println();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-		while (rows.hasNext())
-		{
-			row = (HSSFRow) rows.next();
-			Iterator cells = row.cellIterator();
-
-			while (cells.hasNext())
-			{
-				cell = (HSSFCell) cells.next();
-
-				if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING)
-				{
-					System.out.print(cell.getStringCellValue()+"\t");
-				}
-				else if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC)
-				{
-					System.out.print(cell.getNumericCellValue()+"\t");
-				}
-				else
-				{
-					// TODO: Boolean, Formula, Errors
-				}
-			}
-			System.out.println();
-		}
-	}
-
-	catch (Exception e)
-
-	{
-
-		e.printStackTrace();
-
-	}
-
-
-	try
-
-	{
-		
-		String filename = "demo.xlsx"; 
-
-                XSSFWorkbook wb = new XSSFWorkbook(filename );
-		XSSFSheet sheet = wb.getSheetAt(0);
-		XSSFRow row;
-		XSSFCell cell;
-
-		Iterator rows = sheet.rowIterator();
-
-		while (rows.hasNext())
-		{
-			row = (XSSFRow) rows.next();
-			Iterator cells = row.cellIterator();
-
-			while (cells.hasNext())
-			{
-				cell = (XSSFCell) cells.next();
-
-				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING)
-				{
-					System.out.print(cell.getStringCellValue()+"\t");
-				}
-				else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC)
-				{
-					System.out.print(cell.getNumericCellValue()+"\t");
-				}
-				else
-				{
-					// TODO: Boolean, Formula, Errors
-				}
-			}
-			System.out.println();
-		}
-	}
-
-	catch (Exception e)
-
-	{
-
-		e.printStackTrace();
-
-	}
-
-
-
+	try {
+    String filename = "demo.xlsx"; 
+    XSSFWorkbook wb = new XSSFWorkbook(filename );
+    XSSFSheet sheet = wb.getSheetAt(0);
+    XSSFRow row;
+    XSSFCell cell;
+    Iterator rows = sheet.rowIterator();
+    while (rows.hasNext()) {
+      row = (XSSFRow) rows.next();
+      Iterator cells = row.cellIterator();
+      while (cells.hasNext()) {
+        cell = (XSSFCell) cells.next();
+        if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+          System.out.print(cell.getStringCellValue() + "(" + cell.getColumnIndex()  + ")\t" );
+        } else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+          System.out.print(cell.getNumericCellValue()+ "(" + cell.getColumnIndex()  + ")\t" );
+        } else {
+          // TODO: Boolean, Formula, Errors
+        }
+      }
+      System.out.println();
+    }
+  }  catch (Exception e)  {
+    e.printStackTrace();
+  }
+  parseXSSFWorkbook();
 }
+
+  // @DataProvider(parallel = false)
+  public static Object[][] parseXSSFWorkbook() {
+    
+    List<Object[]> data = new ArrayList<Object[]>();
+    Object[] dataRow = { };
+   
+    String filename = "demo.xlsx"; 
+    try{
+
+      XSSFWorkbook wb = new XSSFWorkbook(filename );
+      XSSFSheet sheet = wb.getSheetAt(0);
+      XSSFRow row;
+      XSSFCell cell;
+      String name = "";
+      double count = 0;
+
+      Iterator rows = sheet.rowIterator();  
+      // https://poi.apache.org/apidocs/org/apache/poi/xssf/usermodel/XSSFRow.html
+      while (rows.hasNext()) {
+        row = (XSSFRow) rows.next();
+        Iterator cells = row.cellIterator();
+        if (row.getRowNum() == 0){
+          continue;
+        }
+        // https://poi.apache.org/apidocs/org/apache/poi/ss/usermodel/Cell.html
+        while (cells.hasNext()) {
+          cell = (XSSFCell) cells.next();
+          if (cell.getColumnIndex() == 0) { 
+            if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+              count = cell.getNumericCellValue();
+            } else {
+              count = 0;
+            }
+          }
+          if (cell.getColumnIndex() == 1) { 
+            if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+              name = cell.getStringCellValue();
+            } else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC){
+              name = Double.toString(cell.getNumericCellValue());
+            } else {
+              // TODO: Boolean, Formula, Errors
+              name = "";
+            }
+          }
+        }
+        dataRow = new Object[]{name, count} ;
+        data.add(dataRow);
+        System.out.print(dataRow[1].toString() + "\t" +  dataRow[0].toString() + "\n");
+      }
+    }  catch (Exception e) {
+      e.printStackTrace();
+    }
+    Object[][] dataArray = new Object[ data.size() ][];
+    data.toArray( dataArray );
+    return dataArray;
+  }
+
 }
