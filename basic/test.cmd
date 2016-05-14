507674e2b6ab6d27f5cdaf5c0662466b8468c2fb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-13 22:26:43 -0400

    intermediate revision - refactored code, updated selenium driver standalone jar version

--- basic/test.cmd
+++ basic/test.cmd
@@ -1,14 +1,19 @@
 @echo OFF
-REM works
+
 call mvn clean package install
+set SELENIUM_HOME=c:\java\selenium
+
 set TARGET=%CD%\target
 set MAIN_APP_CLASS=com.mycompany.app.App
-set SELENIUM_VERSION=2.44.0
-REM adding selenium-standalone to the CLASSPATH is not necessary.
-REM java -cp %TARGET%\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS% 
-java -cp target\app-1.1-SNAPSHOT.jar;target\lib\*  com.mycompany.app.App
+set SELENIUM_VERSION=2.47.1
+set APP_VERSION=1.1-SNAPSHOT
 
-goto :EOF
+set LAUNCHER_OPTS=-XX:PermSize=512M -XX:MaxPermSize=512M -Xmn128M -Xms512M -Xmx512M
 
-REM http://selenium-suresh.blogspot.com/2013/09/selenium-webdriver-methods-with-examples.html
-REM https://groups.google.com/forum/#!topic/selenium-users/i_xKZpLfuTk
+java.exe %LAUNCHER_OPTS%  -cp ^
+%TARGET%\app-%APP_VERSION%.jar;^
+%SELENIUM_HOME%\selenium-server-standalone-%SELENIUM_VERSION%.jar;^
+%TARGET%\lib\* ^
+%MAIN_APP_CLASS%
+
+
