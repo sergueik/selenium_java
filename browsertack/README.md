95cfbfa446a77c50017b4800d322d64ff9a12188
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-07-09 23:08:24 -0400

    initial snapshot

---
+++ browsertack/README.md
@@ -0,0 +1,4 @@
+Info
+----
+
+origin: [testng-browserstack](https://github.com/nidhimj22/testng-browserstack)
---
+++ browsertack/src/test/java/com/mycompany/app/ParallelTest.java
@@ -0,0 +1,64 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ParallelTest {
+
+ 	private static final String USERNAME = "********";
+	private static final String ACCESS_KEY = "********";
+
+	private WebDriver driver;	
+
+	@BeforeClass
+	@org.testng.annotations.Parameters( value = { "browser", "version", "platform" })
+	public void setUp(String browser, String version, String platform) throws Exception {
+		DesiredCapabilities capability = new DesiredCapabilities();
+		capability.setCapability("platform",platform);
+		capability.setCapability("browserName", browser);
+		capability.setCapability("browserVersion", version);
+		capability.setCapability("project", "P1");
+		capability.setCapability("build", "1.0");
+		driver = new RemoteWebDriver(
+			new URL("http://USERNAME:ACCESS_KEY@hub.browserstack.com/wd/hub"),
+			capability);
+	}	
+
+	@Test
+	public void test() throws Exception {
+		driver.get("http://www.google.com");
+		System.out.println("Page title is: " + driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Browser Stack");
+		element.submit();
+		driver = new Augmenter().augment(driver);
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(srcFile, new File("Screenshot.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass	
+	public void tearDown() throws Exception {	
+		driver.quit();	
+	}
+}
