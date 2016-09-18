0642edacf96cad18df28438eb196d8627779ed0d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-09-18 16:19:16 -0400

    intermediate revision - added gradle environment

--- utils/setup.cmd
+++ utils/setup.cmd
@@ -1,22 +1,30 @@
 @echo OFF
 
 REM allow override from the environment
+if "%GRADLE_VERSION%"=="" set GRADLE_VERSION=3.0
+set GRADLE_HOME=c:\java\gradle-%GRADLE_VERSION%
+
 if "%ANT_VERSION%"=="" set ANT_VERSION=1.9.7
 set ANT_HOME=c:\java\apache-ant-%ANT_VERSION%
+
 if "%IVY_VERSION%"=="" set IVY_VERSION=2.4.0
 set IVY_HOME=c:\java\apache-ivy-%IVY_VERSION%
 REM cannot symlink
 REM copy %IVY_HOME%\ivy-%IVY_VERSION%.jar %ANT_HOME%\lib
+
 if "%GROOVY_VERSION%"==""  set GROOVY_VERSION=2.4.4
 set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
+
 if "%JAVA_VERSION%"=="" set JAVA_VERSION=1.7.0_79
+set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+set JAVA_OPTS=-Xms256m -Xmx512m
+
 if "%MAVEN_VERSION%"=="" set MAVEN_VERSION=3.3.9
 set M2_HOME=c:\java\apache-maven-%MAVEN_VERSION%
 set M2=%M2_HOME%\bin
-set MAVEN_OPTS=-Xms256m -Xmx512m
-set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+set MAVEN_OPTS=%JAVA_OPTS%
 
-PATH=%JAVA_HOME%\bin;%PATH%;%ANT_HOME%\bin;%GROOVY_HOME%\bin;%M2%
+PATH=%JAVA_HOME%\bin;%PATH%;%ANT_HOME%\bin;%GROOVY_HOME%\bin;%GRADLE_HOME%\bin;%M2%
 
 set PHANTOMJS_HOME=c:\tools\phantomjs
 PATH=%PATH%;%PHANTOMJS_HOME%\bin
