18c2576b17c497a9a0dce65fb2bb75d1c27f5e34
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-10-12 11:35:33 -0400

    intermediate revision -refactoring bash scripts

--- utils/hub.log4j.sh
+++ utils/hub.log4j.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
-# This is the standalone launcher script for hub it is listening to 4444
-export HUB_PORT=4444
-export HUB_IP_ADDRESS=127.0.0.1
-export SELENIUM_JAR_VERSION=2.47.1
+# Standalone Selenium hub launcher script The bub is listening to 4444
 
-# This code verifies that selenium jar of the correct version is linked to 
-# simply selenium-sever-standalone.jar
-/bin/readlink selenium-server-standalone.jar | grep -in $SELENIUM_JAR_VERSION
+HUB_PORT=4444
+HUB_IP_ADDRESS=127.0.0.1
+SELENIUM_VERSION=2.47.1
+LOG4J_VERSION=1.2.17
+# Verify that `selenium-sever-standalone.jar` points to the correct version of `selenium jar`.
+/bin/readlink selenium-server-standalone.jar | grep -in $SELENIUM_VERSION
 if [ $? != 0 ]
-then 
-echo "The Selenium version is incorrect: need version '$SELENIUM_JAR_VERSION'"
-ls -l selenium*jar
-exit 0
-fi
-# This code detects the already running instances. Only one selenium hub can run at a time
-#
-RUNNING_PID=$(sudo netstat -npl | grep $HUB_PORT | awk '{print $7}'| grep '/java'|head -1 | sed 's/\/.*$//')
-if [ "$RUNNING_PID" != "" ] ; then
-echo killing java process $RUNNING_PID
-ps -ocomm -oargs -p $RUNNING_PID
-# alternarive
-# sudo ps ax -opid,comm,args |  grep java |  grep $HUB_PORT 2>/dev/null| tail -1 | awk  '{print $1}' | xargs kill -HUP
-# sleep 1
-
-# sending HUP
-kill -HUP $RUNNING_PID
-# echo
+then
+  echo "The Selenium version is incorrect: need version '$SELENIUM_VERSION'"
+  ls -l selenium*jar
+  exit 0
 fi
 
-# This is options for java runtime.
-export LAUNCHER_OPTS='-XX:MaxPermSize=256M -Xmn128M -Xms256M -Xmx256M'
+# Only one selenium hub can run at a time
+# This code detects the already running instances.
+RUNNING_HUB_PID=$(sudo netstat -npl | grep $HUB_PORT | awk '{print $7}'| grep '/java'|head -1 | sed 's/\/.*$//')
+if [[ $RUNNING_HUB_PID ]]
+then
+  echo killing java process $RUNNING_HUB_PID
+  ps -ocomm -oargs -p $RUNNING_HUB_PID
+  # alternarive
+  # sudo ps ax -opid,comm,args |  grep java |  grep $HUB_PORT 2>/dev/null| tail -1 | awk  '{print $1}' | xargs kill -HUP
+  # sleep 1
+  # sending HUP
+  kill -HUP $RUNNING_HUB_PID
+  # echo
+fi
 
-java $LAUNCHER_OPTS -Dlog4j.configuration=/root/hub.log4j.properties -cp log4j-1.2.17.jar:selenium-server-standalone.jar:log4j.xml:log4j.properties org.openqa.grid.selenium.GridLauncher -role hub -host $HUB_IP_ADDRESS -port $HUB_PORT &
+# options for java runtime.
+LAUNCHER_OPTS='-XX:MaxPermSize=256M -Xmn128M -Xms256M -Xmx256M'
+
+SELENIUM_HUB_OPTS=$(
+cat <<EOF
+-Dlog4j.configuration=/root/hub.log4j.properties \
+-cp log4j-${LOG4J_VERSION}.jar:selenium-server-standalone.jar:log4j.xml:log4j.properties org.openqa.grid.selenium.GridLauncher \
+-role hub \
+-host ${HUB_IP_ADDRESS} \
+-port ${HUB_PORT}
+EOF
+)
+
+java $LAUNCHER_OPTS $SELENIUM_HUB_OPTS &
