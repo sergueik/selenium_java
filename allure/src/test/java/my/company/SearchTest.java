e743b8fba7be5d1eac8c41de8026f6ce790d7c0c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-10-14 22:48:21 -0400

    initial snapshot, merged with standard Selenim other dependencies

---
+++ allure/src/test/java/my/company/SearchTest.java
@@ -0,0 +1,28 @@
+package my.company;
+
+import my.company.steps.WebDriverSteps;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru Date: 24.11.13
+ */
+public class SearchTest {
+
+	private WebDriverSteps steps;
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		steps = new WebDriverSteps(new PhantomJSDriver(new DesiredCapabilities()));
+	}
+
+	@Test
+	public void searchTest() throws Exception {
+		steps.openMainPage();
+		steps.search("iSteer QATools");
+		steps.makeScreenshot();
+		steps.quit();
+	}
+}
