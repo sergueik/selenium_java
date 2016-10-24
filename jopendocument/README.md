d4d26c4de4f90dff24aad4a898037f9bc22c2973
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-10-24 13:24:21 -0400

    removed demo project, now merged in testng-dataproviders

--- jopendocument/README.md
+++
@@ -1,4 +0,0 @@
-About
-
-=====
-Example how to [open](http://half-wit4u.blogspot.com/2011/05/read-openoffice-spreadsheet-ods.html) OpenOffice SpreadSheet (ods)
--- jopendocument/test.cmd
+++
@@ -1,8 +0,0 @@
-@echo OFF
-REM works
-call mvn clean package install
-set TARGET=%CD%\target
-
-java -cp target\app-1.1-SNAPSHOT.jar;target\lib\*  com.mycompany.app.App
-
-goto :EOF
