34190dfbca9aa6fcb2d40d0195ef10e8ae1d9845
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-11-18 19:58:00 -0500

    intermediate revision - added skeleton tests for the rest of the site

--- IDE/utils/java/src/test/java/com/mycompany/app/AppTest.java
+++ IDE/utils/java/src/test/java/com/mycompany/app/AppTest.java
@@ -115,6 +115,11 @@ public class AppTest {
 		driver.get("about:blank");
 	}
 
+  // this test contains extra debugging in the ExpectedConditions anonimous class and filter
+  // this test is somewat redundant, used for debugging
+  // validate in Firebug
+  // $x("xpath to validate")
+  // $$("cssSelector to validate")
 	@Test(enabled = false)
 	public void Test0() {
 
@@ -123,29 +128,20 @@ public class AppTest {
     try{
       Thread.sleep(1000);
     } catch (InterruptedException e) {}
-    // Act
-
-    // $x("//div[@class='intro-message']/h3[contains(text(), 'Link Successfully clicked')]")
-    // $$(".container .row .intro-message h3 a")
 
 		String cssSelector = ".container .row .intro-message h3 a";
     String xpath = "//div[1]/div/div/div/div/h3[2]/a";
-		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By
-				.cssSelector(cssSelector))));
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(cssSelector))));
     List<WebElement> elements = driver.findElements(By.xpath(xpath));
 		assertTrue(elements.size() > 0);
 
-    // Assert
+    // Act
     String linkText = "Click Here";
     WebElement element = elements.get(0);
     highlight(element);
     assertTrue(element.getText().equalsIgnoreCase(linkText), element.getText());
-    // Act
     element.click();
 
-    try{
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {}
     linkText = "Link Successfully clicked";
     cssSelector = ".container .row .intro-message h3";
 		String className = "intro-message";
@@ -157,22 +153,23 @@ public class AppTest {
     try {
       wait.until(ExpectedConditions.urlContains("1.1link_validate.html"));
 		} catch (UnreachableBrowserException e) {
-			// TODO
 		}
 
+    // Assert
     try{
       (new WebDriverWait(driver, 5))
         .until(new ExpectedCondition<Boolean>() {
           public Boolean apply(WebDriver d) {
             WebElement e = d.findElement(By.className("intro-message")) ;
             String t = e.getText();
-              Boolean result = t.contains("Link Successfully clicked");
-              System.err.println("in apply: Text = " + t + "\n result = " + result.toString());
-              return result;
+            Boolean result = t.contains("Link Successfully clicked");
+            System.err.println("in apply: Text = " + t + "\n result = " + result.toString());
+            return result;
           }
         });
     } catch(Exception e) {
     }
+
     // http://stackoverflow.com/questions/12858972/how-can-i-ask-the-selenium-webdriver-to-wait-for-few-seconds-in-java
     // http://stackoverflow.com/questions/31102351/selenium-java-lambda-implementation-for-explicit-waits
     elements = driver.findElements(By.cssSelector(cssSelector));
@@ -184,12 +181,9 @@ public class AppTest {
 
 		assertThat(elements.size(), equalTo(1));
 
-    // Assert
     element = elements.get(0);
     highlight(element);
     assertTrue(element.getText().equalsIgnoreCase(linkText), element.getText());
-    // Act
-  	elements.forEach(System.out::println);			//output : spring node
 	}
 
 	@Test(enabled = true)
@@ -198,10 +192,9 @@ public class AppTest {
     // Arrange
     driver.get("http://suvian.in/selenium/1.1link.html");
 
-		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By
-				.cssSelector(".container .row .intro-message h3 a"))));
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
     List<WebElement> elements = driver.findElements(By.xpath("//div[1]/div/div/div/div/h3[2]/a"));
-		assertTrue(elements.size() > 0);
+    assertTrue(elements.size() > 0);
 
     // Act
     WebElement element = elements.get(0);
@@ -210,6 +203,11 @@ public class AppTest {
     element.click();
 
     // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.1link_validate.html"));
+    } catch (UnreachableBrowserException e) {
+    }
+    // Inspect enclosing element to confirm the page loaded
     try{
       (new WebDriverWait(driver, 5))
         .until(new ExpectedCondition<Boolean>() {
@@ -227,6 +225,500 @@ public class AppTest {
     highlight(element);
     assertTrue(element.getText().equalsIgnoreCase("Link Successfully clicked"), element.getText());
   	elements.forEach(System.out::println);			// output :  ??
+	}
+
+
+	@Test(enabled = false)
+	public void Test2() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.2text_field.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.2text_field_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test3() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.3age_plceholder.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.3age_plceholder_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test4() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.4gender_dropdown.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.4gender_dropdown_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test5() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.5married_radio.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.5married_radio_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test6() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.6checkbox.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.6checkbox_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test7() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.7button.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.7button_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test8() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.8copyAndPasteText.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.8copyAndPasteText_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test9() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.9copyAndPasteTextAdvanced.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.9copyAndPasteTextAdvanced_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test10() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/1.10selectElementFromDD.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("1.10selectElementFromDD_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test11() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.1alert.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.1alert_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test12() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.2browserPopUp.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.2browserPopUp_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test13() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.3frame.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.3frame_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test14() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.4mouseHover.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.4mouseHover_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test15() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.5resize.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.5resize_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test16() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.6liCount.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.6liCount_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test17() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.7waitUntil.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.7waitUntil_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test18() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.8progressBar.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.8progressBar_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test19() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.9greenColorBlock.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.9greenColorBlock_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test20() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/2.10dragAndDrop.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("2.10dragAndDrop_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test21() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.1fileupload.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.1fileupload_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test22() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.2dragAndDrop.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.2dragAndDrop_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test23() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.3javaemail.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.3javaemail_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test24() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.4readWriteExcel.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.4readWriteExcel_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test25() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.5cricketScorecard.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.5cricketScorecard_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test26() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.6copyTextFromTextField.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.6copyTextFromTextField_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test27() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.7correspondingRadio.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.7correspondingRadio_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test28() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.8screeshotToEmail.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.8screeshotToEmail_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test29() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.9FacebookTest.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.9FacebookTest_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
+	}
+
+	@Test(enabled = false)
+	public void Test30() {
+    // Arrange
+    driver.get("http://suvian.in/selenium/3.10select1stFriday.html");
+
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".container .row .intro-message h3 a"))));
+
+    // Act
+
+    // Wait page to load
+    try {
+      wait.until(ExpectedConditions.urlContains("3.10select1stFriday_validate.html"));
+      } catch (UnreachableBrowserException e) {
+      }
+    // Assert
 	}
 
 	private void highlight(WebElement element) {
