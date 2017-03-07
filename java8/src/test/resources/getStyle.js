b34a6606cfb1008ec7891560e256b1ebf83c5b5f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-03-07 18:40:20 -0500

    intermediate revision - added getText

--- java8/src/test/resources/getStyle.js
+++ java8/src/test/resources/getStyle.js
@@ -1,6 +1,13 @@
+/**
+ * Returns computed style of the element or some of its properties
+ *
+ * arguments[0] {Element} The event target.
+ * arguments[1] {String} The name of the property (e.g. 'width').
+ */
+
 // http://www.htmlgoodies.com/html5/css/referencing-css3-properties-using-javascript.html#fbid=88eQV8NzD6Q 
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-function getStyle(element, property) {
+getStyle = function(element, property) {
 	var result = '';
 	if (window.getComputedStyle) {
 		var styleObj = window.getComputedStyle(element, null);
@@ -21,10 +28,8 @@ function getStyle(element, property) {
 			result = element.currentStyle[property];
 		} catch (e) {}
 	}
-
 	return result;
 }
-
 
 var element = arguments[0];
 var property = arguments[1];
