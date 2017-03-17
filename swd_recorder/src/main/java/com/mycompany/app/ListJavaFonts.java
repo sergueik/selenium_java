9d09630082c4c10264081e95ca7d6fdab1f7d5f0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-03-17 00:14:09 -0400

    intermediate revision - added quick utility for font evaluation

---
+++ swd_recorder/src/main/java/com/mycompany/app/ListJavaFonts.java
@@ -0,0 +1,73 @@
+package com.mycompany.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Boolean.*;
+
+import org.eclipse.swt.SWT;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
+
+// based on: http://stackoverflow.com/questions/922052/testing-whether-a-font-is-monospaced-in-java
+public class ListJavaFonts {
+
+	public static void main(String[] args) {
+
+		List<String> monospaceFontFamilyNames = new ArrayList<String>();
+		GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment
+				.getLocalGraphicsEnvironment();
+		String[] fontFamilyNames = graphicsEnvironment
+				.getAvailableFontFamilyNames();
+
+		BufferedImage bufferedImage = new BufferedImage(1, 1,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics graphics = bufferedImage.createGraphics();
+
+		for (String fontFamilyName : fontFamilyNames) {
+			boolean isMonospaced = true;
+			//
+			int fontStyle = Font.PLAIN;
+			int fontSize = 12;
+			Font font = new Font(fontFamilyName, fontStyle, fontSize);
+			List<Integer> codePoints = new ArrayList<Integer>(Arrays.asList(
+					108 /* l */, 109 /* m */, 119 /* w */, 49 /* 1 */ , 52 /* 4 */));
+			FontMetrics fontMetrics = graphics.getFontMetrics(font);
+
+			int firstCharacterWidth = 0;
+			boolean hasFirstCharacterWidth = false;
+			for (int codePoint : codePoints) {
+				if (Character.isValidCodePoint(codePoint)
+						&& (Character.isLetter(codePoint)
+								|| Character.isDigit(codePoint))) {
+					char character = (char) codePoint;
+					int characterWidth = fontMetrics.charWidth(character);
+					if (hasFirstCharacterWidth) {
+						if (characterWidth != firstCharacterWidth) {
+							isMonospaced = false;
+							break;
+						}
+					} else {
+						firstCharacterWidth = characterWidth;
+						hasFirstCharacterWidth = true;
+					}
+				}
+			}
+
+			if (isMonospaced) {
+				monospaceFontFamilyNames.add(fontFamilyName);
+			}
+		}
+
+		graphics.dispose();
+		for (String fontFamily : monospaceFontFamilyNames) {
+			System.out.println(fontFamily);
+		}
+	}
+
+}
