06d1d248abc104356d82c2ab14c0a465cd3a2bc2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-05-08 00:07:48 -0400

    intermediate revision - added URL probing in wget, curl, powershell / Invoke-WebRequest

--- utils/selenium_setup.cmd
+++ utils/selenium_setup.cmd
@@ -1,38 +1,48 @@
 @echo OFF
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-rem origin: https://raw.githubusercontent.com/BecauseQA/becauseQA-selenium/master/Selenium-Server-Standalone.bat
-rem uses
-rem wget.exe https://eternallybored.org/misc/wget/
-rem xml.exe http://xmlstar.sourceforge.net/download.php
-rem 7z.exe http://www.7-zip.org/
-rem jq.exe https://stedolan.github.io/jq/
+REM origin: https://raw.githubusercontent.com/BecauseQA/becauseQA-selenium/master/Selenium-Server-Standalone.bat
+REM origin http://forum.oszone.net/thread-326552.html
 
-rem define tool aliases
+REM uses
+REM wget.exe https://eternallybored.org/misc/wget/
+REM xml.exe http://xmlstar.sourceforge.net/download.php
+REM 7z.exe http://www.7-zip.org/
+REM jq.exe https://stedolan.github.io/jq/
+
+REM define tool aliases
 if defined PROGRAMFILES(X86) (
     set OS=64BIT
     set JQ="C:\TOOLS\jq-win64.exe"
+    REM set WGET="C:\Program Files (x86)\GnuWin32\bin\wget.exe"
     set WGET="C:\TOOLS\wget.exe"
+    REM set CURL="C:\Program Files (x86)\curl\curl.exe"
+    REM set CURL="C:\TOOLS\curl.exe"
     set XMLSTARLET="C:\TOOLS\XMLSTARLET-1.6.1\xml.exe"
 ) else (
     set OS=32BIT
+    set JQ="C:\TOOLS\jq-win32.exe"
+    REM set WGET="C:\Program Files\GnuWin32\bin\wget.exe"
     set WGET="C:\TOOLS\wget.exe"
+    REM set CURL="C:\Program Files\curl\curl.exe"
+    REM set CURL="C:\TOOLS\curl.exe"
     set XMLSTARLET="C:\TOOLS\XMLSTARLET-1.6.1\xml.exe"
 )
-rem alternatively may add to the PATH environment
+REM Alternatively may add to the PATH environment
 set PATH=%PATH%;C:\TOOLS
 set PATH=%PATH%;C:\TOOLS\XMLSTARLET-1.6.1
 set PATH=%PATH%;C:\PROGRAM FILES\7-ZIP
+REM set PATH=%PATH%;C:\C:\Program Files (x86)\curl
+REM set PATH=%PATH%;C:\Program Files (x86)\GnuWin32\bin
 
-rem stop Selenium hub or standalone server
+REM Stop Selenium hub or standalone server
 for /F "tokens=5 delims= " %%_ IN ('netstat -a -n -o ^| findstr :4444') do taskkill.exe /F /PID %%_
 
-
-rem the old versions of Chrome are available on http://www.slimjetbrowser.com/chrome/win/
-rem the currently installed Chrome version can be found from several registry locations
-rem reg.exe query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Google\Update\Clients\{8A69D345-D564-463c-AFF1-A69D9E530F96}" -v pv
-rem reg.exe query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe\"
-rem dir /od /ad /b "C:\Program Files (x86)\Google\Chrome\Application"
+REM the old versions of Chrome are available on http://www.slimjetbrowser.com/chrome/win/
+REM the currently installed Chrome version can be found from several registry locations
+REM reg.exe query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Google\Update\Clients\{8A69D345-D564-463c-AFF1-A69D9E530F96}" -v pv
+REM reg.exe query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe\"
+REM dir /od /ad /b "C:\Program Files (x86)\Google\Chrome\Application"
 
 :CHROMEDRIVER
 set CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com
@@ -52,14 +62,18 @@ set RENAMED_CHROMEDRIVER_NAME=%CHROMEDRIVER_PROCESS_NAME%-%BUILD%.exe
 
 if not exist "%BUILD%" (mkdir %BUILD% )
 if not exist "%CHROMEDRIVER_PACKAGE:"=%" (
+  REM chromeDriver does not allow HEAD
+  REM %CURL% --output NUL --silent --head --fail --url %CHROMEDRIVER_WIN32_URL% >NUL 2>&1 && echo %CHROMEDRIVER_WIN32_URL% is present
+  REM %WGET% --spider --no-dns-cache --no-cache --quiet %CHROMEDRIVER_WIN32_URL% >NUL 2>&1 && echo %CHROMEDRIVER_WIN32_URL% is present
+  REM powershell.exe -command "& {Param([string]$url) write-output ('url = {0}' -f $url); try { $o = Invoke-WebRequest -Uri $url -Method Head -UseBasicParsing -ErrorAction SilentlyContinue; echo $o.StatusCode; $status = ( $o.StatusCode -eq 200 ); $exitcode  =  [int]( -not ($status)); write-output ('will exit with {0}' -f ${exitcode}) ; exit $exitcode ; } catch [Exception] {exit -1} }"  -url "%CHROMEDRIVER_WIN32_URL%"&& echo %CHROMEDRIVER_WIN32_URL% is present
 	echo Download Chrome driver %BUILD% from %CHROMEDRIVER_WIN32_URL%
 	wget.exe -r -np -l 1 -A zip %CHROMEDRIVER_WIN32_URL% -O %CHROMEDRIVER_PACKAGE%
 	7z.exe x %CHROMEDRIVER_PACKAGE% -y
   echo Chromedriver can be renamed to: %RENAMED_CHROMEDRIVER_NAME%
-	rem optional - rename the exe file, and add to the path and system variables
-	rem move %CHROMEDRIVER_PROCESS_NAME%.exe %RENAMED_CHROMEDRIVER_NAME%
-  rem set webdriver.chrome.driver=%RENAMED_CHROMEDRIVER_NAME%
-  rem setx PATH "%PATH%;%RENAMED_CHROMEDRIVER_NAME:"=%"
+	REM optional - rename the exe file, and add to the path and system variables
+	REM move %CHROMEDRIVER_PROCESS_NAME%.exe %RENAMED_CHROMEDRIVER_NAME%
+  REM set webdriver.chrome.driver=%RENAMED_CHROMEDRIVER_NAME%
+  REM setx PATH "%PATH%;%RENAMED_CHROMEDRIVER_NAME:"=%"
 ) else (
   echo Chrome driver %CHROMEDRIVER_PACKAGE:"=% already downloaded
 )
@@ -69,7 +83,7 @@ set GECKODRIVER_URL=https://api.github.com/repos/mozilla/geckodriver/releases
 echo Determine the latest Gecko driver version
 set RESPONSE_FILE=response.json
 wget.exe -O- -nv %GECKODRIVER_URL% > %RESPONSE_FILE%
-rem TODO: correctly determine the browser_download_url
+REM TODO: correctly determine the browser_download_url
 for /L %%_ in (1,1,4) do (
 set ITEM=%%_
 for /F "delims=" %%_ in ('%JQ:"=% [.[0].assets][0][!ITEM!].name ^< %RESPONSE_FILE%') do set x=%%_
@@ -86,6 +100,9 @@ if not exist "%GECKODRIVER_OUTPUT_FOLDER%" (mkdir %GECKODRIVER_OUTPUT_FOLDER:"=%
 set GECKODRIVER_PACKAGE=%GECKODRIVER_OUTPUT_FOLDER%/!GECKODRIVER_NAME!
 
 if not exist "%GECKODRIVER_PACKAGE:"=%" (
+  REM %CURL% --output NUL --silent --head --fail --url %GECKODRIVER_URLL% >NUL 2>&1 && echo %GECKODRIVER_URL% is present
+  REM %WGET% --spider --no-dns-cache --no-cache --quiet %GECKODRIVER_URL% >NUL 2>&1 && echo %GECKODRIVER_URL% is present
+  REM powershell.exe -command "& {Param([string]$url) write-output ('url = {0}' -f $url); try { $o = Invoke-WebRequest -Uri $url -Method Head -UseBasicParsing -ErrorAction SilentlyContinue; echo $o.StatusCode; $status = ( $o.StatusCode -eq 200 ); $exitcode  =  [int]( -not ($status)); write-output ('will exit with {0}' -f ${exitcode}) ; exit $exitcode ; } catch [Exception] {exit -1} }"  -url "%GECKODRIVER_URL%" && echo %GECKODRIVER_URL% is present
 	echo Download Gecko driver from %GECKODRIVER_URL:"=%
 	wget.exe -r -np -l 1 -A zip !GECKODRIVER_URL! -O %GECKODRIVER_PACKAGE%
 	7z.exe x %GECKODRIVER_PACKAGE%  -y
