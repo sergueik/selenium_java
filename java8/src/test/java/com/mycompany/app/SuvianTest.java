91e8f1623de2cd804e75ddd2c5083b27d1498897
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-05-20 01:42:08 -0400

    intermediate revision - consolidated projects

--- java8/src/test/java/com/mycompany/app/SuvianTest.java
+++ java8/src/test/java/com/mycompany/app/SuvianTest.java
@@ -2900,7 +2900,90 @@ public class SuvianTest {
 		}
 	}
 
+	@Test(enabled = true)
+	public void test33_2() {
+
+		// Go to base url
+		driver.get("https://datatables.net/examples/api/highlight.html");
+
+		// Maximize Window
+		driver.manage().window().maximize();
+
+		String table_id = "example";
+		WebElement table_element;
+
+		try {
+			wait.until(
+					ExpectedConditions.visibilityOfElementLocated(By.id(table_id)));
+		} catch (RuntimeException timeoutException) {
+			return;
+		}
+
+		String script = "var table_row_locator = 'div#example_wrapper table#example tbody tr';\n"
+				+ "var rows = document.querySelectorAll(table_row_locator);\n"
+				+ "var result = [];\n"
+				+ "for (row_cnt = 0; row_cnt != rows.length; row_cnt++) {\n"
+				+ "var row = rows[row_cnt];\n" + "if (row instanceof Element) {\n"
+				+ "var cols = row.querySelectorAll('td');\n"
+				+ "var check_col_num = 1;\n" + "var data_col_num = 0;\n"
+				+ "if (cols[check_col_num].innerHTML.match(/^Software.*/ig)) {\n"
+				+ "result.push(cols[data_col_num].innerHTML);\n" + "}\n" + "}\n" + "}\n"
+				+ "return result.join();\n";
+
+		System.out.println(
+				String.format("Script: '%s'\nResult: '%s'", script, storeEval(script)));
+
+		// NOTE: Works in IDE, does not work with WebDriver
+		script = String.format("(function() { %s })();", script);
+		System.out.println(
+				String.format("Script: '%s'\nResult: '%s'", script, storeEval(script)));
+
+		table_element = driver.findElement(By.id(table_id));
+		// NOTE: no leading slash in XPath
+		List<WebElement> rows = table_element.findElements(By.xpath("tbody/tr"));
+		System.out.println("NUMBER OF ROWS IN THIS TABLE = " + rows.size());
+		int row_num, cell_num, max_rows;
+		max_rows = 3;
+		row_num = 1;
+		String row_role_attribute = "row";
+		for (WebElement row : rows) {
+			if (row_num > max_rows) {
+				break;
+			}
+			assertTrue(
+					// String.format("Unexpected title '%s'", row.getAttribute("role")),
+					row.getAttribute("role").matches(row_role_attribute));
+			cell_num = 1;
+			List<WebElement> cells = row.findElements(By.xpath("td"));
+			String checkColumn = cells.get(cell_num).getText();
+			// System.out.println(checkColumn);
+			if (checkColumn.matches("Software.*")) {
+				// System.out.println("NUMBER OF COLUMNS=" + cells.size());
+				cell_num = 1;
+				for (WebElement cell : cells) {
+					// Hover over cell
+					actions.moveToElement(cell).build().perform();
+					highlight(cell);
+					System.out.println(String.format("row # %d, col # %d text='%s'",
+							row_num, cell_num, cell.getText()));
+					cell_num++;
+				}
+				row_num++;
+			}
+		}
+	}
+
 	// Utils
+
+	private String storeEval(String script) {
+		String result = null;
+		if (driver instanceof JavascriptExecutor) {
+			System.out.println("running the script");
+			result = (String) ((JavascriptExecutor) driver).executeScript(script);
+		}
+		return result;
+	}
+
 	public static String getOsName() {
 		if (osName == null) {
 			osName = System.getProperty("os.name");
