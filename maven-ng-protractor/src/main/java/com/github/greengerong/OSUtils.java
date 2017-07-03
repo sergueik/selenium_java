d7233c2096b8ff575fdf8a9bf731aa9e094813b1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-02 22:26:39 -0400

    intermediate revision - updated version of dependencies

--- maven-ng-protractor/src/main/java/com/github/greengerong/OSUtils.java
+++ maven-ng-protractor/src/main/java/com/github/greengerong/OSUtils.java
@@ -1,16 +1,7 @@
 package com.github.greengerong;
 
-/******************************************
- *                                        *
- * Auth: green gerong                     *
- * Date: 2014.                         *
- * blog: http://greengerong.github.io/    *
- * github: https://github.com/greengerong *
- *                                        *
- ******************************************/
-
 public class OSUtils {
-    public static boolean isWindows() {
-        return System.getProperty("os.name").toLowerCase().contains("windows");
-    }
+	public static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().contains("windows");
+	}
 }
