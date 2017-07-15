23d80c9f02a75d89e1729a459c7e7e340321e2ad
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-15 19:44:32 -0400

    intermediate revision - added TodoListTest

---
+++ protractor/src/test/java/com/jprotractor/integration/NgTodoListTest.java
@@ -0,0 +1,137 @@
+package com.jprotractor.integration;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import java.util.concurrent.TimeUnit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+import com.jprotractor.NgBy;
+import com.jprotractor.NgWebDriver;
+import com.jprotractor.NgWebElement;
+
+/**
+ * Integration tests of Angular TODO List http://jaykanakiya.com/demos/angular-js-todolist/
+ * @author Serguei Kouzmine (kouzmine_serguei@yahoo.com)
+ */
+
+public class NgTodoListTest {
+
+	private static NgWebDriver ngDriver;
+	private static WebDriver seleniumDriver;
+	static WebDriverWait wait;
+	static Actions actions;
+	static Alert alert;
+	static int implicitWait = 10;
+	static int flexibleWait = 5;
+	static long pollingInterval = 500;
+	static int width = 600;
+	static int height = 800;
+	// set to true for Desktop, false for headless browser testing
+	static boolean isCIBuild = false;
+	private static String baseUrl = "http://jaykanakiya.com/demos/angular-js-todolist/";
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		isCIBuild = CommonFunctions.checkEnvironment();
+		seleniumDriver = CommonFunctions.getSeleniumDriver();
+		seleniumDriver.manage().window().setSize(new Dimension(width, height));
+		seleniumDriver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS)
+				.implicitlyWait(implicitWait, TimeUnit.SECONDS)
+				.setScriptTimeout(10, TimeUnit.SECONDS);
+		wait = new WebDriverWait(seleniumDriver, flexibleWait);
+		wait.pollingEvery(pollingInterval, TimeUnit.MILLISECONDS);
+		actions = new Actions(seleniumDriver);
+		ngDriver = new NgWebDriver(seleniumDriver);
+	}
+
+	@Before
+	public void beforeEach() {
+		ngDriver.navigate().to(baseUrl);
+	}
+
+	@Test
+	public void testCountItems() {
+		List<WebElement> elements = ngDriver
+				.findElements(NgBy.cssContainingText("span.todoName", "Angular-js"));
+		assertThat(elements, notNullValue());
+		assertThat(elements.size(), equalTo(2));
+
+		elements.stream().forEach(element -> {
+			actions.moveToElement(element);
+			highlight(element);
+			System.err.println(element.getText());
+		});
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+		}
+
+	}
+
+	@AfterClass
+	public static void teardown() {
+		ngDriver.close();
+		seleniumDriver.quit();
+	}
+
+	private static void highlight(WebElement element) {
+		CommonFunctions.highlight(element);
+	}
+
+}
