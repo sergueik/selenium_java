1269dd11f596ac12523b54d5c5ded25eb4555ef9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-23 13:00:35 -0400

    intermediate revision - added initial sqlite jdbc code

---
+++ Jenkinsfile
@@ -0,0 +1,38 @@
+pipeline {
+  agent any
+  triggers {
+    pollSCM('H/5 * * * *')
+  }
+  
+  stages {
+    stage('Example') {
+      steps {
+        echo 'hello jenkins'
+      }
+    }
+  }
+}
+
+node {
+  def mvnHome
+  stage('Preparation') {
+    git 'https://github.com/LinuxSuRen/phoenix.webui.server.git'
+    mvnHome = tool 'M3'
+  }
+  
+  stage('Build') {
+    if(isUnix()){
+      sh "'${mvnHome}/bin/mvn' clean package"
+    }else{
+      bat(/"${mvnHome}\bin\mvn" clean package/)
+    }
+  }
+  
+  stage('Deploy') {
+    if(isUnix()){
+      sh "'${mvnHome}/bin/mvn' deploy -DsignSkip=false -DdocSkip=false"
+    }else{
+      bat(/"${mvnHome}\bin\mvn" deploy -DsignSkip=false -DdocSkip=false/)
+    }
+  }
+}
--- chrome_page_performance_sqlite/src/test/java/org/utils/ChromePagePerformanceTest.java
+++ chrome_page_performance_sqlite/src/test/java/org/utils/ChromePagePerformanceTest.java
@@ -27,10 +27,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.sqlite.Function;
+import org.sqlite.SQLiteConnection;
 
 public class ChromePagePerformanceTest {
 
 	private static WebDriver driver;
+	private static Connection conn;
 
 	@BeforeClass
 	public static void setup() throws IOException {
@@ -72,26 +82,67 @@ public class ChromePagePerformanceTest {
 		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		driver = new ChromeDriver(capabilities);
+		try {
+			// origin:
+			// https://github.com/xerial/sqlite-jdbc/blob/master/demo/Sample.java
+			// http://www.codejava.net/java-se/jdbc/connect-to-sqlite-via-jdbc
+			// http://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/
+			// https://www.tutorialspoint.com/sqlite/sqlite_java.htm
+			Class.forName("org.sqlite.JDBC");
+			String dbURL = "jdbc:sqlite:product.db";
+			conn = DriverManager.getConnection(dbURL);
+			if (conn != null) {
+				System.out.println("Connected to the database");
+				DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+				System.out.println("Driver name: " + dm.getDriverName());
+				System.out.println("Driver version: " + dm.getDriverVersion());
+				System.out.println("Product name: " + dm.getDatabaseProductName());
+				System.out
+						.println("Product version: " + dm.getDatabaseProductVersion());
+				conn.close();
+			}
+		} catch (ClassNotFoundException | SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			// TODO: refactor after the saving data is implemented
+			try {
+				if (conn != null) {
+					conn.close();
+					conn = null;
+				}
+			} catch (SQLException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
 	}
 
 	@AfterClass
 	public static void teardown() {
 		driver.close();
 		driver.quit();
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 	@Test
 	public void testSetTimer() {
 		String url = "https://www.royalcaribbean.com/";
-		double test = new ChromePagePerformanceObject(driver, url, new ById("find-a-cruise"))
-				.getLoadTime();
+		double test = new ChromePagePerformanceObject(driver, url,
+				new ById("find-a-cruise")).getLoadTime();
 		System.out.println(test);
 	}
 
 	@Test
 	public void testUtil() {
-		ChromePagePerformanceUtil pageLoadTimer = ChromePagePerformanceUtil.getInstance();
-		double test = pageLoadTimer.getLoadTime(driver, "https://www.royalcaribbean.com/", new ById("find-a-cruise"));
+		ChromePagePerformanceUtil pageLoadTimer = ChromePagePerformanceUtil
+				.getInstance();
+		double test = pageLoadTimer.getLoadTime(driver,
+				"https://www.royalcaribbean.com/", new ById("find-a-cruise"));
 		System.out.println(test);
 	}
 }
