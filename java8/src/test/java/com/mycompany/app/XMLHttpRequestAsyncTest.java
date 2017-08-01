6b4aa88d74cb3658c7741809ff7b262fd9e323fb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-31 23:43:38 -0400

    initial revision

---
+++ java8/src/test/java/com/mycompany/app/XMLHttpRequestAsyncTest.java
@@ -0,0 +1,241 @@
+package com.mycompany.app;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+// origin: https://github.com/TsvetomirSlavov/JavaScriptSwTestAcademy/blob/master/ExecuteAsyncXMLHttpRequestTest.java
+
+//Ref1: https://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/JavascriptExecutor.html
+//Ref2: http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
+
+public class XMLHttpRequestAsyncTest {
+
+	private static WebDriver driver;
+	private WebDriverWait wait;
+	private static Actions actions;
+	private int flexibleWait = 5;
+	private long pollingInterval = 500;
+	private static final StringBuffer verificationErrors = new StringBuffer();
+	private static final String browser = "chrome";
+	private static String osName;
+	private static String baseURL = "http://phppot.com/demo/jquery-dependent-dropdown-list-countries-and-states/";
+  
+	@Test
+	public void sendXMLHTTPRequestTest() {
+		String response = null;
+
+		// Set ScriptTimeout
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
+		// Declare JavascriptExecutor
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+    int index  = 3;
+    selectDropdownByIndex(By.xpath("select[@id='country-list']"), index);
+
+		// Injecting a XMLHttpRequest and waiting for the result
+		try {
+			response = (String )js.executeAsyncScript(
+      String.format(
+					"var callback = arguments[arguments.length - 1];\n"
+			    +	"var http = new XMLHttpRequest();\n" 
+					+ "var url = 'get_state.php';\n"
+					+ "var params = 'country_id=%d';\n"
+					+	"http.open('POST', url, true);\n"
+					+ "http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');\n"
+					+ "http.setRequestHeader('Content-length', params.length);\n"
+					+ "http.setRequestHeader('Connection', 'close');\n"
+					+ "http.onreadystatechange = function() {\n"
+          + "    if (http.readyState == 4) {\n"
+          + "        callback(http.responseText);" + "    };\n" 
+          + "};\n"
+          + "http.send(params);"
+          , index));
+		} catch (UnhandledAlertException e) {
+			System.err.println("Error Occured!");
+		}
+
+		// Assert that returned cities are related with USA
+		System.out.println(response);
+		assertThat(response,
+				containsString("<option value=\"13\">Picardie</option>"));
+        // evaluate !
+        
+		try {
+			// https://stackoverflow.com/questions/8923398/regex-doesnt-work-in-string-matches
+			assertTrue(
+					response.matches(".*<option value=\"\\d+\">Picardie</option>.*"));
+
+		} catch (AssertionError e) {
+			System.err.println("Exception (ignored) " + e.toString());
+		}
+
+		Pattern p = Pattern.compile(".*<option value=\"(\\d+)\">Picardie</option>.*", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(response);
+		assertTrue(m.find());
+    sleep(100);
+	}
+
+  // common code
+	@BeforeSuite
+	@SuppressWarnings("deprecation")
+	public void beforeSuite() throws Exception {
+		getOsName();
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					(new File("c:/java/selenium/chromedriver.exe")).getAbsolutePath());
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			ChromeOptions options = new ChromeOptions();
+
+			HashMap<String, Object> chromePrefs = new HashMap<>();
+			chromePrefs.put("profile.default_content_settings.popups", 0);
+			String downloadFilepath = System.getProperty("user.dir")
+					+ System.getProperty("file.separator") + "target"
+					+ System.getProperty("file.separator");
+			chromePrefs.put("download.default_directory", downloadFilepath);
+			chromePrefs.put("enableNetwork", "true");
+			options.setExperimentalOption("prefs", chromePrefs);
+
+			for (String optionAgrument : (new String[] {
+					"allow-running-insecure-content", "allow-insecure-localhost",
+					"enable-local-file-accesses", "disable-notifications",
+					/* "start-maximized" , */
+					"browser.download.folderList=2",
+					"--browser.helperApps.neverAsk.saveToDisk=image/jpg,text/csv,text/xml,application/xml,application/vnd.ms-excel,application/x-excel,application/x-msexcel,application/excel,application/pdf",
+					String.format("browser.download.dir=%s", downloadFilepath)
+					/* "user-data-dir=/path/to/your/custom/profile"  , */
+			})) {
+				options.addArguments(optionAgrument);
+			}
+
+			// options for headless
+			/*
+			for (String optionAgrument : (new String[] { "headless",
+					"window-size=1200x600", })) {
+				options.addArguments(optionAgrument);
+			}
+			*/
+			capabilities
+					.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
+
+			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			driver = new ChromeDriver(capabilities);
+		} else if (browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					osName.toLowerCase().startsWith("windows")
+							? new File("c:/java/selenium/geckodriver.exe").getAbsolutePath()
+							: "/tmp/geckodriver");
+			System
+					.setProperty("webdriver.firefox.bin",
+							osName.toLowerCase().startsWith("windows") ? new File(
+									"c:/Program Files (x86)/Mozilla Firefox/firefox.exe")
+											.getAbsolutePath()
+									: "/usr/bin/firefox");
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			// use legacy FirefoxDriver
+			capabilities.setCapability("marionette", false);
+			// http://www.programcreek.com/java-api-examples/index.php?api=org.openqa.selenium.firefox.FirefoxProfile
+			capabilities.setCapability("locationContextEnabled", false);
+			capabilities.setCapability("acceptSslCerts", true);
+			capabilities.setCapability("elementScrollBehavior", 1);
+			FirefoxProfile profile = new FirefoxProfile();
+			profile.setAcceptUntrustedCertificates(true);
+			profile.setAssumeUntrustedCertificateIssuer(true);
+			profile.setEnableNativeEvents(false);
+
+			System.out.println(System.getProperty("user.dir"));
+			capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+			try {
+				driver = new FirefoxDriver(capabilities);
+			} catch (WebDriverException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Cannot initialize Firefox driver");
+			}
+		}
+		actions = new Actions(driver);
+		/*
+		System.setProperty("webdriver.chrome.driver",
+				"c:/java/selenium/chromedriver.exe");
+		driver = new ChromeDriver();
+		*/
+		driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, flexibleWait);
+		wait.pollingEvery(pollingInterval, TimeUnit.MILLISECONDS);
+		actions = new Actions(driver);
+	}
+
+	@AfterSuite
+	public void afterSuite() throws Exception {
+		driver.quit();
+	}
+
+	@BeforeMethod
+	public void BeforeMethod() {
+		driver.get(baseURL);
+	}
+
+	@AfterMethod
+	public void AfterMethod(ITestResult result) {
+		if (verificationErrors.length() != 0) {
+			throw new RuntimeException(String.format("Error in the method %s : %s",
+					result.getMethod().getMethodName(), verificationErrors.toString()));
+		}
+		driver.get("about:blank");
+	}
+
+	// utilities
+	public static String getOsName() {
+		if (osName == null) {
+			osName = System.getProperty("os.name");
+		}
+		return osName;
+	}
+
+	public void sleep(Integer seconds) {
+		long secondsLong = (long) seconds;
+		try {
+			Thread.sleep(secondsLong);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+  public static void selectDropdownByIndex(By locator, int index) {
+    try {
+      Select select = new Select(driver.findElement(locator));
+      select.selectByIndex(index);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
