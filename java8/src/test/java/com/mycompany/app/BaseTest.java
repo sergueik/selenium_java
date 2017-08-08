40c3b93f7473ed08b9f6c5f395a5e51f35defcab
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-08-08 19:01:31 -0400

    intermediate revision - temporarily broken in the refactoring

---
+++ java8/src/test/java/com/mycompany/app/BaseTest.java
@@ -0,0 +1,186 @@
+package com.mycompany.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+	public WebDriver driver;
+	public WebDriverWait wait;
+	public Actions actions;
+	public Alert alert;
+	private static TakesScreenshot screenshot;
+
+	private static final String browser = "chrome";
+	private static String osName;
+
+	private int scriptTimeout = 5;
+	private int flexibleWait = 120;
+	private int implicitWait = 1;
+	private long pollingInterval = 500;
+
+	public String baseURL = "about:blank";
+
+	// WARMING:  do not define or the descendant test class will fail 
+	@AfterSuite
+	public void afterSuite() throws Exception {
+	}
+
+	// WARMING:  do not define or the descendant test class will fail 
+	@BeforeSuite
+	public void beforeSuite(){
+	}	
+	
+	@AfterClass
+	public void afterClass() throws Exception {
+		driver.quit();
+	}
+
+	@BeforeClass
+	public void beforeClass() throws IOException {
+
+		getOsName();
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					(new File("c:/java/selenium/chromedriver.exe")).getAbsolutePath());
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			ChromeOptions options = new ChromeOptions();
+
+			HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+			chromePrefs.put("profile.default_content_settings.popups", 0);
+			String downloadFilepath = System.getProperty("user.dir")
+					+ System.getProperty("file.separator") + "target"
+					+ System.getProperty("file.separator");
+			chromePrefs.put("download.default_directory", downloadFilepath);
+			chromePrefs.put("enableNetwork", "true");
+			options.setExperimentalOption("prefs", chromePrefs);
+			options.addArguments("allow-running-insecure-content");
+			options.addArguments("allow-insecure-localhost");
+			options.addArguments("enable-local-file-accesses");
+			options.addArguments("disable-notifications");
+			// options.addArguments("start-maximized");
+			options.addArguments("browser.download.folderList=2");
+			options.addArguments(
+					"--browser.helperApps.neverAsk.saveToDisk=image/jpg,text/csv,text/xml,application/xml,application/vnd.ms-excel,application/x-excel,application/x-msexcel,application/excel,application/pdf");
+			options.addArguments("browser.download.dir=" + downloadFilepath);
+			// options.addArguments("user-data-dir=/path/to/your/custom/profile");
+			capabilities
+					.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
+			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			driver = new ChromeDriver(capabilities);
+		} else if (browser.equals("firefox")) {
+
+			System.setProperty("webdriver.gecko.driver",
+					osName.toLowerCase().startsWith("windows")
+							? new File("c:/java/selenium/geckodriver.exe").getAbsolutePath()
+							: "/tmp/geckodriver");
+			System
+					.setProperty("webdriver.firefox.bin",
+							osName.toLowerCase().startsWith("windows") ? new File(
+									"c:/Program Files (x86)/Mozilla Firefox/firefox.exe")
+											.getAbsolutePath()
+									: "/usr/bin/firefox");
+			// https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			// use legacy FirefoxDriver
+			capabilities.setCapability("marionette", false);
+			// http://www.programcreek.com/java-api-examples/index.php?api=org.openqa.selenium.firefox.FirefoxProfile
+			capabilities.setCapability("locationContextEnabled", false);
+			capabilities.setCapability("acceptSslCerts", true);
+			capabilities.setCapability("elementScrollBehavior", 1);
+			FirefoxProfile profile = new FirefoxProfile();
+			profile.setAcceptUntrustedCertificates(true);
+			profile.setAssumeUntrustedCertificateIssuer(true);
+			profile.setEnableNativeEvents(false);
+
+			System.out.println(System.getProperty("user.dir"));
+			capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+			try {
+				driver = new FirefoxDriver(capabilities);
+			} catch (WebDriverException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Cannot initialize Firefox driver");
+			}
+		}
+		actions = new Actions(driver);
+
+		driver.manage().timeouts().setScriptTimeout(scriptTimeout,
+				TimeUnit.SECONDS);
+		// Declare a wait time
+		wait = new WebDriverWait(driver, flexibleWait);
+		wait.pollingEvery(pollingInterval, TimeUnit.MILLISECONDS);
+		screenshot = ((TakesScreenshot) driver);
+		// driver.manage().window().maximize();
+
+		// Go to URL
+		driver.get(baseURL);
+	}
+
+	@BeforeMethod
+	public void beforeMethod(Method method) {
+		String testName = method.getName();
+		System.out.println("Test Name: " + testName + "\n");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.get("about:blank");
+	}
+// Utilities
+
+	public static String getOsName() {
+		if (osName == null) {
+			osName = System.getProperty("os.name");
+		}
+		return osName;
+	}
+
+	public void highlight(WebElement element, long highlight_interval) {
+		if (wait == null) {
+			wait = new WebDriverWait(driver, flexibleWait);
+		}
+		wait.pollingEvery(pollingInterval, TimeUnit.MILLISECONDS);
+		try {
+			wait.until(ExpectedConditions.visibilityOf(element));
+			if (driver instanceof JavascriptExecutor) {
+				((JavascriptExecutor) driver).executeScript(
+						"arguments[0].style.border='3px solid yellow'", element);
+			}
+			Thread.sleep(highlight_interval);
+			if (driver instanceof JavascriptExecutor) {
+				((JavascriptExecutor) driver)
+						.executeScript("arguments[0].style.border=''", element);
+			}
+		} catch (InterruptedException e) {
+			// System.err.println("Ignored: " + e.toString());
+		}
+	}
+
+}
