98fedc332c08d6bae7662d82d23fbe07ec666ebc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-09-09 22:45:00 -0400

    intermediate revision - misc. cleanup

--- sqlite/src/main/java/com/mycompany/app/App.java
+++ sqlite/src/main/java/com/mycompany/app/App.java
@@ -7,52 +7,36 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 // https://bitbucket.org/xerial/sqlite-jdbc
-public class App
-{
-  public static void main(String[] args) throws ClassNotFoundException
-  {
-    // load the sqlite-JDBC driver using the current class loader
-    Class.forName("org.sqlite.JDBC");
+public class App {
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("org.sqlite.JDBC");
 
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite::memory:");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);
 
-      statement.executeUpdate("drop table if exists person");
-      statement.executeUpdate("create table person (id integer, name string)");
-      statement.executeUpdate("insert into person values(1, 'leo')");
-      statement.executeUpdate("insert into person values(2, 'yui')");
-      ResultSet rs = statement.executeQuery("select * from person");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("name = " + rs.getString("name"));
-        System.out.println("id = " + rs.getInt("id"));
-      }
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory", 
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-  }
+			statement.executeUpdate("drop table if exists person");
+			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+			ResultSet rs = statement.executeQuery("select * from person");
+			while (rs.next()) {
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			// NOTE: the "out of memory" error message is likely because no database
+			// file is found
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				System.err.println(e);
+			}
+		}
+	}
 }
-
