4c8d5a8c46dda1fa9f24eaf72222c85f10d65eab
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-30 21:26:08 -0500

    intermediate revision - refactored showAndWait to pass results back

---
+++ javafx_example/src/main/java/example/ChoicesDialog.java
@@ -0,0 +1,139 @@
+package example;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.StageStyle;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Category;
+
+@SuppressWarnings("restriction")
+// see also: https://github.com/4ntoine/JavaFxDialog/wiki
+//
+public class ChoicesDialog extends Stage {
+
+	private Stage stage;
+	// NOTE: can't do that
+	// private static final Map<String, String> userData = new HashMap<>();
+	static final Category logger = Category.getInstance(ChoicesDialog.class);
+	public static String result = null;
+	private Scene scene;
+
+	// https://stackoverflow.com/questions/34118025/javafx-pass-values-from-child-to-parent
+	public String getResult() {
+		return result;
+	}
+
+	// https://stackoverflow.com/questions/12830402/javafx-2-buttons-size-fill-width-and-are-each-same-width
+	@SuppressWarnings("restriction")
+	public ChoicesDialog(Stage stage) {
+		super();
+		this.stage = stage;
+		initOwner(stage);
+		setTitle("title");
+		initModality(Modality.APPLICATION_MODAL);
+		// https://stackoverflow.com/questions/8341305/how-to-remove-javafx-stage-buttons-minimize-maximize-close
+		this.initStyle(StageStyle.UNDECORATED);
+		Group root = new Group();
+		HBox outerHBox = new HBox();
+		outerHBox.setAlignment(Pos.CENTER);
+
+		scene = new Scene(root, 300, 200);
+		setScene(scene);
+
+		VBox outerVBox = new VBox();
+		outerHBox.getChildren().add(outerVBox);
+		outerHBox.setSpacing(12);
+		scene.setRoot(outerHBox);
+
+		HBox labelHBox = new HBox();
+		Label label = new Label();
+		label.setText("Do yo relly want to exit?");
+		labelHBox.setHgrow(label, Priority.ALWAYS);
+		labelHBox.getChildren().add(label);
+
+		HBox buttonHBox1 = new HBox();
+		Button button1 = new Button();
+		button1.setText("Exit and save my project");
+		button1.setMaxWidth(Double.MAX_VALUE);
+		buttonHBox1.setHgrow(button1, Priority.ALWAYS);
+		buttonHBox1.getChildren().add(button1);
+		Map<String, String> userData = new HashMap<>();
+		userData.put("test", "1");
+		// userData.replace("test", "1");
+		button1.setUserData(userData);
+		button1.setOnAction(new buttonHandler());
+
+		HBox buttonHBox2 = new HBox();
+		Button button2 = new Button();
+		button2.setText("Exit and don't save");
+		button2.setMaxWidth(Double.MAX_VALUE);
+		buttonHBox2.setHgrow(button2, Priority.ALWAYS);
+		buttonHBox2.getChildren().add(button2);
+		userData = new HashMap<>();
+		userData.put("test", "2");
+		// userData.replace("test", "2");
+		button2.setUserData(userData);
+		button2.setOnAction(new buttonHandler());
+
+		HBox buttonHBox3 = new HBox();
+		Button button3 = new Button();
+		button3.setText("Don't exit");
+		button3.setMaxWidth(Double.MAX_VALUE);
+		userData = new HashMap<>();
+		userData.put("test", "3");
+		button3.setUserData(userData);
+		button3.setOnAction(new buttonHandler());
+		buttonHBox3.setHgrow(button3, Priority.ALWAYS);
+		buttonHBox3.getChildren().add(button3);
+
+		outerVBox.setSpacing(12);
+		outerVBox.getChildren().addAll(labelHBox, buttonHBox1, buttonHBox2,
+				buttonHBox3);
+	}
+
+	private static class buttonHandler implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent event) {
+			Button button = (Button) event.getSource();
+
+			Map<String, String> data = (Map<String, String>) button.getUserData();
+			String eventData = null;
+			try {
+				eventData = data == null ? null : data.getOrDefault("test", null);
+			} catch (Exception e) {
+				System.err.println("Exception (ignored) " + e.toString());
+				eventData = null;
+			}
+			logger.info("Received: "
+					+ (eventData == null ? "no user data was received" : eventData));
+			Stage stage = (Stage) button.getScene().getWindow();
+			result = eventData;
+			stage.close();
+		}
+
+	}
+}
--- javafx_example/src/main/java/example/FlowPaneEx.java
+++ javafx_example/src/main/java/example/FlowPaneEx.java
@@ -1,43 +1,33 @@
 package example;
 
+import org.apache.log4j.Category;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-
-import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.stage.StageStyle;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.log4j.Category;
+import javafx.stage.StageStyle;
 
 @SuppressWarnings("restriction")
 public class FlowPaneEx extends Application {
 
 	static final Category logger = Category.getInstance(FlowPaneEx.class);
+	static Stage stage = null;
 
 	@Override
 	public void start(Stage stage) {
+		this.stage = stage;
 		stage.setTitle("SWET");
 		stage.setWidth(500);
 		stage.setHeight(160);
@@ -100,18 +90,41 @@ public class FlowPaneEx extends Application {
 		quitButton.setGraphic(new ImageView(quitImage));
 		quitButton.setTooltip(new Tooltip("Exit"));
 
+		stage.setOnCloseRequest(e -> {
+			e.consume();
+			ChoicesDialog choicesDialog = new ChoicesDialog(stage);
+			// choicesDialog.initStyle(StageStyle.UNDECORATED);
+			choicesDialog.sizeToScene();
+			// optionally hide self
+			// ((Node)(event.getSource())).getScene().getWindow().hide();
+			/*		choicesDialog.show();
+			while (choicesDialog.isShowing()) {
+			
+			}
+			*/
+			choicesDialog.showAndWait();
+			int code = Integer.parseInt(choicesDialog.getResult());
+			logger.info("Exit app with code: " + code);
+			if (code == 1 || code == 2) {
+				stage.close();
+			}
+		});
+
 		quitButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Stage stage = new Stage();
-
-				Stage ChoicesDialog = new ChoicesDialog(stage);
-				ChoicesDialog.initStyle(StageStyle.UNDECORATED);
-				ChoicesDialog.sizeToScene();
-				ChoicesDialog.show();
-				int code = 0;
+				// Stage stage = new Stage();
+				ChoicesDialog choicesDialog = new ChoicesDialog(stage);
+				choicesDialog.initStyle(StageStyle.UNDECORATED);
+				choicesDialog.sizeToScene();
+				// Hide this current window (if this is what you want)
+				// ((Node)(event.getSource())).getScene().getWindow().hide();
+				choicesDialog.showAndWait();
+				int code = Integer.parseInt(choicesDialog.getResult());
 				logger.info("Exit app with code: " + code);
-
+				if (code == 1 || code == 2) {
+					stage.close();
+				}
 			}
 		});
 
@@ -137,138 +150,5 @@ public class FlowPaneEx extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-	}
-}
-
-// see also: https://github.com/4ntoine/JavaFxDialog/wiki
-//
-class ChoicesDialog extends Stage {
-
-	// NOTE: can't do that
-	// private static final Map<String, String> userData = new HashMap<>();
-	static final Category logger = Category.getInstance(ChoicesDialog.class);
-
-	// https://stackoverflow.com/questions/12830402/javafx-2-buttons-size-fill-width-and-are-each-same-width
-	@SuppressWarnings("restriction")
-	public ChoicesDialog(Stage stage) {
-		super();
-		// https://stackoverflow.com/questions/8341305/how-to-remove-javafx-stage-buttons-minimize-maximize-close
-		// stage.initStyle(StageStyle.UNDECORATED);
-		initOwner(stage);
-		setTitle("title");
-		initModality(Modality.APPLICATION_MODAL);
-		Group root = new Group();
-		HBox outerHBox = new HBox();
-		outerHBox.setAlignment(Pos.CENTER);
-
-		Scene scene = new Scene(root, 300, 200);
-		setScene(scene);
-
-		VBox outerVBox = new VBox();
-		outerHBox.getChildren().add(outerVBox);
-		outerHBox.setSpacing(12);
-		scene.setRoot(outerHBox);
-
-		HBox labelHBox = new HBox();
-		Label label = new Label();
-		label.setText("Do yo relly want to exit?");
-		labelHBox.setHgrow(label, Priority.ALWAYS);
-		labelHBox.getChildren().add(label);
-
-		HBox buttonHBox1 = new HBox();
-		Button button1 = new Button();
-		button1.setText("Exit and save my project");
-		button1.setMaxWidth(Double.MAX_VALUE);
-		buttonHBox1.setHgrow(button1, Priority.ALWAYS);
-		buttonHBox1.getChildren().add(button1);
-		Map<String, String> userData = new HashMap<>();
-		userData.put("test", "1");
-		// userData.replace("test", "1");
-		button1.setUserData(userData);
-		button1.setOnAction(new buttonHandler());
-
-		HBox buttonHBox2 = new HBox();
-		Button button2 = new Button();
-		button2.setText("Exit and don't save");
-		button2.setMaxWidth(Double.MAX_VALUE);
-		buttonHBox2.setHgrow(button2, Priority.ALWAYS);
-		buttonHBox2.getChildren().add(button2);
-		userData = new HashMap<>();
-		userData.put("test", "2");
-		// userData.replace("test", "2");
-		button2.setUserData(userData);
-		button2.setOnAction(new buttonHandler());
-
-		HBox buttonHBox3 = new HBox();
-		Button button3 = new Button();
-		button3.setText("Don't exit");
-		button3.setMaxWidth(Double.MAX_VALUE);
-		userData = new HashMap<>();
-		userData.put("test", "3");
-		button3.setUserData(userData);
-		button3.setOnAction(new buttonHandler());
-		/*
-		button3.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				close();
-			}
-		});
-		*/
-		buttonHBox3.setHgrow(button3, Priority.ALWAYS);
-		buttonHBox3.getChildren().add(button3);
-
-		outerVBox.setSpacing(12);
-		outerVBox.getChildren().addAll(labelHBox, buttonHBox1, buttonHBox2,
-				buttonHBox3);
-
-		/*
-				GridPane gridpane = new GridPane();
-				gridpane.setPadding(new Insets(5));
-				gridpane.setHgap(5);
-				gridpane.setVgap(5);
-		
-				Label userNameLbl = new Label("User Name: ");
-				gridpane.add(userNameLbl, 0, 1);
-		
-				Label passwordLbl = new Label("Password: ");
-				gridpane.add(passwordLbl, 0, 2);
-				final TextField userNameFld = new TextField("Admin");
-				gridpane.add(userNameFld, 1, 1);
-		
-				final PasswordField passwordFld = new PasswordField();
-				passwordFld.setText("password");
-				gridpane.add(passwordFld, 1, 2);
-		
-				Button login = new Button("Change");
-				login.setOnAction(new EventHandler<ActionEvent>() {
-		
-					public void handle(ActionEvent event) {
-						close();
-					}
-				});
-				gridpane.add(login, 1, 3);
-				GridPane.setHalignment(login, HPos.RIGHT);
-				root.getChildren().add(gridpane);
-				*/
-	}
-
-	private static class buttonHandler implements EventHandler<ActionEvent> {
-		public void handle(ActionEvent event) {
-			Button button = (Button) event.getSource();
-
-			Map<String, String> data = (Map<String, String>) button.getUserData();
-			String eventData = null;
-			try {
-				eventData = data == null ? null : data.getOrDefault("test", null);
-			} catch (Exception e) {
-				System.err.println("Exception (ignored) " + e.toString());
-				eventData = null;
-			}
-			logger.info("Received: "
-					+ (eventData == null ? "no user data was received" : eventData));
-			Stage stage = (Stage) button.getScene().getWindow();
-			stage.close();
-		}
-
 	}
 }
