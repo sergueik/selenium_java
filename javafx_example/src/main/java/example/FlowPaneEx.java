f4abb894ebd24715d9ad57b43b8b972fa85cee17
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-02-05 21:19:26 -0500

    intermediat revision - cosmetic mods in ExceptionDialogEx

--- javafx_example/src/main/java/example/FlowPaneEx.java
+++ javafx_example/src/main/java/example/FlowPaneEx.java
@@ -52,18 +52,7 @@ public class FlowPaneEx extends Application {
 				getClass().getClassLoader().getResourceAsStream("browsers_32.png"));
 		launchButton.setGraphic(new ImageView(launchImage));
 		launchButton.setTooltip(new Tooltip("Launch browser"));
-		launchButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				try {
-					testFunction();
-				} catch (Exception e) {
 
-					Dialog.showThrowable("title of exxeption", "xxx",
-							(Exception) e /* e.getCause()*/, stage);
-				}
-			}
-		});
 		Button injectButton = new Button();
 		Image injectImage = new Image(
 				getClass().getClassLoader().getResourceAsStream("find_32.png"));
@@ -95,6 +84,18 @@ public class FlowPaneEx extends Application {
 				getClass().getClassLoader().getResourceAsStream("save_32.png"));
 		saveButton.setGraphic(new ImageView(saveImage));
 		saveButton.setTooltip(new Tooltip("Save session"));
+		saveButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					testFunction();
+				} catch (Exception e) {
+
+					Dialog.showThrowable("Exception", "Exception thrown",
+							(Exception) e /* e.getCause()*/, stage);
+				}
+			}
+		});
 
 		Button configButton = new Button();
 		Image configImage = new Image(
--- javafx_example/src/main/java/name/antonsmirnov/javafx/dialog/Dialog.java
+++ javafx_example/src/main/java/name/antonsmirnov/javafx/dialog/Dialog.java
@@ -31,7 +31,6 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
 
-
 // based on: https://github.com/4ntoine/JavaFxDialog/blob/master/code/name/antonsmirnov/javafx/dialog/Dialog.java
 
 /**
@@ -60,23 +59,17 @@ public class Dialog extends Stage {
 	protected HBox buttonsPanel;
 	protected Button okButton;
 
-	/**
-	 * Extracts stack trace from Throwable
-	 */
 	protected static class StacktraceExtractor {
 
 		public String extract(Throwable t) {
 			StringWriter sw = new StringWriter();
 			PrintWriter pw = new PrintWriter(sw);
-			System.err.println("Printing the exception " + t.toString());
+			// System.err.println("Printing the exception " + t.toString());
 			t.printStackTrace(pw);
 			return sw.toString();
 		}
 	}
 
-	/**
-	 * Dialog builder
-	 */
 	public static class Builder {
 		protected static final int STACKTRACE_LABEL_MAXHEIGHT = 240;
 		protected static final int MESSAGE_MIN_WIDTH = 180;
@@ -167,15 +160,16 @@ public class Dialog extends Stage {
 		}
 
 		private void alignScrollPane() {
-			stage.setWidth(stage.icon.getImage().getWidth()
+			double width = stage.icon.getImage().getWidth()
 					+ Math.max(stage.messageLabel.getWidth(),
 							(stage.stacktraceVisible
 									? Math.max(stage.stacktraceButtonsPanel.getWidth(),
 											stage.stackTraceLabel.getWidth())
-									: stage.stacktraceButtonsPanel.getWidth()))
-					+ 5 * MARGIN);
+									: stage.stacktraceButtonsPanel.getWidth()) + 5 * MARGIN);
 
-			stage.setHeight(Math.max(stage.icon.getImage().getHeight(),
+			System.err.println("set width " + width);
+			stage.setWidth(width);
+			double height = Math.max(stage.icon.getImage().getHeight(),
 					stage.messageLabel.getHeight()
 							+ stage.stacktraceButtonsPanel.getHeight()
 							+ (stage.stacktraceVisible
@@ -183,7 +177,9 @@ public class Dialog extends Stage {
 											STACKTRACE_LABEL_MAXHEIGHT)
 									: 0))
 
-					+ stage.buttonsPanel.getHeight() + 3 * MARGIN);
+					+ stage.buttonsPanel.getHeight() + 3 * MARGIN;
+			System.err.println("set height " + height);
+			stage.setHeight(height);
 			if (stage.stacktraceVisible) {
 				stage.scrollPane
 						.setPrefHeight(stage.getHeight() - stage.messageLabel.getHeight()
@@ -197,16 +193,31 @@ public class Dialog extends Stage {
 		private Builder setStackTrace(Throwable t) {
 			// view button
 			stage.viewStacktraceButton = new ToggleButton(
-					getString("buttonlabel.viewstacktrace"));
+					String.format("%s >>", getString("buttonlabel.viewstacktrace")));
+			stage.viewStacktraceButton.setMinWidth(BUTTON_WIDTH);
+			HBox.setMargin(stage.viewStacktraceButton, new Insets(0, 0, 0, MARGIN));
 
 			// copy button
 			stage.copyStacktraceButton = new Button(
 					getString("buttonlabel.copystacktrace"));
+			stage.copyStacktraceButton.setMinWidth(BUTTON_WIDTH);
 			HBox.setMargin(stage.copyStacktraceButton, new Insets(0, 0, 0, MARGIN));
 
+			// close button
+			stage.okButton = new Button(getString("buttonlabel.ok"));
+			stage.okButton.setPrefWidth(BUTTON_WIDTH);
+			stage.okButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				public void handle(ActionEvent t) {
+					stage.close();
+				}
+
+			});
+			HBox.setMargin(stage.okButton, new Insets(0, 0, 0, MARGIN));
+
 			stage.stacktraceButtonsPanel = new HBox();
-			stage.stacktraceButtonsPanel.getChildren()
-					.addAll(stage.viewStacktraceButton, stage.copyStacktraceButton);
+			stage.stacktraceButtonsPanel.getChildren().addAll(stage.okButton,
+					stage.copyStacktraceButton, stage.viewStacktraceButton);
 			VBox.setMargin(stage.stacktraceButtonsPanel,
 					new Insets(MARGIN, MARGIN, MARGIN, 0));
 			stage.messageBox.getChildren().add(stage.stacktraceButtonsPanel);
@@ -218,6 +229,8 @@ public class Dialog extends Stage {
 
 						public void changed(ObservableValue<? extends Number> ov,
 								Number oldValue, Number newValue) {
+							// too late?
+							System.err.println("Listener fired (changed)");
 							alignScrollPane();
 						}
 					});
@@ -227,6 +240,7 @@ public class Dialog extends Stage {
 
 						public void changed(ObservableValue<? extends Number> ov,
 								Number oldValue, Number newValue) {
+							System.err.println("Listener fired  (height)");
 							alignScrollPane();
 						}
 					});
@@ -234,6 +248,7 @@ public class Dialog extends Stage {
 			StacktraceExtractor extractor = new StacktraceExtractor();
 			stage.stacktrace = extractor.extract(t);
 
+			stage.setResizable(true);
 			stage.scrollPane = new ScrollPane();
 			stage.scrollPane.setContent(stage.stackTraceLabel);
 
@@ -242,6 +257,7 @@ public class Dialog extends Stage {
 				public void handle(ActionEvent t) {
 					stage.stacktraceVisible = !stage.stacktraceVisible;
 					if (stage.stacktraceVisible) {
+						// alignScrollPane();
 						stage.messageBox.getChildren().add(stage.scrollPane);
 						stage.stackTraceLabel.setText(stage.stacktrace);
 
@@ -263,7 +279,7 @@ public class Dialog extends Stage {
 
 				public void handle(ActionEvent t) {
 					Clipboard clipboard = Clipboard.getSystemClipboard();
-					Map<DataFormat, Object> map = new HashMap<DataFormat, Object>();
+					Map<DataFormat, Object> map = new HashMap<>();
 					map.put(DataFormat.PLAIN_TEXT, stage.stacktrace);
 					clipboard.setContent(map);
 				}
@@ -284,9 +300,8 @@ public class Dialog extends Stage {
 		}
 
 		protected void setIconFromResource(String resourceName) {
-			System.out.println("set icon from: " + resourceName);
-			final Image image = new Image(getClass().getClassLoader()
-					.getResourceAsStream(resourceName));
+			final Image image = new Image(
+					getClass().getClassLoader().getResourceAsStream(resourceName));
 			stage.icon.setImage(image);
 		}
 
@@ -301,7 +316,7 @@ public class Dialog extends Stage {
 		}
 
 		protected Builder setThrowableIcon() {
-			setIconFromResource(ICON_PATH + "bugIcon.png");
+			setIconFromResource(ICON_PATH + "facepalmIcon.png");
 			return this;
 		}
 
@@ -346,47 +361,26 @@ public class Dialog extends Stage {
 			return this;
 		}
 
-		/**
-		 * Add Yes button to confirmation dialog
-		 * 
-		 * @param actionHandler action handler
-		 * @return 
-		 */
 		public Builder addYesButton(EventHandler actionHandler) {
 			return addConfirmationButton(getString("buttonlabel.yes"), actionHandler);
 		}
 
-		/**
-		 * Add No button to confirmation dialog
-		 * 
-		 * @param actionHandler action handler
-		 * @return 
-		 */
 		public Builder addNoButton(EventHandler actionHandler) {
 			return addConfirmationButton(getString("buttonlabel.no"), actionHandler);
 		}
 
-		/**
-		 * Add Cancel button to confirmation dialog
-		 * 
-		 * @param actionHandler action handler
-		 * @return 
-		 */
 		public Builder addCancelButton(EventHandler actionHandler) {
 			return addConfirmationButton(getString("buttonlabel.cancel"),
 					actionHandler);
 		}
 
-		/**
-		 * Build dialog
-		 * 
-		 * @return dialog instance
-		 */
 		public Dialog build() {
-			if (stage.buttonsPanel.getChildren().size() == 0)
-				throw new RuntimeException("Add one dialog button at least");
-
-			stage.buttonsPanel.getChildren().get(0).requestFocus();
+			if (stage.buttonsPanel.getChildren().size() == 0) {
+				// must be showThrowable
+				// throw new RuntimeException("Add one dialog button at least");
+			} else {
+				stage.buttonsPanel.getChildren().get(0).requestFocus();
+			}
 			return stage;
 		}
 
@@ -395,118 +389,51 @@ public class Dialog extends Stage {
 		}
 	}
 
-	/**
-	 * Show information dialog box as parentWindow child
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 * @param owner parent window
-	 */
 	public static void showInfo(String title, String message, Window owner) {
 		new Builder().create().setOwner(owner).setTitle(title).setInfoIcon()
 				.setMessage(message).addOkButton().build().show();
 	}
 
-	/**
-	 * Show information dialog box as parentStage child
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 */
 	public static void showInfo(String title, String message) {
 		showInfo(title, message, null);
 	}
 
-	/**
-	 * Show warning dialog box as parentStage child
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 * @param owner parent window
-	 */
 	public static void showWarning(String title, String message, Window owner) {
 		new Builder().create().setOwner(owner).setTitle(title).setWarningIcon()
 				.setMessage(message).addOkButton().build().show();
 	}
 
-	/**
-	 * Show warning dialog box
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 */
 	public static void showWarning(String title, String message) {
 		showWarning(title, message, null);
 	}
 
-	/**
-	 * Show error dialog box
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 * @param owner parent window
-	 */
 	public static void showError(String title, String message, Window owner) {
 		new Builder().create().setOwner(owner).setTitle(title).setErrorIcon()
 				.setMessage(message).addOkButton().build().show();
 	}
 
-	/**
-	 * Show error dialog box
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 */
 	public static void showError(String title, String message) {
 		showError(title, message, null);
 	}
 
-	/**
-	 * Show error dialog box with stacktrace
-	 * 
-	 * @param title dialog title
-	 * @param message dialog message
-	 * @param t throwable
-	 * @param owner parent window 
-	 */
-	public static void showThrowable(String title, String message, Throwable t,
-			Window owner) {
+	public static void showThrowable(String title, String message,
+			Throwable throwable) {
+		showThrowable(title, message, throwable, null);
+	}
+
+	public static void showThrowable(String title, String message,
+			Throwable throwable, Window owner) {
 		new Builder().create().setOwner(owner).setTitle(title).setThrowableIcon()
-				.setMessage(message).setStackTrace(t).addOkButton().build().show();
+				.setMessage(message).setStackTrace(throwable)/*.addOkButton()*/.build()
+				.show();
 	}
 
-	/**
-	 * Show error dialog box with stacktrace
-	 * 
-	 * @param title dialog title     
-	 * @param message dialog message
-	 * @param t throwable
-	 */
-	public static void showThrowable(String title, String message, Throwable t) {
-		showThrowable(title, message, t, null);
-	}
-
-	/**
-	 * Build confirmation dialog builder
-	 * 
-	 * @param title dialog title     
-	 * @param message dialog message
-	 * @param owner parent window
-	 * @return 
-	 */
 	public static Builder buildConfirmation(String title, String message,
 			Window owner) {
 		return new Builder().create().setOwner(owner).setTitle(title)
 				.setConfirmationIcon().setMessage(message);
 	}
 
-	/**
-	 * Build confirmation dialog builder
-	 * 
-	 * @param title dialog title     
-	 * @param message dialog message
-	 * @return 
-	 */
 	public static Builder buildConfirmation(String title, String message) {
 		return buildConfirmation(title, message, null);
 	}
