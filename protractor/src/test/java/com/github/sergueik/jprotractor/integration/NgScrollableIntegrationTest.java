16e270e1ca59ad8d20faac8b9d5a6185fc72254e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-25 18:37:29 -0400

    intermediate revision - added chainedBy test

--- protractor/src/test/java/com/github/sergueik/jprotractor/integration/NgScrollableIntegrationTest.java
+++ protractor/src/test/java/com/github/sergueik/jprotractor/integration/NgScrollableIntegrationTest.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -24,6 +26,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.github.sergueik.jprotractor.NgBy;
 import com.github.sergueik.jprotractor.NgWebDriver;
+import com.github.sergueik.jprotractor.NgWebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.pagefactory.ByAll;
+import org.openqa.selenium.support.pagefactory.ByChained;
 
 /**
  * Tests of Javascript scrroll
@@ -40,11 +48,15 @@ public class NgScrollableIntegrationTest {
 	private static WebDriverWait wait;
 	private static Actions actions;
 	private static Alert alert;
-	private static int implicitWait = 10;
-	private static int flexibleWait = 5;
+	private static final int implicitWait = 10;
+	private static final int flexibleWait = 5;
 	private static long pollingInterval = 500;
-	private static int width = 600;
-	private static int height = 400;
+	private static final int width = 600;
+	private static final int height = 400;
+
+	private static final int maxRows = 5;
+	private static int rowCnt = 0;
+
 	// set to true for Desktop, false for headless browser testing
 	private static boolean isCIBuild = false;
 	private static String baseUrl = "http://www.globalsqa.com/angularJs-protractor/Scrollable/";
@@ -68,15 +80,12 @@ public class NgScrollableIntegrationTest {
 		ngDriver.navigate().to(baseUrl);
 	}
 
-	// @Ignore
+	@Ignore
 	@Test
-	public void testRowsVisibility() {
-
-		// Given we are using scrollable table
-
-		WebElement divTableContainer = wait.until( // Wait page to load
-				ExpectedConditions.visibilityOf(
-						ngDriver.findElement(By.className("table-container"))));
+	public void testRowsScrollVisibility() {
+		// Wait page to load
+		WebElement tableContainer = wait.until(ExpectedConditions
+				.visibilityOf(ngDriver.findElement(By.className("table-container"))));
 
 		List<WebElement> rows = ngDriver
 				.findElements(NgBy.repeater("row in rowCollection"));
@@ -84,23 +93,26 @@ public class NgScrollableIntegrationTest {
 		assertTrue(rows.size() > 1);
 		int size = rows.size();
 		System.err.println(String.format("Protractor finds: %d rows", size));
+		rowCnt = 0;
 		for (WebElement rowElement : rows) {
+			if (rowCnt++ > maxRows) {
+				break;
+			}
 			try {
 				if (!rowElement.isDisplayed()) {
 					break;
 				}
 				highlight(rowElement);
-				System.err.println(
-						rowElement.getText() + " " + rowElement.getLocation().getY());
+				System.err.println(String.format("row %d: %s\ny position: %d", rowCnt,
+						rowElement.getText(), rowElement.getLocation().getY()));
 				try {
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
 				}
-				scrollDown(rowElement.getSize().height, divTableContainer);
+				scrollDown(rowElement.getSize().height, tableContainer);
 				actions.moveToElement(rowElement).build().perform();
 			} catch (StaleElementReferenceException e) {
-
-				System.err.println("ignored StaleElementReferenceException");
+				System.err.println("Exception(ignored) " + e.toString());
 			}
 		}
 		//
@@ -121,6 +133,94 @@ public class NgScrollableIntegrationTest {
 			}
 		}
 		//
+	}
+
+	@Ignore
+	@Test
+	public void testRowColumnsWithByChained() {
+		// Wait page to load
+		wait.until(ExpectedConditions
+				.visibilityOf(ngDriver.findElement(By.className("table-container"))));
+		List<WebElement> nameRows = ngDriver.findElements(new ByChained(
+				NgBy.repeater("row in rowCollection"), NgBy.binding("row.firstName")));
+		rowCnt = 0;
+		for (WebElement rowElement : nameRows) {
+			if (rowCnt++ > maxRows) {
+				break;
+			}
+			try {
+				if (!rowElement.isDisplayed()) {
+					break;
+				}
+				highlight(rowElement);
+				System.err.println(String.format("First name in row %d: %s ", rowCnt,
+						rowElement.getText()));
+			} catch (StaleElementReferenceException e) {
+				System.err.println("Exception(ignored) " + e.toString());
+			}
+		}
+	}
+
+	// https://stackoverflow.com/questions/25914156/difference-between-findall-and-findbys-annotations-in-webdriver-page-factory
+	@Ignore
+	@Test
+	public void testRowColumnsWithByAll() {
+		// Wait page to load
+		wait.until(ExpectedConditions
+				.visibilityOf(ngDriver.findElement(By.className("table-container"))));
+		List<WebElement> rows = ngDriver
+				.findElements(new ByAll(NgBy.repeater("row in rowCollection")));
+		rowCnt = 0;
+		for (WebElement rowElement : rows) {
+			if (rowCnt++ > maxRows) {
+				break;
+			}
+			try {
+				if (!rowElement.isDisplayed()) {
+					break;
+				}
+				highlight(rowElement);
+				System.err.println(
+						String.format("Row %d: %s ", rowCnt, rowElement.getText()));
+			} catch (StaleElementReferenceException e) {
+				System.err.println("Exception(ignored) " + e.toString());
+			}
+		}
+	}
+
+	@Test
+	public void testRowsAndColumns() {
+		// Wait page to load
+		wait.until(ExpectedConditions
+				.visibilityOf(ngDriver.findElement(By.className("table-container"))));
+		List<WebElement> rows = ngDriver
+				.findElements(NgBy.repeater("row in rowCollection"));
+		assertThat(rows, notNullValue());
+		assertTrue(rows.size() > 1);
+		int size = rows.size();
+		System.err.println(String.format("Protractor finds: %d rows", size));
+		rowCnt = 0;
+		// NOTE: birthdate is empty
+		// <a ng-href="mailto:{{row.email}}">email</a></td>
+		// appears impossible to locate
+		List<String> columnBindings = Arrays.asList("row.firstName", "row.lastName"
+		/*, "row.birthDate" */, "row.balance" /*,  "row.email" */);
+		System.err.println("Inspecting first " + maxRows + " rows.");
+		for (WebElement rowElement : rows) {
+			if (rowCnt++ > maxRows) {
+				break;
+			}
+			System.err
+					.println(String.format("row %d: %s", rowCnt, rowElement.getText()));
+			highlight(rowElement);
+			for (String columnBinding : columnBindings) {
+				WebElement columnElement = new NgWebElement(ngDriver, rowElement)
+						.findElement(NgBy.binding(columnBinding));
+				System.err.println(columnBinding + ": " + columnElement.getText());
+				// will miss
+				highlight(columnElement);
+			}
+		}
 	}
 
 	@AfterClass
