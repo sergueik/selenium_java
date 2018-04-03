a5823aadc82d303add5c4f59ce07047c97cd2e4b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-04-02 21:03:06 -0400

    intermediate revision: fixed the APP_JAR, cofirmed the issue with javaFx on osx

--- javafx_example/run.sh
+++ javafx_example/run.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 # set -x
 
-JAVA_VERSION='1.8.0_121'
-MAVEN_VERSION='3.3.9'
-PACKAGE_NAME='javafx_example'
-PACKAGE_VERSION='0.0.1-SNAPSHOT'
+PACKAGE_NAME='javafx_example_app'
+SKIP_PACKAGE_VERSION=true
+
+# TODO: xmllint the `pom.xml`
+if [[ $SKIP_PACKAGE_VERSION ]] ; then
+  PACKAGE_NAME='javafx_example_app'
+  APP_JAR="$PACKAGE_NAME.jar"
+else
+  PACKAGE_NAME='javafx_example'
+  PACKAGE_VERSION='0.0.1-SNAPSHOT'
+  APP_JAR="$PACKAGE_NAME-$PACKAGE_VERSION.jar"
+fi
 
 MAIN_APP_PACKAGE='example'
 MAIN_APP_CLASS=${1:-FlowPaneEx}
 
 if $(uname -s | grep -qi Darwin)
 then
+  JAVA_VERSION='1.8.0_121'
+  MAVEN_VERSION='3.3.9'
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk$JAVA_VERSION.jdk/Contents/Home
   export M2_HOME="$HOME/Downloads/apache-maven-$MAVEN_VERSION"
   export M2="$M2_HOME/bin"
   export MAVEN_OPTS='-Xms256m -Xmx512m'
   export PATH=$M2_HOME/bin:$PATH
-  # http://stackoverflow.com/questions/3976342/running-swt-based-cross-platform-jar-properly-on-a-mac
-# only needed for SWT -  bus error and NPE with javaFx
-LAUNCH_OPTS='-XstartOnFirstThread'
+
+  # https://bugs.openjdk.java.net/browse/JDK-8167419
+  # Problematic frame:
+  # C  [libGL.dylib+0x1c3d]  glGetString+0x1c
+  # may need to install some of https://github.com/phracker/MacOSX-SDKs
+
 fi
 
 mvn -Dmaven.test.skip=true package install
-java $LAUNCH_OPTS -cp target/$PACKAGE_NAME-$PACKAGE_VERSION.jar:target/lib/* $MAIN_APP_PACKAGE.$MAIN_APP_CLASS
-mvn clean spring-boot:run
+echo "java -cp target/$APP_JAR:target/lib/* $MAIN_APP_PACKAGE.$MAIN_APP_CLASS"
+java -cp target/$APP_JAR:target/lib/* $MAIN_APP_PACKAGE.$MAIN_APP_CLASS
+# mvn clean spring-boot:run
