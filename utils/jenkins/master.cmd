c5e01c1e14cba3cc3b363f430ac5c15931cfdeb5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-04-29 12:37:39 -0400

    intermediate revision -  added explicit env for java 6,7,8

--- utils/jenkins/master.cmd
+++ utils/jenkins/master.cmd
@@ -1,11 +1,15 @@
 @echo OFF
+REM script to run the hpi package produced by mvn hpi:hpi command in a Windows based Jenkins instance
 pushd %~dp0
 set GIT_HOME=C:\Progra~1\GIT
 set JENKINS_HOME=%CD:\=/%
 set MASTER_HOST=127.0.0.1
 set MASTER_PORT=8080
 set GROOVY_VERSION=2.4.4
-set JAVA_VERSION=1.7.0_79
+set JAVA6_VERSION=1.6.0_45
+set JAVA7_VERSION=1.8.0_101
+set JAVA8_VERSION=1.7.0_79
+set JAVA_VERSION=%JAVA7_VERSION%
 set MAVEN_VERSION=3.3.9
 set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
 set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
