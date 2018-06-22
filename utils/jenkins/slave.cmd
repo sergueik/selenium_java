260121a9185435e98721bd46f5ac59ce13b69e67
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-22 14:32:10 -0400

    intermediate revision - detect the host ip to build jnlpUrl

--- utils/jenkins/slave.cmd
+++ utils/jenkins/slave.cmd
@@ -1,4 +1,6 @@
 @echo OFF
+setlocal enableextensions enabledelayedexpansion
+set DEBUG=false
 set HTTP_PORT=4444
 set HTTPS_PORT=-1
 set APP_VERSION=2.42.2
@@ -13,8 +15,15 @@ set MAVEN_OPTS=-Xms256m -Xmx512m
 PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin
 CHOICE /T 1 /C ync /CS /D y
 REM This setting needs adjustment.
-set MASTER_HOST=192.168.56.102
+REM set MASTER_HOST=192.168.56.102
+for /F "tokens=2 delims=:" %%. in ('ipconfig^| findstr /c:"IPv4 Address" ') do @set MASTER_HOST=%%.
 set MASTER_PORT=8080
+set MASTER_HOST=!MASTER_HOST: =!
+if /i "%DEBUG%"=="true" echo MASTER_HOST=!MASTER_HOST!
 set JENKINS_SLAVE_NAME=Windows_slave
-java.exe -jar slave.jar -jnlpUrl http://%MASTER_HOST%:%MASTER_PORT%/computer/%JENKINS_SLAVE_NAME%/slave-agent.jnlp
+set URL=http://!MASTER_HOST!:%MASTER_PORT%/computer/%JENKINS_SLAVE_NAME%/slave-agent.jnlp
+if /i "%DEBUG%"=="true" echo URL=%URL%
+echo ON
+call java.exe -jar slave.jar -jnlpUrl %URL%
+@echo OFF
 goto :EOF
