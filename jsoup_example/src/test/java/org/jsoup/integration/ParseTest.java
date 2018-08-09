ef7dc3d325e4f4f1f8421eaac77ec2f48e7d2f89
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-08-09 13:19:25 -0400

    intermediate revision - aded filtering by release

--- jsoup_example/src/test/java/org/jsoup/integration/ParseTest.java
+++ jsoup_example/src/test/java/org/jsoup/integration/ParseTest.java
@@ -35,7 +35,8 @@ import java.net.URL;
  */
 public class ParseTest {
 
-	private static final String filename = "/htmltests/edge_download_page.html";
+	private static final String buildNumber = "15063"; // "17134"
+	private static final String filename = "/edge_download_page.html";
 	private static String edgedriverDownloadURL = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
 	@Test
@@ -46,7 +47,7 @@ public class ParseTest {
 
 	@Test
 	public void edgeDownloadPageTest() throws IOException {
-		File in = getFile("/htmltests/edge_download_page.html");
+		File in = getFile(filename);
 		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
 		assertEquals("WebDriver - Microsoft Edge Development", doc.title());
 		assertEquals("en-US", doc.select("html").attr("lang"));
@@ -55,7 +56,7 @@ public class ParseTest {
 
 	@Test
 	public void edgeDownloadBasicLinksTest() throws IOException {
-		File in = getFile("/htmltests/edge_download_page.html");
+		File in = getFile(filename);
 		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
 
 		Elements downloadLinks = doc
@@ -66,9 +67,12 @@ public class ParseTest {
 		assertEquals(9, versionParagraphs.size());
 	}
 
+	// This test replicates what currently is done in
+	// https://github.com/bonigarcia/webdrivermanager
+	// src/main/java/io/github/bonigarcia/wdm/EdgeDriverManager.java
 	@Test
 	public void edgeDownloadExtendedLinksTest() throws IOException {
-		File in = getFile("/htmltests/edge_download_page.html");
+		File in = getFile(filename);
 		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
 
 		Elements downloadLinks = doc
@@ -81,54 +85,61 @@ public class ParseTest {
 		List<URL> urls = new ArrayList<>();
 		for (int cnt = 0; cnt < downloadLinks.size(); cnt++) {
 			String[] _versions = versionParagraphs.get(cnt).text().split(" ");
+			// NOTE: this is really muddy code!
 			String _version = _versions[1];
 			if (!_version.equalsIgnoreCase("version")) {
 				versions.add(_version);
 				String _url = downloadLinks.get(cnt).attr("href");
-				System.err.println(
-						String.format("Link %d, Version :%s URL: %s", cnt, _version, _url));
-				urls.add(new URL(_url));
+				String _text = downloadLinks.get(cnt).text();
+				if (_text.contains(String.format("Release %s", buildNumber))) { // whitespace-fragile
+					System.err
+							.println(String.format("Link %d|Version :%s|URL: %s|TEXT: %s",
+									cnt, _version, _url, _text));
+					urls.add(new URL(_url));
+
+				}
+				if (_text.contains(buildNumber)) {
+					if (!urls.contains(new URL(_url))) {
+						System.err
+								.println(String.format("Link %d|Version :%s|URL: %s|TEXT: %s",
+										cnt, _version, _url, _text));
+						urls.add(new URL(_url));
+					}
+
+				}
 			}
 		}
-		assertEquals(6, urls.size());
+		assertEquals(6, versions.size());
+		assertEquals(1, urls.size());
 	}
 
-	@Ignore
+	// @Ignore
 	@Test
-	public void testNewsHomepage() throws IOException {
-		File in = getFile("/htmltests/edge_download_page.html");
-		Document doc = Jsoup.parse(in, "UTF-8", "http://www.news.com.au/");
-		assertEquals(
-				"News.com.au | News from Australia and around the world online | NewsComAu",
-				doc.title());
-		assertEquals("Brace yourself for Metro meltdown",
-				doc.select(".id1225817868581 h4").text().trim());
+	public void edgeDownloadLinkFilterTest() throws IOException {
+		File in = getFile(filename);
+		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
 
-		Element a = doc.select("a[href=/entertainment/horoscopes]").first();
-		assertEquals("/entertainment/horoscopes", a.attr("href"));
-		assertEquals("http://www.news.com.au/entertainment/horoscopes",
-				a.attr("abs:href"));
-
-		Element hs = doc.select("a[href*=naughty-corners-are-a-bad-idea]").first();
-		assertEquals(
-				"http://www.heraldsun.com.au/news/naughty-corners-are-a-bad-idea-for-kids/story-e6frf7jo-1225817899003",
-				hs.attr("href"));
-		assertEquals(hs.attr("href"), hs.attr("abs:href"));
-		assertEquals("ipod - Google Search", doc.title());
-		Elements results = doc.select("h3.r > a");
-		assertEquals(12, results.size());
-		assertEquals(
-				"http://news.google.com/news?hl=en&q=ipod&um=1&ie=UTF-8&ei=uYlKS4SbBoGg6gPf-5XXCw&sa=X&oi=news_group&ct=title&resnum=1&ved=0CCIQsQQwAA",
-				results.get(0).attr("href"));
-		assertEquals("http://www.apple.com/itunes/", results.get(1).attr("href"));
-		Element p = doc.select("p:contains(Volt will be sold in the United States")
-				.first();
-		assertEquals(
-				"In July, GM said its electric Chevrolet Volt will be sold in the United States at $41,000 -- $8,000 more than its nearest competitor, the Nissan Leaf.",
-				p.text());
+		Elements downloadLinks = doc
+				.select("ul.driver-downloads li.driver-download > a");
+		assertEquals(6, downloadLinks.size());
+		for (int cnt = 0; cnt < downloadLinks.size(); cnt++) {
+			System.err.println(String.format("# %d|TEXT: %s|URL: %s", cnt,
+					downloadLinks.get(cnt).text(), downloadLinks.get(cnt).attr("href")));
+		}
+		Elements downloadFilteredLinks = doc.select(
+				String.format("ul.driver-downloads li.driver-download > a:contains(%s)",
+						buildNumber));
+		assertEquals(1, downloadFilteredLinks.size());
+		System.err.println(String.format("Filtered|TEXT: %s|URL: %s",
+				downloadFilteredLinks.get(0).text(),
+				downloadFilteredLinks.get(0).attr("href")));
+		Elements versionParagraphs = doc.select(String.format(
+				"ul.driver-downloads li.driver-download p:contains(%s)", buildNumber));
+		assertEquals(1, versionParagraphs.size());
+		System.err.println("Element HTML:" + versionParagraphs.get(0).hasText());
 	}
 
-	File getFile(String resourceName) {
+	public File getFile(String resourceName) {
 		try {
 			File file = new File(ParseTest.class.getResource(resourceName).toURI());
 			return file;
