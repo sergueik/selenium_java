918bf6111331b1e3df5ce7e07c976a6ea08e444d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-08-11 01:11:48 -0400

    intermediate revision - removed unneded test

--- jsoup_example/src/test/java/org/jsoup/integration/ParseTest.java
+++ jsoup_example/src/test/java/org/jsoup/integration/ParseTest.java
@@ -1,6 +1,7 @@
 package org.jsoup.integration;
 
 import org.jsoup.Jsoup;
+import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -13,19 +14,13 @@ import org.jsoup.UnsupportedMimeTypeException;
 import org.junit.Test;
 import org.junit.Ignore;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.Jsoup;
-import org.jsoup.Connection;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -40,15 +35,15 @@ public class ParseTest {
 	private static String edgedriverDownloadURL = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
 	@Test
-	public void fetchURl() throws IOException {
+	public void fetchURL() throws IOException {
 		Document doc = Jsoup.parse(new URL(edgedriverDownloadURL), 10 * 1000);
 		assertTrue(doc.title().contains("WebDriver - Microsoft Edge Development"));
 	}
 
 	@Test
 	public void edgeDownloadPageTest() throws IOException {
-		File in = getFile(filename);
-		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
+		File file = getFile(filename);
+		Document doc = Jsoup.parse(file, "UTF-8", edgedriverDownloadURL);
 		assertEquals("WebDriver - Microsoft Edge Development", doc.title());
 		assertEquals("en-US", doc.select("html").attr("lang"));
 
@@ -56,8 +51,8 @@ public class ParseTest {
 
 	@Test
 	public void edgeDownloadBasicLinksTest() throws IOException {
-		File in = getFile(filename);
-		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
+		File file = getFile(filename);
+		Document doc = Jsoup.parse(file, "UTF-8", edgedriverDownloadURL);
 
 		Elements downloadLinks = doc
 				.select("ul.driver-downloads li.driver-download > a");
@@ -67,72 +62,30 @@ public class ParseTest {
 		assertEquals(9, versionParagraphs.size());
 	}
 
-	// This test replicates what currently is done in
-	// https://github.com/bonigarcia/webdrivermanager
-	// src/main/java/io/github/bonigarcia/wdm/EdgeDriverManager.java
-	@Test
-	public void edgeDownloadExtendedLinksTest() throws IOException {
-		File in = getFile(filename);
-		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
-
-		Elements downloadLinks = doc
-				.select("ul.driver-downloads li.driver-download > a");
-		assertEquals(6, downloadLinks.size());
-		Elements versionParagraphs = doc.select(
-				"ul.driver-downloads li.driver-download p.driver-download__meta");
-		assertEquals(9, versionParagraphs.size());
-		List<String> versions = new ArrayList<>();
-		List<URL> urls = new ArrayList<>();
-		for (int cnt = 0; cnt < downloadLinks.size(); cnt++) {
-			String[] _versions = versionParagraphs.get(cnt).text().split(" ");
-			// NOTE: this is really muddy code!
-			String _version = _versions[1];
-			if (!_version.equalsIgnoreCase("version")) {
-				versions.add(_version);
-				String _url = downloadLinks.get(cnt).attr("href");
-				String _text = downloadLinks.get(cnt).text();
-				if (_text.contains(String.format("Release %s", buildNumber))) { // whitespace-fragile
-					System.err
-							.println(String.format("Link %d|Version :%s|URL: %s|TEXT: %s",
-									cnt, _version, _url, _text));
-					urls.add(new URL(_url));
-
-				}
-				if (_text.contains(buildNumber)) {
-					if (!urls.contains(new URL(_url))) {
-						System.err
-								.println(String.format("Link %d|Version :%s|URL: %s|TEXT: %s",
-										cnt, _version, _url, _text));
-						urls.add(new URL(_url));
-					}
-
-				}
-			}
-		}
-		assertEquals(6, versions.size());
-		assertEquals(1, urls.size());
-	}
-
-	// @Ignore
 	@Test
 	public void edgeDownloadLinkFilterTest() throws IOException {
-		File in = getFile(filename);
-		Document doc = Jsoup.parse(in, "UTF-8", edgedriverDownloadURL);
+		File file = getFile(filename);
+		Document doc = Jsoup.parse(file, "UTF-8", edgedriverDownloadURL);
 
-		Elements downloadLinks = doc
+		org.jsoup.select.Elements downloadLinks = doc
 				.select("ul.driver-downloads li.driver-download > a");
 		assertEquals(6, downloadLinks.size());
 		for (int cnt = 0; cnt < downloadLinks.size(); cnt++) {
+			org.jsoup.nodes.Element downloadLink = downloadLinks.get(cnt);
 			System.err.println(String.format("# %d|TEXT: %s|URL: %s", cnt,
-					downloadLinks.get(cnt).text(), downloadLinks.get(cnt).attr("href")));
+					downloadLink.text(), downloadLink.attr("href")));
 		}
-		Elements downloadFilteredLinks = doc.select(
+		org.jsoup.select.Elements downloadFilteredLinks = doc.select(
 				String.format("ul.driver-downloads li.driver-download > a:contains(%s)",
 						buildNumber));
 		assertEquals(1, downloadFilteredLinks.size());
 		System.err.println(String.format("Filtered|TEXT: %s|URL: %s",
 				downloadFilteredLinks.get(0).text(),
 				downloadFilteredLinks.get(0).attr("href")));
+
+		// The following code replicates what currently is done in
+		// https://github.com/bonigarcia/webdrivermanager
+		// src/main/java/io/github/bonigarcia/wdm/EdgeDriverManager.java
 		Elements versionParagraphs = doc.select(String.format(
 				"ul.driver-downloads li.driver-download p:contains(%s)", buildNumber));
 		assertEquals(1, versionParagraphs.size());
