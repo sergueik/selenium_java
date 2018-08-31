dfe000aa5253ada7d9503a8018f54dce9b4ad369
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-08-31 15:44:19 -0400

    minor improvement of and added uniform notation in the launcher scripts

--- javafx_example/run.sh
+++ javafx_example/run.sh
@@ -20,9 +20,15 @@ if [  $? -eq  0 ] ; then
   APP_VERSION=$(xmllint -xpath "/*[local-name() = 'project' ]/*[local-name() = 'version' ]/text()" pom.xml)
   APP_PACKAGE=$(xmllint -xpath "/*[local-name() = 'project' ]/*[local-name() = 'groupId' ]/text()" pom.xml)
   APP_NAME=$(xmllint -xpath "/*[local-name() = 'project' ]/*[local-name() = 'artifactId' ]/text()" pom.xml)
-  DEFAULT_MAIN_APP_CLASS=$(xmllint -xpath "/*[local-name() = 'project' ]/*[local-name() = 'properties' ]/*[local-name() = 'mainClass']/text()" pom.xml)
+  MAIN_APP_CLASS=$(xmllint -xpath "/*[local-name() = 'project' ]/*[local-name() = 'properties' ]/*[local-name() = 'mainClass']/text()" pom.xml)
 fi
 
+# echo "MAIN_APP_CLASS=${MAIN_APP_CLASS}" 1>& 2
+# echo "APP_PACKAGE=${APP_PACKAGE}" 1>& 2
+# echo "APP_VERSION=${APP_VERSION}" 1>& 2
+# echo "APP_NAME=${APP_NAME}" 1>& 2
+
+for APP_CONFIG_ENTRY in 'MAIN_APP_CLASS' 'APP_PACKAGE' 'APP_VERSION' 'APP_NAME'; do echo "${APP_CONFIG_ENTRY}=$(eval echo \$$APP_CONFIG_ENTRY )" 1>& 2 ; done
 
 if $(uname -s | grep -qi Darwin)
 then
