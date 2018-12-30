12847b14e1ecf35905b53baf8c9ade1a1b544c94
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-12-30 08:58:59 -0500

    cosmetic changes

--- jodl_stw/src/main/java/example/CanvasEx.java
+++ jodl_stw/src/main/java/example/CanvasEx.java
@@ -1,23 +1,20 @@
 package example;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2ES2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLCapabilitiesChooser;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
-import com.jogamp.opengl.GLCapabilitiesImmutable;
-import com.jogamp.opengl.GLCapabilitiesChooser;
-
-import com.jogamp.newt.opengl.GLWindow;
-import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.swt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
 
 // based on: http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/DemonstratesaCanvas.htm
 // https://github.com/sgothel/jogl-demos/blob/master/maven/jp4da/jp4da-desktop/src/main/java/com/io7m/examples/jp4da/DesktopExample.java
@@ -25,10 +22,12 @@ import com.jogamp.opengl.swt.GLCanvas;
 public class CanvasEx {
 
 	private FPSAnimator animator;
+	@SuppressWarnings("unused")
 	private Shell shell;
 	private GLCanvas glCanvas;
 
 	public void run() {
+
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setText("Animator Example");
@@ -40,7 +39,7 @@ public class CanvasEx {
 				display.sleep();
 			}
 		}
-		this.animator.stop();
+		animator.stop();
 		display.dispose();
 	}
 
@@ -49,42 +48,33 @@ public class CanvasEx {
 		shell.setLayout(new FillLayout());
 		animator = new FPSAnimator(60);
 		animator.setUpdateFPSFrames(60, System.err);
+
+		@SuppressWarnings("unused")
 		Canvas canvas = new Canvas(shell, SWT.NONE);
 		final GLProfile pro = GLProfile.get(GLProfile.GL2GL3);
 		final GLCapabilities cap = new GLCapabilities(pro);
-		glCanvas = new GLCanvas(shell /* does not show when on a canvas */, 0, cap,
-				(GLCapabilitiesChooser) null);
+		glCanvas = new GLCanvas(shell, 0, cap, (GLCapabilitiesChooser) null);
 		glCanvas.addGLEventListener(new GLEventListener() {
 			@Override
 			public void display(final GLAutoDrawable drawable) {
 				final GL2ES2 gl = drawable.getGL().getGL2ES2();
-
-				// will spam a lot
-				// System.err.println("display");
 				gl.glClearColor(1.0f, 0f, 1.0f, 1f);
 				gl.glClear(GL.GL_COLOR_BUFFER_BIT);
 			}
 
 			@Override
-			public void dispose(
-					@SuppressWarnings("unused") final GLAutoDrawable drawable) {
+			public void dispose(final GLAutoDrawable drawable) {
 				System.err.println("dispose");
 			}
 
 			@Override
-			public void init(
-					@SuppressWarnings("unused") final GLAutoDrawable drawable) {
+			public void init(final GLAutoDrawable drawable) {
 				System.err.println("init");
 			}
 
 			@Override
-			public void reshape(
-					@SuppressWarnings("unused") final GLAutoDrawable drawable,
-					@SuppressWarnings("unused") final int x,
-					@SuppressWarnings("unused") final int y,
-					@SuppressWarnings("unused") final int w,
-					@SuppressWarnings("unused") final int h) {
-				// No op
+			public void reshape(final GLAutoDrawable drawable, final int x,
+					final int y, final int w, final int h) {
 			}
 		});
 		animator.add(glCanvas);
--- jodl_stw/src/main/java/example/OwnWindowEx.java
+++ jodl_stw/src/main/java/example/OwnWindowEx.java
@@ -1,65 +1,64 @@
 package example;
 
+import com.jogamp.newt.opengl.GLWindow;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2ES2;
+
+import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
 
-import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.util.FPSAnimator;
 
-import com.jogamp.opengl.GL;
-import com.jogamp.opengl.GL2ES2;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
-public class OwnWindowEx implements Runnable
-{
-  public static void main(
-    final String args[])
-  {
-    final OwnWindowEx de = new OwnWindowEx(new Example());
-    de.run();
-  }
+// based on: https://github.com/sgothel/jogl-demos/blob/master/maven/jp4da/jp4da-desktop/src/main/java/com/io7m/examples/jp4da/DesktopExample.java
+// and https://github.com/sgothel/jogl-demos/blob/master/maven/jp4da/jp4da-core/src/main/java/com/io7m/examples/jp4da/Example.java
 
-  private final GLWindow    window;
-  private final FPSAnimator animator;
+public class OwnWindowEx implements Runnable {
 
-  private OwnWindowEx(
-    final GLEventListener gle)
-  {
-    final GLProfile pro = GLProfile.get(GLProfile.GL2GL3);
-    final GLCapabilities cap = new GLCapabilities(pro);
+	private final GLWindow topWindow;
+	private final FPSAnimator animator;
+	private static final String profile = GLProfile.GL2GL3;
 
-    this.window = GLWindow.create(cap);
-    this.window.setSize(640, 480);
-    this.window.setTitle("Test1");
-    this.window.addGLEventListener(gle);
-    this.window.setVisible(true);
+	public static void main(final String args[]) {
+		(new OwnWindowEx(new ExampleGLEventListener())).run();
+	}
 
-    this.animator = new FPSAnimator(60);
-    this.animator.setUpdateFPSFrames(60, System.err);
-    this.animator.add(this.window);
-    this.animator.start();
-  }
+	private OwnWindowEx(final GLEventListener gle) {
+		final GLCapabilities cap = new GLCapabilities(GLProfile.get(profile));
 
-  @Override public void run()
-  {
-    try {
-      while (this.animator.isAnimating() && this.window.isVisible()) {
-        Thread.sleep(100);
-      }
+		topWindow = GLWindow.create(cap);
+		topWindow.setSize(640, 480);
+		topWindow.setTitle("Test1");
+		topWindow.addGLEventListener(gle);
+		topWindow.setVisible(true);
 
-      this.animator.stop();
-      this.window.destroy();
+		animator = new FPSAnimator(60);
+		animator.setUpdateFPSFrames(60, System.err);
+		animator.add(topWindow);
+		animator.start();
+	}
 
-      System.err.println("Exiting...");
-    } catch (final InterruptedException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
+	@Override
+	public void run() {
+		try {
+			while (animator.isAnimating() && topWindow.isVisible()) {
+				Thread.sleep(100);
+			}
 
-private static class Example implements GLEventListener
-{
+			animator.stop();
+			topWindow.destroy();
+
+			System.err.println("Exiting...");
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private static class ExampleGLEventListener implements GLEventListener {
+
 		@Override
 		public void display(final GLAutoDrawable drawable) {
 			final GL2ES2 gl = drawable.getGL().getGL2ES2();
@@ -69,25 +68,16 @@ private static class Example implements GLEventListener
 		}
 
 		@Override
-		public void dispose(
-				@SuppressWarnings("unused") final GLAutoDrawable drawable) {
-			// Nothing.
+		public void dispose(final GLAutoDrawable drawable) {
 		}
 
 		@Override
-		public void init(
-				@SuppressWarnings("unused") final GLAutoDrawable drawable) {
-			// Nothing.
+		public void init(final GLAutoDrawable drawable) {
 		}
 
 		@Override
-		public void reshape(
-				@SuppressWarnings("unused") final GLAutoDrawable drawable,
-				@SuppressWarnings("unused") final int x,
-				@SuppressWarnings("unused") final int y,
-				@SuppressWarnings("unused") final int w,
-				@SuppressWarnings("unused") final int h) {
-			// Nothing.
+		public void reshape(final GLAutoDrawable drawable, final int x, final int y,
+				final int w, final int h) {
 		}
-}
+	}
 }
