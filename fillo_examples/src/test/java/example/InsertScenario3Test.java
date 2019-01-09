8caf2b1c8eb7bc965ca22f9996f68c2388def699
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-09 00:23:35 -0500

    intermediate revision - added column extend and write data example

--- fillo_examples/src/test/java/example/InsertScenario3Test.java
+++ fillo_examples/src/test/java/example/InsertScenario3Test.java
@@ -9,8 +9,10 @@ import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
 import com.codoid.products.fillo.Recordset;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -18,6 +20,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellReference;
@@ -33,37 +36,40 @@ public class InsertScenario3Test {
 	private static String query = null;
 	private static final String filePath = System.getProperty("user.dir")
 			+ "/src/test/resources/TestData.xlsx";
-	private static final String newFilePath = System.getProperty("user.dir")
-			+ "/src/test/resources/TestData.new.xlsx";
+	private static final String saveFilePath = System.getProperty("user.dir")
+			+ "/src/test/resources/TestData.BACKUP.xlsx";
+	private static List<String> columnHeaders = new ArrayList<>();
+	private static String newColumn = null;
 
 	@BeforeClass
 	public void beforeTest() throws FilloException {
-		connection = fillo.getConnection(filePath);
+		columnHeaders = readColumnHeaders();
+		newColumn = String.format("Column%d", columnHeaders.size());
+		appendColumnHeader(newColumn);
+		columnHeaders = readColumnHeaders();
+		System.err.println("Appnded new column before query test");
 	}
 
-	@Test
-	public void insertExampleTest() throws FilloException, IOException {
-		List<String> tableNames = connection.getMetaData().getTableNames();
-		for (String tableName : tableNames) {
-			System.err.println("Table: " + tableName);
-		}
-		List<Object[]> result = new LinkedList<>();
+	private List<String> readColumnHeaders() {
+
 		Map<String, String> columns = new HashMap<>();
-		FileOutputStream fileOut = new FileOutputStream(newFilePath);
-		XSSFWorkbook workBook = new XSSFWorkbook(filePath);
-
+		XSSFWorkbook workBook = null;
+		try {
+			workBook = new XSSFWorkbook(filePath);
+		} catch (IOException e) {
+		}
+		if (workBook == null) {
+			return new ArrayList<>();
+		}
 		XSSFSheet sheet = workBook.getSheetAt(0);
-
 		Iterator<Row> rows = sheet.rowIterator();
-		Iterator<org.apache.poi.ss.usermodel.Cell> cells;
 		while (rows.hasNext()) {
 			XSSFRow row = (XSSFRow) rows.next();
-			XSSFCell cell;
 			if (row.getRowNum() == 0) {
-				cells = row.cellIterator();
+				Iterator<org.apache.poi.ss.usermodel.Cell> cells = row.cellIterator();
 				while (cells.hasNext()) {
 
-					cell = (XSSFCell) cells.next();
+					XSSFCell cell = (XSSFCell) cells.next();
 					int columnIndex = cell.getColumnIndex();
 					String columnHeader = cell.getStringCellValue();
 					String columnName = CellReference
@@ -72,40 +78,74 @@ public class InsertScenario3Test {
 					System.err
 							.println(columnIndex + "[" + columnName + "]: " + columnHeader);
 				}
-
-				List<String> headers = Arrays
-						.asList(new String[] { "Column1", "Column2", "Column3" });
-				for (int col = 0; col < headers.size(); col++) {
-					cell = row.createCell(col + 1);
-					cell.setCellValue(headers.get(col));
-					System.err
-							.println("Writing " + row + " " + col + "  " + headers.get(col));
-				}
-				// java.io.IOException: Cannot write data, document seems to have been
-				// closed already
-				// workBook.close();
-
-				// attempting to read and write the same file
-				// org.apache.poi.EmptyFileException:
-				// The supplied file was empty (zero bytes long)
-
-				workBook.write(fileOut);
-				workBook.close();
-				fileOut.flush();
-				fileOut.close();
-
 			}
 		}
-		connection.close();
+		try {
+			workBook.close();
+			return new ArrayList<String>(columns.keySet());
+		} catch (IOException e) {
+			return new ArrayList<>();
+		}
+	}
+
+	private void appendColumnHeader(String columnHeader) {
+		XSSFWorkbook workBook = null;
+		try {
+			workBook = new XSSFWorkbook(filePath);
+		} catch (IOException e) {
+		}
+		if (workBook == null) {
+			return;
+		}
+		XSSFSheet sheet = workBook.getSheetAt(0);
+		Iterator<Row> rows = sheet.rowIterator();
+		Iterator<org.apache.poi.ss.usermodel.Cell> cells;
+		while (rows.hasNext()) {
+			XSSFRow row = (XSSFRow) rows.next();
+			if (row.getRowNum() == 0) {
+				XSSFCell cell = row.createCell(columnHeaders.size());
+				cell.setCellValue(columnHeader);
+				System.err.println("Adding column # " + (columnHeaders.size())
+						+ " with the name: " + columnHeader);
+				cells = row.cellIterator();
+			}
+		}
+		try {
+			// TODO: saving properly, without dummy temp file
+
+			FileOutputStream fileOut = new FileOutputStream(saveFilePath);
+			workBook.write(fileOut);
+			fileOut.flush();
+			fileOut.close();
+
+			workBook.close();
+		} catch (IOException e) {
+		}
+		File file = new File(saveFilePath);
+
+		if (file.delete()) {
+			System.out.println("File " + saveFilePath + " deleted successfully");
+		} else {
+			System.out.println("Failed to delete the file " + saveFilePath);
+		}
+	}
+
+	@Test
+	public void insertExampleTest() throws FilloException {
+
 		connection = fillo.getConnection(filePath);
-		// query = "insert into States(MethodName,Column1)
-		// values('anotherMethod','1')";
+		List<String> tableNames = connection.getMetaData().getTableNames();
+		for (String tableName : tableNames) {
+			System.err.println("Table: " + tableName);
+		}
+		query = String.format("update States set %s='true' where TestMethod='someTest'",
+				newColumn);
+		System.err.println("Runnin	query: " + query);
 		connection.executeUpdate(query);
-		// when bad column name:
-		// currently corrupting the "TestData.new.xlsx"
-		// the data could not be loaded copletely because the maximum number of rows
-		// per sheet was exceeded
-		query = "insert into States(TestMethod,Column1,Column2,Column3) values('anotherMethod','1','1','1')";
-		connection.executeUpdate(query);
+		// When column name is "bad":
+		// fillo may be corrupting the spreadsheet:
+		// attempt to open the resulting file "TestData.new.xlsx" shows dialog:
+		// the data could not be loaded completely because
+		// the maximum number of rows per sheet was exceeded
 	}
 }
