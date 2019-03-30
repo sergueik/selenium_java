082e9199ed649c554e6534b926194a24ba9ef528
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-03-30 19:40:38 -0400

    intermediate revosion - fixed vulenaraility in jetty version and updated launcher test script to actually do launch it

--- IDE/utils/java/test.cmd
+++ IDE/utils/java/test.cmd
@@ -3,9 +3,10 @@ REM works
 call mvn clean package install
 set TARGET=%CD%\target
 set MAIN_APP_CLASS=com.mycompany.app.App
-set SELENIUM_VERSION=2.44.0
+set SELENIUM_VERSION=2.53.0
+set APP_VERSION=0.0.3-SNAPSHOT
 REM adding selenium-standalone to the CLASSPATH is not necessary.
-REM java -cp %TARGET%\app-1.1-SNAPSHOT.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS%
-java -cp %TARGET%\app-1.1-SNAPSHOT.jar;%TARGET%\lib\*  %MAIN_APP_CLASS%
+REM java -cp %TARGET%\app-%APP_VERSION%.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS%
+java -cp %TARGET%\app-%APP_VERSION%.jar;%TARGET%\lib\*  %MAIN_APP_CLASS%
 
 goto :EOF
