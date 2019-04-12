37c4f515bac31c0ee67b42d78c29705b47723c59
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-04-12 18:34:41 -0400

    intermediate revision - added "SwingUtilities.invokeLater" = "Display.asyncExec" to fix race condition

---
+++ swing/run.cmd
@@ -0,0 +1,133 @@
+@echo OFF
+SETLOCAL
+
+REM NOTE: Cannot set SETLOCAL ENABLEDELAYEDEXPANSION
+REM Because of inline ie script in :CALL_JAVASCRIPT
+
+if "%TOOLS_DIR%"=="" set TOOLS_DIR=c:\java
+if "%JAVA_VERSION%"=="" set JAVA_VERSION=1.8.0_101
+if "%JAVA_HOME%"=="" set JAVA_HOME=%TOOLS_DIR%\jdk%JAVA_VERSION%
+set JAVA_OPTS=-Xms256m -Xmx512m
+
+if "%MAVEN_VERSION%"=="" set MAVEN_VERSION=3.5.0
+if "%M2_HOME%"=="" set M2_HOME=%TOOLS_DIR%\apache-maven-%MAVEN_VERSION%
+if "%M2%"=="" set M2=%M2_HOME%\bin
+
+set MAVEN_OPTS=-Xms256m -Xmx512m
+REM TODO: monochrome
+set TERM=
+
+PATH=%JAVA_HOME%\bin;%M2%;%PATH%
+set TARGET=%CD%\target
+set DEBUG=false
+set SKIP_TEST=tue
+set SKIP_PACKAGE_VERSION=false
+
+REM this currently is not loaded from pom.xml
+set DEFAULT_MAIN_CLASS=SwetMainFlowPanel
+REM These get extracted from the pom.xml, keeping the default values for backwad compatibility
+set APP_NAME=swet_javafx_app
+set APP_PACKAGE=com.github.sergueik.swet_javafx
+set APP_VERSION=0.0.2-SNAPSHOT
+
+set APP_JAR=%APP_NAME%.jar
+
+if /i "%SKIP_PACKAGE_VERSION%"=="true" goto :SKIP_PACKAGE_VERSION
+
+call :CALL_JAVASCRIPT groupId
+set APP_PACKAGE=%VALUE%
+
+call :CALL_JAVASCRIPT artifactId
+set APP_NAME=%VALUE%
+
+call :CALL_JAVASCRIPT version
+set APP_VERSION=%VALUE%
+
+REM Cannot get the mainClass currently ?
+set APP_JAR=%APP_NAME%-%APP_VERSION%.jar
+
+:SKIP_PACKAGE_VERSION
+
+if /i NOT "%VERBOSE%"=="true" goto :CONTINUE
+
+call :SHOW_VARIABLE APP_VERSION
+call :SHOW_VARIABLE APP_NAME
+call :SHOW_VARIABLE APP_PACKAGE
+call :SHOW_VARIABLE APP_JAR
+
+:CONTINUE
+
+set MAIN_CLASS=%1
+if NOT "%MAIN_CLASS%" == "" shift
+if "%MAIN_CLASS%"=="" set MAIN_CLASS=%DEFAULT_MAIN_CLASS%
+set APP_HOME=%CD:\=/%
+
+REM omit the extension - varies with Windows releases to be mvn.bat or mvn.cmd
+
+if "%SKIP_TEST%"=="" (
+REM Test
+call mvn test
+REM Compile
+call mvn package install
+) else (
+REM compile
+call mvn -Dmaven.test.skip=true package install
+)
+
+REM Run
+REM NOTE: shift does not modify %*
+REM The log4j configuration argument seems to be ignored
+REM -Dlog4j.configuration=file:///%APP_HOME%/src/main/resources/log4j.properties ^
+set COMMAND=^
+java ^
+  -cp %TARGET%\%APP_JAR%;%TARGET%\lib\* ^
+  %APP_PACKAGE%.%MAIN_CLASS% ^
+  %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo %COMMAND%>&2
+%COMMAND%
+ENDLOCAL
+exit /b
+
+:CALL_JAVASCRIPT
+
+set "SCRIPT=mshta.exe "javascript:{"
+set "SCRIPT=%SCRIPT% var fso = new ActiveXObject('Scripting.FileSystemObject');"
+set "SCRIPT=%SCRIPT% var out = fso.GetStandardStream(1);"
+set "SCRIPT=%SCRIPT% var handle = fso.OpenTextFile('pom.xml',1,1);"
+set "SCRIPT=%SCRIPT% var xml = new ActiveXObject('Msxml2.DOMDocument.6.0');"
+set "SCRIPT=%SCRIPT% xml.async = false;"
+set "SCRIPT=%SCRIPT% xml.loadXML(handle.ReadAll());"
+set "SCRIPT=%SCRIPT% root = xml.documentElement;"
+set "SCRIPT=%SCRIPT% var tag ='%~1';"
+set "SCRIPT=%SCRIPT% nodes = root.childNodes;"
+set "SCRIPT=%SCRIPT% for(i = 0; i != nodes .length; i++){"
+set "SCRIPT=%SCRIPT%   if (nodes.item(i).nodeName.match(RegExp(tag, 'g'))) {"
+set "SCRIPT=%SCRIPT%     out.Write(tag + '=' + nodes.item(i).text + '\n');"
+set "SCRIPT=%SCRIPT%   }"
+set "SCRIPT=%SCRIPT% }"
+set "SCRIPT=%SCRIPT%close();}""
+
+REM if /i "%DEBUG%"=="true" echo %SCRIPT%
+REM if /i "%DEBUG%"=="true" for /F "delims=" %%_ in ('%SCRIPT% 1 ^| more') do echo %%_
+
+for /F "tokens=2 delims==" %%_ in ('%SCRIPT% 1 ^| more') do set VALUE=%%_
+ENDLOCAL
+exit /b
+
+
+:SHOW_VARIABLE
+SETLOCAL ENABLEDELAYEDEXPANSION
+set VAR=%1
+if /i "%DEBUG%"=="true" echo>&2 VAR=!VAR!
+set RESULT=!VAR!
+call :SHOW_VARIABLE_VALUE !%VAR%!
+set RESULT=!RESULT!="!DATA!"
+echo>&2 !RESULT!
+ENDLOCAL
+goto :EOF
+
+:SHOW_VARIABLE_VALUE
+set VAL=%1
+set DATA=%VAL%
+if /i "%DEBUG%"=="true" echo>&2 VALUE=%VAL%
+goto :EOF
--- swing/src/main/java/example/SwingButtonTextChangeEx.java
+++ swing/src/main/java/example/SwingButtonTextChangeEx.java
@@ -2,29 +2,23 @@ package example;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-// import javax.swing.SwingUtilities;
-import static javax.swing.SwingUtilities.isMiddleMouseButton;
-import static javax.swing.SwingUtilities.isLeftMouseButton;
-import static javax.swing.SwingUtilities.isRightMouseButton;
-
-import static javax.swing.SwingUtilities.invokeLater;
+// based on:
+// see also http://www.kdgregory.com/index.php?page=swing.async
+// http://www.java2s.com/Code/Java/Swing-JFC/ButtonActionSample.htm
+// http://www.java2s.com/Tutorials/Java/Swing_How_to/Thread/Shake_Button_text_in_animation.htm
 
 // based on: http://www.java2s.com/Code/Java/Swing-JFC/ButtonActionSample.htm
 // http://www.java2s.com/Tutorials/Java/Swing_How_to/Thread/Shake_Button_text_in_animation.htm
 
-@SuppressWarnings({ "unused", "serial" })
+@SuppressWarnings({ "serial" })
 public class SwingButtonTextChangeEx extends JPanel {
 
 	private static JButton button;
@@ -37,74 +31,48 @@ public class SwingButtonTextChangeEx extends JPanel {
 
 		ActionListener actionListener = new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
-				// System.err.println("I was selected.");
-			}
-		};
+				System.err.println("Counting down from: " + total);
 
-		MouseListener mouseListener = new MouseAdapter() {
-			public void mousePressed(MouseEvent mouseEvent) {
-				int modifiers = mouseEvent.getModifiers();
-				if ((modifiers & InputEvent.BUTTON1_MASK) == InputEvent.BUTTON1_MASK) {
-					System.err.println("Left button pressed.");
-				}
+				// NOTE: when loop is moved outside of the runnable, the
+				// SwingUtilities.invokeLater displays only the last update of
+				// the button text:
+				// invokeLater(new Runnable() { button.setText(buttonText); });
+				Runnable buttonAnimationRunnable = new Runnable() {
+					@Override
+					public void run() {
+						final int progress = total;
+						for (int current = 0; current != progress; current++) {
+							String buttonText = String.format("Close in %d sec",
+									(progress - current));
 
-				if ((modifiers & InputEvent.BUTTON2_MASK) == InputEvent.BUTTON2_MASK) {
-					System.err.println("Middle button pressed.");
-				}
-				if ((modifiers & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK) {
-					System.err.println("Right button pressed.");
-				}
-
-			}
-
-			public void mouseReleased(MouseEvent mouseEvent) {
-				if (isLeftMouseButton(mouseEvent)) {
-					System.err.println("Left button released.");
-					System.err.println("Counting down from: " + total);
-
-					// NOTE: SwingUtilities.invokeLater displays only the last update of
-					// the button text:
-					// invokeLater(new Runnable() { button.setText(buttonText); });
-					Runnable buttonAnimationRunnable = new Runnable() {
-						@Override
-						public void run() {
-							final int progress = total;
-							for (int current = progress; current > 0; current--) {
-								String buttonText = String.format("Calculated %d of %d",
-										current, progress);
-								button.setVisible(false);
-								button.setText(buttonText);
-								button.doLayout();
-								button.setVisible(true);
-								System.err.println("Set button text to: " + buttonText);
-								try {
-									Thread.sleep(300);
-								} catch (InterruptedException e) {
-									System.err.println("Exception (ignored): " + e.toString());
+							SwingUtilities.invokeLater(new Runnable() {
+								public void run() {
+									button.setVisible(false);
+									button.setText(buttonText);
+									button.doLayout();
+									button.setVisible(true);
 								}
+							});
+							System.err.println("Set button text to: " + buttonText);
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e) {
+								System.err.println("Exception (ignored): " + e.toString());
 							}
 						}
-					};
-					Thread buttonAnimationThread = new Thread(buttonAnimationRunnable);
-					buttonAnimationThread.start();
-				}
-				if (isMiddleMouseButton(mouseEvent)) {
-					System.err.println("Middle button released.");
-				}
-				if (isRightMouseButton(mouseEvent)) {
-					System.err.println("Right button released.");
-				}
-
-				// indicate unmet progress
-				// System.err.println();
+						System.err.println("Closing.");
+						frame.dispose();
+					}
+				};
+				Thread buttonAnimationThread = new Thread(buttonAnimationRunnable);
+				buttonAnimationThread.start();
 			}
 		};
 
 		button.addActionListener(actionListener);
-		button.addMouseListener(mouseListener);
 
 		Container contentPane = frame.getContentPane();
-		contentPane.add(button, BorderLayout.SOUTH);
+		contentPane.add(button, BorderLayout.CENTER);
 		frame.setSize(300, 100);
 		frame.setVisible(true);
 	}
