61df060a8c83f04ed0e6774f515d68688f94a932
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-08 12:23:30 -0400

    intermediate revision of quick script to shrink video - added target presence guard to prevent overwrites -  process is etremely slow

--- utils/generate_convert.sh
+++ utils/generate_convert.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 
 # ffmpeg options from: https://opensource.com/article/17/6/ffmpeg-convert-media-file-formats
 
-find . -iname '*mp4' | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting '${S}'\"";echo "ffmpeg -i '${S}' -c:v vp9 -s hd480 -v 0 '${T}'" ; echo popd ;  done | 
-tee /tmp/convert.sh
+
+find . -iname '*mp4' | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting '${S}'\""; echo "if [[ ! -f '$T' ]] ; then ffmpeg -i '${S}' -c:v vp9 -s hd480 -v 0 '${T}'; fi" ; echo popd ;  done | tee /tmp/convert.sh
 chmod +x /tmp/convert.sh
 /tmp/convert.sh
