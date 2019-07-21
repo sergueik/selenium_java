b23853f177f5950ad8b50947ad8c2688810ef044
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-21 19:12:05 -0400

    intermediate revison - replaced quotes and sort the file list

--- utils/generate_convert.sh
+++ utils/generate_convert.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Convert high-res mp4 to low res mkv
+# ffmpeg options tken from:
+# https://opensource.com/article/17/6/ffmpeg-convert-media-file-formats
 
-# ffmpeg options from: https://opensource.com/article/17/6/ffmpeg-convert-media-file-formats
-
-
-find . -iname '*mp4' | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting '${S}'\""; echo "if [[ ! -f '$T' ]] ; then ffmpeg -i '${S}' -c:v vp9 -s hd480 -v 0 '${T}'; fi" ; echo popd ;  done | tee /tmp/convert.sh
-chmod +x /tmp/convert.sh
-/tmp/convert.sh
+SCRIPT='/tmp/convert.sh'
+# SCRIPT='/tmp/convert1.sh'
+find . -iname '*mp4' |sort | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting \\"${S}\\"\""; echo "if [[ ! -f \"$T\" ]] ; then ffmpeg -i \"${S}\" -c:v vp9 -s hd480 -v 0 \"${T}\"; fi" ; echo popd ;  done | tee $SCRIPT
+chmod +x $SCRIPT
+$SCRIPT
