ba1ed4fb62cd0ded65b32a44a40d01a342712c55
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-22 19:08:14 -0400

    intermediate revision - fixed the escape case, which was observed leading to an generation of only even members of the list of pending files defect

--- utils/generate_convert.sh
+++ utils/generate_convert.sh
@@ -6,6 +6,6 @@
 
 SCRIPT='/tmp/convert.sh'
 # SCRIPT='/tmp/convert1.sh'
-find . -iname '*mp4' |sort | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting \\"${S}\\"\""; echo "if [[ ! -f \"$T\" ]] ; then ffmpeg -i \"${S}\" -c:v vp9 -s hd480 -v 0 \"${T}\"; fi" ; echo popd ;  done | tee $SCRIPT
+find . -iname '*mp4' |sort | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting \\\"${S}\\\"\""; echo "if [[ ! -f \"$T\" ]] ; then ffmpeg -i \"${S}\" -c:v vp9 -s hd480 -v 0 \"${T}\"; fi" ; echo popd ;  done | tee $SCRIPT
 chmod +x $SCRIPT
 $SCRIPT
