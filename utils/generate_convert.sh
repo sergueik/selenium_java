f68461293fed68c4b32d8c18ee1621ad7971fc83
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-29 00:31:47 -0400

    inermedate revision - make size argument, added link to recognized abbreviations

--- utils/generate_convert.sh
+++ utils/generate_convert.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Convert high-res mp4 to low res mkv
-# ffmpeg options tken from:
+# ffmpeg options taken from:
 # https://opensource.com/article/17/6/ffmpeg-convert-media-file-formats
-
+# https://ffmpeg.org/ffmpeg-utils.html#Video-size
+# e.g. -s svga -s hd480
 SCRIPT='/tmp/convert.sh'
-# SCRIPT='/tmp/convert1.sh'
-find . -iname '*mp4' |sort | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting \\\"${S}\\\"\""; echo "if [[ ! -f \"$T\" ]] ; then ffmpeg -i \"${S}\" -c:v vp9 -s hd480 -v 0 \"${T}\"; fi" ; echo popd ;  done | tee $SCRIPT
+# e.g.
+# SIZE='svga'
+# SIZE='hd480'
+SIZE=${1:-hd480}
+find . -iname '*mp4' |sort | while read filename ; do D=$(dirname "$filename"); S=$(basename "$filename"); T="${S/mp4/mkv}" ;  echo "pushd '${D}'"; echo "echo \"Converting \\\"${S}\\\"\""; echo "if [[ ! -f \"$T\" ]] ; then ffmpeg -i \"${S}\" -c:v vp9 -s ${SIZE} -v 0 \"${T}\"; fi" ; echo popd ;  done | tee $SCRIPT
 chmod +x $SCRIPT
 $SCRIPT
