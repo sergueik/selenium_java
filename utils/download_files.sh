78f7fba36006f5a2f52480796f6985ab0b349edb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-11-23 00:30:42 -0500

    added options and fixed ignoring expired certs

--- utils/download_files.sh
+++ utils/download_files.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 
 
+# origin: https://gist.github.com/cosimo/3760587
+OPTS=`getopt -o nhsbl: --long dry-run,help,batterycheck,listfile: -n 'parse-options' -- "$@"`
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+VERBOSE=false
+DRY_RUN=false
+BATTERY_CHECK=false
+LISTFILE=
+
+while true; do
+  case "$1" in
+    -v | --verbose ) VERBOSE=true; shift ;;
+    -n | --dry-run ) DRY_RUN=true; shift ;;
+    -b | --battery-check ) BATTERY_CHECK=true; shift ;;
+    -l | --listfile ) LISTFILE="$2"; shift; shift ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [[ "${VERBOSE}" = "true" ]]; then
+  echo VERBOSE=$VERBOSE
+  echo HELP=$HELP
+  echo DRY_RUN=$DRY_RUN
+  echo LISTFILE=$LISTFILE
+  echo BATTERY_CHECK=$BATTERY_CHECK
+fi
+
 TITLE='Course Title'
 FILELIST="files.$$.txt"
 1>/dev/null 2>/dev/null pushd '/tmp'
 mkdir "${TITLE}"
 cd "${TITLE}"
 pwd
+if [[ -z $LISTFILE ]] ; then
 cat <<EOF| grep '[a-z]'| tee $FILELIST
-list of urls with possible whitespace in the path 
 
 EOF
+else
+  grep '[a-z]' $LISTFILE | tee $FILELIST
+fi
 # incorrect: quotes too early
 # awk '{print "\""$0"\""}' $FILELIST | while read FILENAME; do wget $FILENAME ; done
 # correct:
-awk '{print $0}' $FILELIST | while read FILENAME; do wget "${FILENAME}" ; done
+awk '{print $0}' $FILELIST | while read FILENAME; do wget --no-check-certificate "${FILENAME}" ; done
 rm -f $FILELIST
 
 popd 1>/dev/null 2>/dev/null
