c3f6685d68babfa9aa85018f5859290aee620a56
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-05 15:04:36 -0500

    intermediate revision - switched default size to "qhd", added list of recognized

--- utils/generate_convert.sh
+++ utils/generate_convert.sh
@@ -4,7 +4,66 @@
 # ffmpeg options taken from:
 # https://opensource.com/article/17/6/ffmpeg-convert-media-file-formats
 # https://ffmpeg.org/ffmpeg-utils.html#Video-size
-# e.g.  svga, hd480, hd720
+# e.g. svga, qhd, hd480, hd720
+# default is qhd which is scaled down hd720 with same aspect raio. 
+# videos converted from hs720 to svga is best viewed with aspect ratio of 16:9
+# lynx -dump https://ffmpeg.org/ffmpeg-utils.html#Video-size 2>&1 |sed -n '/Video size/,/Video rate/p'
+# see also: https://www.maketecheasier.com/manipulate-html-and-xml-files-from-commnad-line/
+# following is the complete list of abbreviations recognized by ffmpeg:
+
+# ‘sqcif’ 128x96
+# ‘qqvga’ 160x120
+# ‘qcif’ 176x144
+# ‘hqvga’ 240x160
+# ‘cga’ 320x200
+# ‘qvga’ 320x240
+# ‘cif’ 352x288
+# ‘film’ 352x240
+# ‘ntsc-film’ 352x240
+# ‘qntsc’ 352x240
+# ‘qpal’ 352x288
+# ‘wqvga’ 400x240
+# ‘fwqvga’ 432x240
+# ‘hvga’ 480x320
+# ‘ega’ 640x350
+# ‘nhd’ 640x360
+# ‘sntsc’ 640x480
+# ‘vga’ 640x480
+# ‘4cif’ 704x576
+# ‘ntsc’ 720x480
+# ‘pal’ 720x576
+# ‘spal’ 768x576
+# ‘svga’ 800x600
+# ‘hd480’ 852x480
+# ‘wvga’ 852x480
+# ‘qhd’ 960x540
+# ‘xga’ 1024x768
+# ‘hd720’ 1280x720
+# ‘sxga’ 1280x1024
+# ‘wxga’ 1366x768
+# ‘16cif’ 1408x1152
+# ‘uxga’ 1600x1200
+# ‘wsxga’ 1600x1024
+# ‘hd1080’ 1920x1080
+# ‘wuxga’ 1920x1200
+# ‘2kflat’ 1998x1080
+# ‘2k’ 2048x1080
+# ‘2kdci’ 2048x1080
+# ‘2kscope’ 2048x858
+# ‘qxga’ 2048x1536
+# ‘qsxga’ 2560x2048
+# ‘woxga’ 2560x1600
+# ‘wqsxga’ 3200x2048
+# ‘uhd2160’ 3840x2160
+# ‘wquxga’ 3840x2400
+# ‘4kflat’ 3996x2160
+# ‘4k’ 4096x2160
+# ‘4kdci’ 4096x2160
+# ‘4kscope’ 4096x1716
+# ‘hsxga’ 5120x4096
+# ‘whsxga’ 6400x4096
+# ‘uhd4320’ 7680x4320
+# ‘whuxga’ 7680x4800
 
 # origin: https://gist.github.com/cosimo/3760587
 OPTS=`getopt -o vnhsbe: --long verbose,dry-run,help,size,batterycheck,extension: -n 'parse-options' -- "$@"`
@@ -14,7 +73,7 @@ VERBOSE=false
 HELP=false
 DRY_RUN=false
 BATTERY_CHECK=false
-SIZE='svga'
+SIZE='qhd'
 EXTENSION='mp4'
 
 while true; do
