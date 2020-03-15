d372d8fab92fea340c2e43f83c264f1ba554ff07
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-15 17:47:42 +0100

    intermediate revision - trying to add a minimal xml serializer, not working yet

--- xml2json/src/main/java/example/JSON2Xml.java
+++ xml2json/src/main/java/example/JSON2Xml.java
@@ -1,12 +1,29 @@
 package example;
 
+import java.beans.ExceptionListener;
+import java.beans.XMLEncoder;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import net.sf.json.JSONSerializer;
 import net.sf.json.xml.XMLSerializer;
+
+import java.io.File;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 public class JSON2Xml {
 	private static final XMLSerializer xmlSerializer = new XMLSerializer();
@@ -15,6 +32,7 @@ public class JSON2Xml {
 		return xmlSerializer.write(JSONSerializer.toJSON(jsonString));
 	}
 
+	@SuppressWarnings("restriction")
 	public static void main(String[] argv) {
 		if (argv.length < 1) {
 			System.out.println("Usage: java -jar JSON2Xml.jar input.xml [output.json]");
@@ -39,6 +57,41 @@ public class JSON2Xml {
 				fileoutStream.print(data);
 				fileoutStream.flush();
 				fileoutStream.close();
+
+				if (argv.length > 2) {
+					// Exception! :java.lang.IllegalAccessException: Class
+					// example.Json$SafeTreeMap with modifiers "public"
+					// sun.reflect.misc.Trampoline can not access a member of class
+					Object jsonObject = Json.parse(contents.toString());
+
+					// https://howtodoinjava.com/jaxb/write-object-to-xml/
+					// https://stackoverflow.com/questions/14057932/javax-xml-bind-jaxbexception-class-nor-any-of-its-super-class-is-known-to-t
+					try {
+						// Create JAXB Context
+						@SuppressWarnings("restriction")
+						JAXBContext jaxbContext = JAXBContext.newInstance(Object.class);
+
+						// Create Marshaller
+						@SuppressWarnings("restriction")
+						Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+						// Required formatting??
+						jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+						// Print XML String to Console
+						StringWriter sw = new StringWriter();
+
+						// Write XML to StringWriter
+						jaxbMarshaller.marshal(jsonObject, sw);
+
+						// Verify XML Content
+						String xmlContent = sw.toString();
+						System.out.println(xmlContent);
+
+					} catch (JAXBException e) {
+						e.printStackTrace();
+					}
+				}
 			} else {
 				System.out.println(data);
 			}
@@ -49,4 +102,19 @@ public class JSON2Xml {
 
 	}
 
+	// https://howtodoinjava.com/java/serialization/xmlencoder-and-xmldecoder-example/
+	// https://qna.habr.com/q/731687
+	// https://qna.habr.com/q/726053?e=8732359#clarification_847799
+	private static void serializeToXML(Object settings, String fileName) throws IOException {
+		FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+		XMLEncoder encoder = new XMLEncoder(fileOutputStream);
+		encoder.setExceptionListener(new ExceptionListener() {
+			public void exceptionThrown(Exception e) {
+				System.err.println("Exception :" + e.toString());
+			}
+		});
+		encoder.writeObject(settings);
+		encoder.close();
+		fileOutputStream.close();
+	}
 }
