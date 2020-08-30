257af45942d917b983ff9d081c93a263b3eb191a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-08-30 18:33:45 +0200

    initial snapshot, refactored ui a little bit

---
+++ javafx_webview/src/main/java/example/webviewdemo/backend/CalculatorService.java
@@ -0,0 +1,9 @@
+package example.webviewdemo.backend;
+
+public class CalculatorService {
+
+	// sync function
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ javafx_webview/src/main/java/example/webviewdemo/backend/Fruit.java
@@ -0,0 +1,13 @@
+package example.webviewdemo.backend;
+
+public class Fruit {
+	private String name;
+
+	public Fruit(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
