5b6a8c133c18e6ed3b6771f54bc8ee564d345f77
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-05 03:53:21 +0200

    removed twin clone of the external project

--- javafxwebview/src/main/resources/js/calculator-service.js
+++
@@ -1,4 +0,0 @@
-// Force java to connect the calculatorService object before any subsequent javascript files,
-// such as angular controllers, are executed. This sentence must be called
-// before any other script which will use calculatorService
-alert("__CONNECT__BACKEND__calculatorService");
