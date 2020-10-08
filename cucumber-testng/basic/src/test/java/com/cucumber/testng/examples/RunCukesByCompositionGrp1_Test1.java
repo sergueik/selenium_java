337e65e03620bb258e1f6ef8ed378cc9311611e0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-10-07 23:57:26 -0400

    initial snapshot of a 5 y old project with otdated  net.masterthought.cucumber.ReportBuilder signature, but working with its dependendies, WIP

---
+++ cucumber-testng/basic/src/test/java/com/cucumber/testng/examples/RunCukesByCompositionGrp1_Test1.java
@@ -0,0 +1,43 @@
+package com.cucumber.testng.examples;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * An example of using TestNG when the test class does not inherit from
+ * AbstractTestNGCucumberTests.
+ */
+/**
+ * Created by amit.rawat on 06/01/16.
+ */
+@CucumberOptions(
+        features = "src/test/resources/com.cucumber.testng.examples/date_calculator1.feature",
+        tags = "@calculator",
+        plugin = "json:target/cucumber1.json")
+public class RunCukesByCompositionGrp1_Test1 {
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("This is beforeSuite");
+    }
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("This is beforeClass");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("This is beforeMethod");
+    }
+    /**
+     * Create one test method that will be invoked by TestNG and invoke the
+     * Cucumber runner within that method.
+     */
+    @Test(groups = "examples-testng", description = "Example of using TestNGCucumberRunner to invoke Cucumber")
+    public void runCukes() {
+        new TestNGCucumberRunner(getClass()).runCukes();
+    }
+}
