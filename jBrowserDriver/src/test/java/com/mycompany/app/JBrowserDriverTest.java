2d971ad15060c738879d8be68172841de113929c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-10-30 21:46:27 -0400

    upgraded the jbrowserdriver version to latest release, enabled few ignored tests

--- jBrowserDriver/src/test/java/com/mycompany/app/JBrowserDriverTest.java
+++ jBrowserDriver/src/test/java/com/mycompany/app/JBrowserDriverTest.java
@@ -4,91 +4,45 @@ package com.mycompany.app;
  * Selected test scenarios for Selenium jBrowserDriver
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.lang.RuntimeException;
-
-import java.net.URLDecoder;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.Formatter;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.concurrent.TimeUnit;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.text.DateFormat;
-
-import org.apache.commons.lang.exception.ExceptionUtils;
-
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni;
-import org.openqa.selenium.interactions.Actions;
-import com.machinepublishers.jbrowserdriver.Timezone;
-import com.machinepublishers.jbrowserdriver.UserAgent;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-// TODO: what is the replacement in 3.13.x+
-// import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.UnreachableBrowserException;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import static java.lang.Boolean.*;
+import com.machinepublishers.jbrowserdriver.Timezone;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-
+@SuppressWarnings("deprecation")
 public class JBrowserDriverTest {
 
 	private static WebDriver driver;
@@ -108,26 +62,20 @@ public class JBrowserDriverTest {
 	private static final StringBuffer verificationErrors = new StringBuffer();
 	private static StringBuilder loggingSb;
 	private static String testFileName = "test.txt";
-	private static String testFilePath = new File(testFileName).getAbsolutePath()
-			.replaceAll("\\\\", "/");
-	private static final Logger log = LogManager
-			.getLogger(JBrowserDriverTest.class);
+	private static String testFilePath = new File(testFileName).getAbsolutePath().replaceAll("\\\\", "/");
+	private static final Logger log = LogManager.getLogger(JBrowserDriverTest.class);
 
 	@BeforeClass
 	public static void setUp() throws Exception {
 		loggingSb = new StringBuilder();
-		driver = new JBrowserDriver(
-				Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
+		driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
 		// driver = new PhantomJSDriver();
 		// driver = new FirefoxDriver();
 		wait = new WebDriverWait(driver, flexible_wait_interval);
 		wait.pollingEvery(wait_polling_interval, TimeUnit.MILLISECONDS);
-		driver.manage().timeouts().implicitlyWait(implicit_wait_interval,
-				TimeUnit.SECONDS);
-		cssSelectorOfElementFinderScript = getScriptContent(
-				"cssSelectorOfElement.js");
-		cssSelectorOfElementAlternativeFinderScript = getScriptContent(
-				"cssSelectorOfElementAlternative.js");
+		driver.manage().timeouts().implicitlyWait(implicit_wait_interval, TimeUnit.SECONDS);
+		cssSelectorOfElementFinderScript = getScriptContent("cssSelectorOfElement.js");
+		cssSelectorOfElementAlternativeFinderScript = getScriptContent("cssSelectorOfElementAlternative.js");
 		xpathOfElementFinderScript = getScriptContent("xpathOfElement.js");
 		driver.get(baseURL);
 	}
@@ -162,8 +110,7 @@ public class JBrowserDriverTest {
 	// the file upload plugin issue:
 	// https://github.com/MachinePublishers/jBrowserDriver/issues/143
 
-	
-	 @Ignore
+	@Ignore
 	@Test
 	public void test1SendKeys() {
 		System.err.println("test1SendKeys");
@@ -181,8 +128,7 @@ public class JBrowserDriverTest {
 
 		}
 		element = driver.findElement(By.className("progress-bar"));
-		assertThat(element.getAttribute("class"),
-				containsString("progress-bar-success"));
+		assertThat(element.getAttribute("class"), containsString("progress-bar-success"));
 		element = driver.findElement(By.id("files"));
 		assertThat(element.getText(), containsString(testFileName));
 	}
@@ -192,8 +138,7 @@ public class JBrowserDriverTest {
 	public void test2SendKeys() {
 		System.err.println("test2SendKeys");
 		driver.get("http://siptv.eu/converter/");
-		element = driver
-				.findElement(By.cssSelector("div#container form#file_form input#file"));
+		element = driver.findElement(By.cssSelector("div#container form#file_form input#file"));
 		assertThat(element, notNullValue());
 		highlight(element);
 
@@ -205,8 +150,7 @@ public class JBrowserDriverTest {
 		}
 		assertThat(element.getAttribute("value"), containsString(testFileName));
 
-		element = driver.findElement(
-				By.cssSelector("div#container form#file_form input#submit"));
+		element = driver.findElement(By.cssSelector("div#container form#file_form input#submit"));
 		element.click();
 	}
 
@@ -216,13 +160,11 @@ public class JBrowserDriverTest {
 		if (driver instanceof PhantomJSDriver) {
 			System.err.println("testExecutePhantomJS");
 			driver.get("http://siptv.eu/converter/");
-			element = driver.findElement(
-					By.cssSelector("div#container form#file_form input#file"));
+			element = driver.findElement(By.cssSelector("div#container form#file_form input#file"));
 			assertThat(element, notNullValue());
 
-			((PhantomJSDriver) driver).executePhantomJS(String.format(
-					"var page = this; page.uploadFile('input[id=file]', '%s' );",
-					testFilePath));
+			((PhantomJSDriver) driver).executePhantomJS(
+					String.format("var page = this; page.uploadFile('input[id=file]', '%s' );", testFilePath));
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -238,11 +180,11 @@ public class JBrowserDriverTest {
 		try {
 			assertEquals("Hotels", findElement("link_text", "Hotels").getText());
 		} catch (Error e) {
-			verificationErrors.append(e.toString());
+			verificationErrors.append("Error in verifyTextTest() : " + e.toString() + "\n");
 		}
 	}
 
-	@Ignore
+	// @Ignore
 	@Test
 	public void xpathOfElementTest() throws Exception {
 		element = findElement("link_text", "Hotels");
@@ -259,18 +201,18 @@ public class JBrowserDriverTest {
 
 	// NOTE: this test is hanging the jbrowserdriver
 	// after the test is run orphaned java processess require a taskkill
-	@Ignore
+	// @Ignore
 	@Test
 	public void test13_1() {
 		// Arrange
 		driver.get("http://suvian.in/selenium/2.3frame.html");
 
-		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
-				By.cssSelector(".container .row .intro-message iframe")));
+		wait.until(ExpectedConditions
+				.frameToBeAvailableAndSwitchToIt(By.cssSelector(".container .row .intro-message iframe")));
 
 		// Act
-		WebElement buttonElement = wait.until(ExpectedConditions
-				.visibilityOf(driver.findElement(By.cssSelector("h3 button"))));
+		WebElement buttonElement = wait
+				.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("h3 button"))));
 
 		assertThat(buttonElement, notNullValue());
 		buttonElement.click();
@@ -283,8 +225,7 @@ public class JBrowserDriverTest {
 		} catch (NoAlertPresentException e) {
 			// Alert not present - ignore
 		} catch (WebDriverException e) {
-			System.err
-					.println("Alert was not handled : " + e.getStackTrace().toString());
+			System.err.println("Alert was not handled : " + e.getStackTrace().toString());
 			return;
 		}
 	}
@@ -317,11 +258,11 @@ public class JBrowserDriverTest {
 			highlight(element);
 			assertEquals("input#mainSearch", selector);
 		} catch (Error e) {
-			verificationErrors.append(e.toString());
+			verificationErrors.append("Error in cssSelectorOfElementTest() : " + e.toString() + "\n");
 		}
 	}
 
-	@Ignore
+	// @Ignore
 	@Test
 	public void cssSelectorOfElementWithIdTest() throws Exception {
 		try {
@@ -330,11 +271,11 @@ public class JBrowserDriverTest {
 			highlight(element);
 			assertEquals("input#searchbox", selector);
 		} catch (Error e) {
-			verificationErrors.append(e.toString());
+			verificationErrors.append("Error in cssSelectorOfElementWithIdTest() : " + e.toString() + "\n");
 		}
 	}
 
-	// @Ignore
+	@Ignore
 	@Test
 	public void testcssSelectorOfElementAlternative() throws Exception {
 
@@ -343,18 +284,15 @@ public class JBrowserDriverTest {
 			highlight(element);
 			selector = cssSelectorOfElementAlternative(element);
 			System.err.println("css_selector: " + selector);
-			assertEquals(
-					"form[name=\"PTPT_HAC_FORM\"] > div > label > input[name=\"q\"]",
-					selector);
+			assertEquals("form[name=\"PTPT_HAC_FORM\"] > div > label > input[name=\"q\"]", selector);
 		} catch (Error e) {
-			verificationErrors.append(e.toString());
+			verificationErrors.append("Error in testcssSelectorOfElementAlternative() : " + e.toString() + "\n");
 		}
 		try {
 			element = findElement("css_selector", selector);
 			highlight(element);
 		} catch (NullPointerException e) {
-
-			verificationErrors.append(e.toString());
+			verificationErrors.append("Error in testcssSelectorOfElementAlternative() : " + e.toString() + "\n");
 		}
 	}
 
@@ -363,16 +301,14 @@ public class JBrowserDriverTest {
 	}
 
 	private String cssSelectorOfElementAlternative(WebElement element) {
-		return (String) executeScript(cssSelectorOfElementAlternativeFinderScript,
-				element);
+		return (String) executeScript(cssSelectorOfElementAlternativeFinderScript, element);
 	}
 
 	private String xpathOfElement(WebElement element) {
 		return (String) executeScript(xpathOfElementFinderScript, element);
 	}
 
-	private List<WebElement> findElements(String selectorKind,
-			String selectorValue, WebElement parent) {
+	private List<WebElement> findElements(String selectorKind, String selectorValue, WebElement parent) {
 		SearchContext finder;
 		String parent_css_selector = null;
 		String parent_xpath = null;
@@ -382,8 +318,7 @@ public class JBrowserDriverTest {
 		supportedSelectorStrategies.put("css_selector", true);
 		supportedSelectorStrategies.put("xpath", true);
 
-		if (selectorKind == null
-				|| !supportedSelectorStrategies.containsKey(selectorKind)
+		if (selectorKind == null || !supportedSelectorStrategies.containsKey(selectorKind)
 				|| !supportedSelectorStrategies.get(selectorKind)) {
 			return null;
 		}
@@ -396,23 +331,19 @@ public class JBrowserDriverTest {
 		}
 
 		if (selectorKind == "css_selector") {
-			String extended_css_selector = String.format("%s  %s",
-					parent_css_selector, selectorValue);
+			String extended_css_selector = String.format("%s  %s", parent_css_selector, selectorValue);
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.cssSelector(extended_css_selector)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(extended_css_selector)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
 			elements = finder.findElements(By.cssSelector(selectorValue));
 		}
 		if (selectorKind == "xpath") {
-			String extended_xpath = String.format("%s/%s", parent_xpath,
-					selectorValue);
+			String extended_xpath = String.format("%s/%s", parent_xpath, selectorValue);
 
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.xpath(extended_xpath)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(extended_xpath)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -421,8 +352,7 @@ public class JBrowserDriverTest {
 		return elements;
 	}
 
-	private List<WebElement> findElements(String selectorKind,
-			String selectorValue) {
+	private List<WebElement> findElements(String selectorKind, String selectorValue) {
 		return findElements(selectorKind, selectorValue, null);
 	}
 
@@ -436,15 +366,13 @@ public class JBrowserDriverTest {
 		supportedSelectorStrategies.put("link_text", true);
 		supportedSelectorStrategies.put("classname", false);
 
-		if (selectorKind == null
-				|| !supportedSelectorStrategies.containsKey(selectorKind)
+		if (selectorKind == null || !supportedSelectorStrategies.containsKey(selectorKind)
 				|| !supportedSelectorStrategies.get(selectorKind)) {
 			return null;
 		}
 		if (selectorKind == "id") {
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.id(selectorValue)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(selectorValue)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -453,8 +381,7 @@ public class JBrowserDriverTest {
 		if (selectorKind == "classname") {
 
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.className(selectorValue)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(selectorValue)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -462,8 +389,7 @@ public class JBrowserDriverTest {
 		}
 		if (selectorKind == "link_text") {
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.linkText(selectorValue)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(selectorValue)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -471,8 +397,7 @@ public class JBrowserDriverTest {
 		}
 		if (selectorKind == "css_selector") {
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.cssSelector(selectorValue)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(selectorValue)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -481,8 +406,7 @@ public class JBrowserDriverTest {
 		if (selectorKind == "xpath") {
 
 			try {
-				wait.until(ExpectedConditions
-						.visibilityOfElementLocated(By.xpath(selectorValue)));
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(selectorValue)));
 			} catch (RuntimeException timeoutException) {
 				return null;
 			}
@@ -495,8 +419,7 @@ public class JBrowserDriverTest {
 		highlight(element, highlight_interval, "yellow");
 	}
 
-	public void highlight(WebElement element, long highlightInterval,
-			String color) {
+	public void highlight(WebElement element, long highlightInterval, String color) {
 		System.err.println("Color: " + color);
 		if (wait == null) {
 			wait = new WebDriverWait(driver, flexible_wait_interval);
@@ -510,8 +433,7 @@ public class JBrowserDriverTest {
 
 		try {
 			wait.until(ExpectedConditions.visibilityOf(element));
-			executeScript(String.format("arguments[0].style.border='3px %s'", color),
-					element);
+			executeScript(String.format("arguments[0].style.border='3px %s'", color), element);
 			Thread.sleep(highlightInterval);
 			executeScript("arguments[0].style.border=''", element);
 		} catch (InterruptedException e) {
@@ -521,8 +443,7 @@ public class JBrowserDriverTest {
 
 	private Object executeScript(String script, Object... arguments) {
 		if (driver instanceof JavascriptExecutor) {
-			JavascriptExecutor javascriptExecutor = JavascriptExecutor.class
-					.cast(driver);
+			JavascriptExecutor javascriptExecutor = JavascriptExecutor.class.cast(driver);
 			return javascriptExecutor.executeScript(script, arguments);
 		} else {
 			throw new RuntimeException("Script execution failed.");
@@ -531,8 +452,7 @@ public class JBrowserDriverTest {
 
 	protected static String getScriptContent(String scriptName) throws Exception {
 		try {
-			final InputStream stream = JBrowserDriverTest.class.getClassLoader()
-					.getResourceAsStream(scriptName);
+			final InputStream stream = JBrowserDriverTest.class.getClassLoader().getResourceAsStream(scriptName);
 			final byte[] bytes = new byte[stream.available()];
 			stream.read(bytes);
 			return new String(bytes, "UTF-8");
