9d874e1149f4025874a4a9c14f259651243feccf
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-11-22 21:45:09 -0500

    intermediate revision - added prename

--- utils/collect_files.sh
+++ utils/collect_files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 LIST=${1:-list.txt}
-
+REMOVE_TITLE_FRAGMENT=${2:--git.ir}
 # list.txt has list of urls
 # incorrect curl arguments
 cat $LIST | while read F
@@ -26,3 +26,10 @@ do
   fi
 done
 
+# Remove some portion of the name
+if false ; then
+  ls -1 *mp4| while read F ; do G=$(echo $F|sed "s|$REMOVE_TITLE_FRAGMENT||") ; mv "$F" "$G" ; done
+fi
+# Better way is to use /usr/bin/prename
+TITLE_FRAGMENT_ARGUMENT=$(echo $REMOVE_TITLE_FRAGMENT| sed 's|-|\\-|g')
+prename -v "s|$TITLE_FRAGMENT_ARGUMENT||" *mp4
