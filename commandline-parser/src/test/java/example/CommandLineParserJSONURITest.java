c1b57b6c3e418ba3cbfd2a7f05eb7f1fa450e579
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-11-25 13:27:00 -0500

    intermedate revision - refactoring JsonReader and associated test

--- commandline-parser/src/test/java/example/CommandLineParserJSONURITest.java
+++ commandline-parser/src/test/java/example/CommandLineParserJSONURITest.java
@@ -12,13 +12,20 @@ import static org.hamcrest.Matchers.hasItems;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -41,6 +48,17 @@ public class CommandLineParserJSONURITest {
 	private static CommandLineParser commandLineParser;
 	private String[] argsArray = {};
 	private String arg = null;
+	protected static String osName = getOSName();
+	private static final String dataFileName = "a.json";
+	private static final String dataFilePath = osName.equals("windows")
+			? Paths.get(System.getProperty("user.home")).resolve("Desktop")
+					.resolve(dataFileName).toString()
+			: "/tmp/" + dataFileName;
+
+	private static final String dataFileUri = osName.equals("windows")
+			? "file:///" + dataFilePath.replaceAll("\\\\", "/")
+			: "file://" + dataFilePath;
+	private static File file;
 
 	// stop keeping it
 	@Before
@@ -49,12 +67,25 @@ public class CommandLineParserJSONURITest {
 		commandLineParser.setDebug(debug);
 	}
 
+	@SuppressWarnings("unused")
+	@AfterClass
+	public static void cleanup() throws FileNotFoundException, IOException {
+
+		if (!file.exists())
+			throw new FileNotFoundException(file.getAbsolutePath());
+		if (!file.delete()) {
+			Object[] filler = { file.getAbsolutePath() };
+			throw new IOException("Delete failed: " + file.getAbsolutePath());
+		}
+
+	}
 	// early tests created for GSON
 
 	@Test
 	public void addStringJSONFromUrlTest() {
 
-		argsArray = new String[] { "-in", "http://echo.jsontest.com/key/value/one/two" };
+		argsArray = new String[] { "-in",
+				"http://echo.jsontest.com/key/value/one/two" };
 		commandLineParser.saveFlagValue("in");
 		commandLineParser.parse(argsArray);
 		assertThat(commandLineParser.getNumberOfFlags(), is(1));
@@ -66,43 +97,57 @@ public class CommandLineParserJSONURITest {
 		// early tests created for GSON
 		// assertThat(commandLineParser.getFlagValue("in"), is("one|key"));
 		try {
-			JSONObject jsonObject = new JSONObject(commandLineParser.getFlagValue("in"));
+			JSONObject jsonObject = new JSONObject(
+					commandLineParser.getFlagValue("in"));
 			assertThat(jsonObject.has("one"), is(true));
 			assertThat(jsonObject.has("key"), is(true));
 		} catch (JSONException e) {
 			System.err.println("Unexpected error: " + e.toString());
 			throw e;
 		}
-		System.err.println("argumentNamesValuesTest(): flag value: " + commandLineParser.getFlagValue("in"));
+		System.err.println("argumentNamesValuesTest(): flag value: "
+				+ commandLineParser.getFlagValue("in"));
 	}
 
 	@Test
 	public void addDataJSONFromFileTest() {
 
-		PrintWriter out = openWriter("/tmp/a.json");
+		PrintWriter out = openWriter(dataFilePath);
+		// TODO: debug
+		// addDataJSONFromFileTest(example.CommandLineParserJSONURITest):
+		// A JSONObject text must begin with '{' at 1 [character 2 line 1]
+		// obseved with Lib.NONE Lib.GSON
 		out.println("{\"foo\":1,\n" + "	\"bar\":2}");
+		out.flush();
 		out.close();
-		argsArray = new String[] { "-in", "file:///tmp/a.json" };
+		argsArray = new String[] { "-in", dataFileUri };
 		commandLineParser.saveFlagValue("in");
 		commandLineParser.parse(argsArray);
 		assertThat(commandLineParser.getNumberOfFlags(), is(1));
 		assertThat(commandLineParser.hasFlag("in"), is(true));
 		assertThat(commandLineParser.getFlagValue("in"), notNullValue());
+		System.err.println("addDataJSONFromFileTest(): flag value: "
+				+ commandLineParser.getFlagValue("in"));
 		try {
-			JSONObject jsonObject = new JSONObject(commandLineParser.getFlagValue("in"));
+			JSONObject jsonObject = new JSONObject(
+					commandLineParser.getFlagValue("in"));
 			assertThat(jsonObject.has("foo"), is(true));
 			assertThat(jsonObject.has("bar"), is(true));
 		} catch (JSONException e) {
 			System.err.println("Unexpected error: " + e.toString());
 			throw e;
 		}
-		System.err.println("argumentNamesValuesTest(): flag value: " + commandLineParser.getFlagValue("in"));
+		System.err.println("argumentNamesValuesTest(): flag value: "
+				+ commandLineParser.getFlagValue("in"));
 	}
 
 	private static PrintWriter openWriter(String name) {
 		try {
-			File file = new File(name);
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)), true);
+			boolean append = true;
+			file = new File(name);
+			OutputStream otputStream = new FileOutputStream(file, append);
+			PrintWriter out = new PrintWriter(
+					new OutputStreamWriter(otputStream, "UTF8"));
 			return out;
 		} catch (IOException e) {
 			System.err.println("I/O Error");
@@ -111,4 +156,14 @@ public class CommandLineParserJSONURITest {
 		return null;
 	}
 
+	// Utilities
+	public static String getOSName() {
+		if (osName == null) {
+			osName = System.getProperty("os.name").toLowerCase();
+			if (osName.startsWith("windows")) {
+				osName = "windows";
+			}
+		}
+		return osName;
+	}
 }
