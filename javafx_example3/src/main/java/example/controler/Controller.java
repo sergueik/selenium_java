b920b0e7eb4c487c5be882258bbe0f79bb385df9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-04-03 21:11:55 -0400

    added skeleton project replica with provided but uncontrolled fxml

---
+++ javafx_example3/src/main/java/example/controler/Controller.java
@@ -0,0 +1,19 @@
+package example.controler;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+	@FXML
+	private Label todayLabel;
+
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+}
