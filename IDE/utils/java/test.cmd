c775842669d69a16802830bf1ae0104ab7691259
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-04-17 13:35:02 -0400

    updated old project to not fail executing otdated expectations against test page DOM

--- IDE/utils/java/test.cmd
+++ IDE/utils/java/test.cmd
@@ -4,7 +4,7 @@ call mvn clean package install
 set TARGET=%CD%\target
 set MAIN_APP_CLASS=com.mycompany.app.App
 set SELENIUM_VERSION=2.53.0
-set APP_VERSION=0.0.3-SNAPSHOT
+set APP_VERSION=0.0.4-SNAPSHOT
 REM adding selenium-standalone to the CLASSPATH is not necessary.
 REM java -cp %TARGET%\app-%APP_VERSION%.jar;c:\java\selenium\selenium-server-standalone-%SELENIUM_VERSION%.jar;%TARGET%\lib\* %MAIN_APP_CLASS%
 java -cp %TARGET%\app-%APP_VERSION%.jar;%TARGET%\lib\*  %MAIN_APP_CLASS%
