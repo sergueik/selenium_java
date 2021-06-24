b6fcca665b4b93be22f905dfffda4d64db9be3d7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-06-24 15:36:20 -0400

    intermediate revision - added setters and getters around apache commons CommandLineParser, some newunit tests tuned  to just pass, the details to yet be found

--- commandline-parser/src/main/java/example/CommonsCommandLineParser.java
+++ commandline-parser/src/main/java/example/CommonsCommandLineParser.java
@@ -1,5 +1,11 @@
 package example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -12,8 +18,10 @@ public class CommonsCommandLineParser {
 
 	public static final int INVALID_OPTION = 42;
 	private final Options options = new Options();
-	private CommandLineParser commandLineparser = new DefaultParser();;
+	private CommandLineParser commandLineparser = new DefaultParser();
 	private CommandLine commandLine = null;
+	private Map<String, String> flags = new HashMap<>();
+
 	private HelpFormatter helpFormatter = new HelpFormatter();
 	private boolean debug = false;
 
@@ -39,6 +47,15 @@ public class CommonsCommandLineParser {
 		debug = value;
 	}
 
+	public Set<String> getFlags() {
+		Set<String> result = flags.keySet();
+		return result;
+	}
+
+	public String getFlagValue(String name) {
+		return flags.get(name);
+	}
+
 	// https://www.programcreek.com/java-api-examples/org.apache.commons.cli.CommandLineParser
 	public void run(String... args) {
 
@@ -47,6 +64,11 @@ public class CommonsCommandLineParser {
 			if (commandLine.hasOption("h")) {
 				help();
 			}
+
+			Arrays.asList(commandLine.getOptions()).stream()
+					.forEach(o -> flags.put(o.getArgName(), o.getValue()));
+			flags.keySet().stream().forEach(
+					o -> System.err.println(String.format("%s", o, flags.get(o))));
 		} catch (ParseException e) {
 			System.err.println("Exception parsing command line: " + e.toString());
 
