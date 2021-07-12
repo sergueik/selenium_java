37eb5bf00542d9911089b399779866974ed1b0a9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-07-12 17:34:47 -0400

    interemdiate revision - added local / headless option

--- elementary/README.md
+++ elementary/README.md
@@ -6,12 +6,17 @@ elementary standalone application intended to probe the grid health
 * start Selenium grid on some host or locally
 * build application packge
 ```sh
-mvn package
+mvn clean package
 ```
 * run specifying the `browser`, the `host` and `port` of the hub (optionally) and the target `url` through commandline arguments
 ```sh
-java  -cp target/elementary-0.3-SNAPSHOT.jar;target/lib/* example.App --url https://www.google.com  -b chrome
+java -cp target/elementary-0.4-SNAPSHOT.jar;target/lib/* example.App --url https://www.google.com  -b chrome
 ```
-(on unix, replace `;` with `:`
+on unix, replace `;` with `:`: 
+```sh
+java -cp target/elementary-0.4-SNAPSHOT.jar:target/lib/* example.App --url https://www.google.com  -b chrome -i
+```
+the `--local` option allows running chrome localy, in headless mode.
+
 ### Author
 [Serguei Kouzmine](kouzmine_serguei@yahoo.com)
--- elementary/src/main/java/example/App.java
+++ elementary/src/main/java/example/App.java
@@ -3,6 +3,7 @@ package example;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 // https://gist.github.com/nickname55/880addec70a8303b2359680376d5d066
@@ -18,6 +19,8 @@ import org.apache.commons.io.FilenameUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -31,19 +34,19 @@ public class App {
 
 	private static Options options = new Options();
 
-	public static void main(String[] args)
-			throws InterruptedException, IOException {
+	private static WebDriver driver = null;
 
-		options.addOption(Option.builder("u").longOpt("url").hasArg(true)
-				.desc("url to open").required().build());
-		options.addOption(Option.builder("h").longOpt("host").hasArg(true)
-				.desc("hub host").required(false).build());
-		options.addOption(Option.builder("p").longOpt("port").hasArg(true)
-				.desc("hub port").required(false).build());
-		options.addOption(Option.builder("b").longOpt("browser").hasArg(true)
-				.desc("browser to run").required(false).build());
-		options.addOption(Option.builder("d").longOpt("debug").hasArg(false)
-				.desc("debug").required(false).build());
+	protected static boolean local = false;
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+
+		options.addOption(Option.builder("u").longOpt("url").hasArg(true).desc("url to open").required().build());
+		options.addOption(Option.builder("h").longOpt("host").hasArg(true).desc("hub host").required(false).build());
+		options.addOption(Option.builder("p").longOpt("port").hasArg(true).desc("hub port").required(false).build());
+		options.addOption(
+				Option.builder("b").longOpt("browser").hasArg(true).desc("browser to run").required(false).build());
+		options.addOption(Option.builder("d").longOpt("debug").hasArg(false).desc("debug").required(false).build());
+		options.addOption(Option.builder("l").longOpt("local").hasArg(false).desc("localt").required(false).build());
 
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = null;
@@ -70,27 +73,74 @@ public class App {
 			} else {
 				url = cmd.getOptionValue("u");
 			}
+			if (cmd.hasOption("l")) {
+				local = true;
+			}
 		} catch (ParseException pe) {
 			System.out.println("Error parsing command-line arguments");
 			HelpFormatter formatter = new HelpFormatter();
 			System.exit(1);
 		}
 
-		final DesiredCapabilities capabilities = new DesiredCapabilities();
+		if (local) {
+			ChromeOptions options = new ChromeOptions();
+			for (String optionAgrument : (new String[] { 
+					// @formatter:off
+					"--allow-insecure-localhost",
+					"--allow-running-insecure-content", 
+					"--browser.download.folderList=2",
+					"--browser.helperApps.neverAsk.saveToDisk=image/jpg,text/csv,text/xml,application/xml,application/vnd.ms-excel,application/x-excel,application/x-msexcel,application/excel,application/pdf",
+					"--disable-blink-features=AutomationControlled", 
+					"--disable-default-app", 
+					"--disable-dev-shm-usage",
+					"--disable-extensions", 
+					"--disable-gpu",
+					"--disable-infobars", 
+					"--disable-in-process-stack-traces",
+					"--disable-logging", 
+					"--disable-notifications", 
+					"--disable-popup-blocking",
+					"--disable-save-password-bubble", 
+					"--disable-translate", 
+					"--disable-web-security",
+					"--enable-local-file-accesses", 
+					"--headless", 
+					"--ignore-certificate-errors",
+					"--ignore-ssl-errors=true", 
+					"--log-level=3", 
+					"--no-proxy-server", 
+					"--no-sandbox",
+					"--output=/dev/null",
+					"--ssl-protocol=any", 
+					"--user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0", 
+				})) {
+				// @formatter:on
+				options.addArguments(optionAgrument);
+			}
+			options.addArguments();
+			System.setProperty("webdriver.chrome.driver",
+					Paths.get(System.getProperty("user.home")).resolve("Downloads")
+							.resolve(
+									System.getProperty("os.name").toLowerCase().contains("windows") ? "chromedriver.exe"
+											: "chromedriver")
+							.toAbsolutePath().toString());
+			driver = new ChromeDriver(options);
+		} else {
+			final DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		capabilities.setBrowserName(browser);
+			capabilities.setBrowserName(browser);
 
-		capabilities.setPlatform(org.openqa.selenium.Platform.ANY);
-		String hub = String.format("http://%s:%s/wd/hub", host, port);
-		if (debug) {
-			System.err.println("launching browser on hub " + hub);
+			capabilities.setPlatform(org.openqa.selenium.Platform.ANY);
+			String hub = String.format("http://%s:%s/wd/hub", host, port);
+			if (debug) {
+				System.err.println("launching browser on hub " + hub);
+			}
+			driver = new RemoteWebDriver(new URL(hub), capabilities);
 		}
-		final WebDriver driver = new RemoteWebDriver(new URL(hub), capabilities);
-
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		// Go to URL
-		// "http://www.hollandamerica.com/"
+		System.err.println("get url: " + url);
 		driver.get(url);
 
 		// Maximize Window
@@ -103,8 +153,7 @@ public class App {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
 		// save the screenshot in png format on the disk.
-		FileUtils.copyFile(scrFile,
-				new File(FilenameUtils.concat(currentDir, "screenshot.png")));
+		FileUtils.copyFile(scrFile, new File(FilenameUtils.concat(currentDir, "screenshot.png")));
 		driver.close();
 		driver.quit();
 
