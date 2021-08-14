1e7b4c2beb326c8a38cd2cfc002224e277b62e58
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-08-14 04:30:57 -0400

    added backup / restore script

---
+++ utils/restore_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IMAGE=${1:-lenovo120s}
+DISK=${2:-/dev/sdc}
+STAGING='/media/sergueik/Windows8_OS/Users/Serguei/Desktop/Private/z/'
+if [ ! -d $STAGING ] ;then
+  exit 0
+fi
+pushd $STAGING
+dd if=$IMAGE.img of=$DISK bs=500M conv=noerror status=progress
+popd
