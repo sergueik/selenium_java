eaa48c9512ace9f2ba689a398e97bd2474b4dd99
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-09-18 00:08:38 -0400

    intermediate reevision -  added loading JSON but mistake in calculating timstamps to relative seconds

--- javafx_treeview/src/main/java/example/ChartEx.java
+++ javafx_treeview/src/main/java/example/ChartEx.java
@@ -1,10 +1,20 @@
 package example;
 
+// import static org.hamcrest.CoreMatchers.is;
+// import static org.hamcrest.CoreMatchers.notNullValue;
+// import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 // import org.apache.commons.cli.CommandLine;
 // import org.apache.commons.cli.CommandLineParser;
@@ -29,6 +39,10 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import example.CommandLineParser;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
 /**
  * based on Oracle's example at
  * https://docs.oracle.com/javafx/2/charts/line-chart.htm#CIHGBCFI
@@ -38,7 +52,7 @@ import example.CommandLineParser;
 public class ChartEx extends Application {
 	private static Logger logger = LogManager.getLogger(ChartEx.class.getName());
 	private static String[] HEADERS = { "column1", "column2" };
-	
+
 	// private static CommandLineParser commandLineparser = new DefaultParser();
 	// private static CommandLine commandLine = null;
 	// private final static Options options = new Options();
@@ -63,7 +77,6 @@ public class ChartEx extends Application {
 				logger.info("Missing required argument: resource");
 				return;
 			}
-
 			Reader in;
 			Iterable<CSVRecord> records;
 			in = new FileReader(resource);
@@ -89,6 +102,34 @@ public class ChartEx extends Application {
 				data.add(row);
 			}
 			logger.info("data: " + data);
+			data.clear();
+			// https://stackoverflow.com/questions/17613363/gson-deserializing-an-array-of-objects
+			final Gson gson = new Gson();
+			String datafilePath = "data.json";
+			String payload = readFile(datafilePath, Charset.forName("UTF-8"));
+			QueryTimeserieResponse[] payloadObj = (QueryTimeserieResponse[]) gson
+					.fromJson(payload, QueryTimeserieResponse[].class);
+			for (QueryTimeserieResponse entry : payloadObj) {
+				if (!entry.getTarget().equals("test")) {
+					continue;
+				}
+				// logger.info(entry.getDatapoints());
+				List<List<Float>> datapoints = (List<List<Float>>) entry
+						.getDatapoints();
+				float x = datapoints.get(0).get(1);
+				for (int cnt = 0; cnt != datapoints.size(); cnt++) {
+					List<Float> dataentry = datapoints.get(cnt);
+					List<Float> row = new ArrayList<>();
+					logger.info("loading row: " + dataentry);
+					// swap columns
+					row.add((dataentry.get(1) - x) / 1000);
+					row.add(dataentry.get(0));
+
+					logger.info("Reading datapoint: " + row);
+					data.add(row);
+				}
+			}
+			logger.info("data: " + data);
 			launch(args);
 			// } catch (ParseException e) {
 		} catch (IOException e) {
@@ -109,22 +150,25 @@ public class ChartEx extends Application {
 
 		lineChart.setTitle("Chart");
 		XYChart.Series series = new XYChart.Series();
-		series.setName("Load Average");
+		series.setName("test");
 		for (int cnt = 0; cnt != data.size(); cnt++) {
 			List<Float> row = data.get(cnt);
-			series.getData().add(new XYChart.Data(cnt, row.get(0)));
+			float x = row.get(0);
+			float y = row.get(1);
+			logger.info(String.format("data point %d (%f %f)", cnt, x, y));
+			series.getData().add(new XYChart.Data(x, y));
 		}
-
-		XYChart.Series series1 = new XYChart.Series();
-		series1.setName("Status");
-		for (int cnt = 0; cnt != data.size(); cnt++) {
-			List<Float> row = data.get(cnt);
-			series1.getData().add(new XYChart.Data(cnt, row.get(1)));
-		}
-
+		/*
+				XYChart.Series series1 = new XYChart.Series();
+				series1.setName("Status");
+				for (int cnt = 0; cnt != data.size(); cnt++) {
+					List<Float> row = data.get(cnt);
+					series1.getData().add(new XYChart.Data(cnt, row.get(1)));
+				}
+		*/
 		Scene scene = new Scene(lineChart, 800, 600);
 		lineChart.getData().add(series);
-		lineChart.getData().add(series1);
+		// lineChart.getData().add(series1);
 
 		stage.setScene(scene);
 		stage.show();
@@ -132,5 +176,10 @@ public class ChartEx extends Application {
 
 	public static void help() {
 		System.exit(1);
+	}
+
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
 	}
 }
