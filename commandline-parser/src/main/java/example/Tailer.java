df3eb432cf7289fc5e0480cc162266dfcf10a908
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-10-03 18:15:37 -0400

    intermediate revision - added properties with getters for length, bytes, data

--- commandline-parser/src/main/java/example/Tailer.java
+++ commandline-parser/src/main/java/example/Tailer.java
@@ -9,29 +9,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 // based on: https://docs.oracle.com/javase/6/docs/api/java/io/RandomAccessFile.html
-//see also: http://www.java2s.com/Tutorial/Java/0180__File/ReadingaFileintoaByteArrayreadstheentirecontentsofafileintoabytearray.htm
+// see also: http://www.java2s.com/Tutorial/Java/0180__File/ReadingaFileintoaByteArrayreadstheentirecontentsofafileintoabytearray.htm
 public class Tailer {
 
+	private boolean debug = true;
 	private String filePath = null;
+
+	public void setFilePath(String value) {
+		filePath = value;
+	}
+
 	private int offset = -1;
+	private int length = -1;
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int value) {
+		offset = value;
+	}
+
 	private byte[] bytes;
 	private String data;
 	private RandomAccessFile file;
 
+	public void setFile(RandomAccessFile value) {
+		file = value;
+	}
+
 	public static void main(String[] args) {
-		if (args.length == 2) {
+		if (args.length >= 2) {
 			Tailer tailer = new Tailer();
-			tailer.filePath = args[0];
-			tailer.offset = Integer.parseInt(args[1]);
+			tailer.setFilePath(args[0]);
+			tailer.setOffset(Integer.parseInt(args[1]));
+			tailer.tail();
+			System.out.println(tailer.data);
+			// System.out.println(tailer.data.length());
+			// System.out.println(tailer.bytes.length);
+			int length = tailer.getLength();
+			System.out.println("length: " + length);
+			System.out.println(
+					String.format("rpm: %d /%d ", tailer.rpm(), tailer.rpm(false)));
+			String fragment = args.length == 2 ? "version>" : args[2];
+			System.out.println(
+					String.format("rpm	(%s): %d", fragment, tailer.rpm(fragment)));
+			tailer.setOffset(length);
 			tailer.tail();
 			System.out.println(tailer.data);
 			// System.out.println(tailer.data.length());
 			// System.out.println(tailer.bytes.length);
 			System.out.println(
 					String.format("RPM(1): %d /%d ", tailer.rpm(), tailer.rpm(false)));
-			String fragment = "<version>";
-			System.out.println(
-					String.format("RPM(%s): %d", fragment, tailer.rpm(fragment)));
 
 		}
 	}
@@ -46,7 +79,11 @@ public class Tailer {
 
 	// NOTE: flag is not used
 	public int rpm(boolean flag) {
-		int result = data.split("\r?\n").length;
+		if (debug)
+			System.err.println(String.format("data: \"%s\"", data));
+		int result = (data.isEmpty()) ? 0 : data.split("\r?\n").length;
+		if (debug)
+			System.err.println(String.format("result: \"%d\"", result));
 		return result;
 	}
 
@@ -64,42 +101,59 @@ public class Tailer {
 
 	public void tail() {
 		try {
-			file = new RandomAccessFile(filePath, "r");
-			int length = (int) file.length();
-			file.seek(offset);
-			int numBytesToRead = length - offset;
-			if (length > Integer.MAX_VALUE) {
-				System.out.println("File is too large");
+			if (file == null) {
+				file = new RandomAccessFile(filePath, "r");
+			}
+			length = (int) file.length();
+			if (debug) {
+				System.err.println(String.format("offset %d lenth %d", offset, length));
 			}
 
-			System.err.println(String.format("Allocate %d bytes", numBytesToRead));
-			bytes = new byte[(int) numBytesToRead];
-
-			int readOffset = 0;
-			int numRead = 0;
-			// numBytesToRead = 1000;
-			// int totalRead = 0;
-			while (numRead >= 0) {
-				System.err.println(String.format("Reading %d %d", offset + readOffset,
-						numBytesToRead));
-				// java.lang.IndexOutOfBoundsException
-				numRead = file.read(bytes, readOffset, numBytesToRead);
-				if (numRead == 0)
-					break;
-				readOffset += numRead;
-				if (numBytesToRead > length - (offset + readOffset)) {
-					System.err.println(String.format("Remaining %d bytes",
-							length - (offset + readOffset)));
-					System.err.println(
-							String.format("Read %d bytes in the buffer already", readOffset));
-					numBytesToRead = length - (offset + readOffset);
+			if (offset < length) {
+				file.seek(offset);
+				int numBytesToRead = length - offset;
+				if (length > Integer.MAX_VALUE) {
+					System.out.println("File is too large");
 				}
-			}
+				if (debug)
+					System.err
+							.println(String.format("Allocate %d bytes", numBytesToRead));
+				bytes = new byte[(int) numBytesToRead];
 
-			if (readOffset < bytes.length) {
-				throw new IOException("Could not completely read file " + filePath);
+				int readOffset = 0;
+				int numRead = 0;
+				// numBytesToRead = 1000;
+				// int totalRead = 0;
+				while (numRead >= 0) {
+					if (debug)
+						System.err.println(String.format("Reading %d %d",
+								offset + readOffset, numBytesToRead));
+					// java.lang.IndexOutOfBoundsException
+					numRead = file.read(bytes, readOffset, numBytesToRead);
+					if (numRead == 0)
+						break;
+					readOffset += numRead;
+					if (numBytesToRead > length - (offset + readOffset)) {
+						if (debug)
+							System.err.println(String.format("Remaining %d bytes",
+									length - (offset + readOffset)));
+						System.err.println(String
+								.format("Read %d bytes in the buffer already", readOffset));
+						numBytesToRead = length - (offset + readOffset);
+					}
+				}
+
+				if (readOffset < bytes.length) {
+					throw new IOException("Could not completely read file " + filePath);
+				}
+			} else {
+				if (debug)
+					System.err.println("clearing bytes and data");
+				bytes = new byte[] {};
+				data = null;
 			}
 			file.close();
+			file = null;
 			data = new String(bytes);
 
 		} catch (IOException e) {
