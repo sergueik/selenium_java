80b434f79481cb7eb4052ccc93f622080c0a8a89
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-03-11 01:41:06 +0100

    intermediate revision - added processing of the project directory argument

--- utils/pickproject.sh
+++ utils/pickproject.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-PROJECT=$(pwd | sed 's|^.*/||') # current directory
+PROJECT=$1
+if [ -z $PROJECT ] ; then
+  PROJECT=$(pwd | sed 's|^.*/||') # current directory
+else
+  mkdir $PROJECT
+  cd $PROJECT
+fi
 tar xzvf ~/$PROJECT.tar.gz
 find . -type f -exec touch {} \;
 git add --all .
