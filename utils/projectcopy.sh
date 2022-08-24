f8a2a3989c1cd474c9ef8669dd06f462d970910b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-08-25 00:26:43 +0200

    intermedate revision - added artifact filtering

--- utils/projectcopy.sh
+++ utils/projectcopy.sh
@@ -6,9 +6,16 @@ REMOTE_USER=${2:-sergueik}
 # unfortunately `whoami` in Windows in git bash can be different from
 # target unix machine user
 
-tar czvf ../$PROJECT.tar.gz  --exclude='.git*' --exclude='target' .
+# using gnu tar exclude option
+# NOTE: can also use simpler expression like --exclude 'packages'
+if [[ $DEBUG == 'true' ]] ; then
+  echo tar czvf ../$PROJECT.tar.gz --exclude='.git*' --exclude='target' --exclude './packages/*' --exclude './*/obj/*' --exclude './*/bin/*' .
+fi
+tar czvf ../$PROJECT.tar.gz --exclude='.git*' --exclude='target' --exclude './packages/*' --exclude './*/obj/*' --exclude './*/bin/*' .
 if [[ $DEBUG == 'true' ]] ; then
   echo scp ../$PROJECT.tar.gz $REMOTE_USER@${REMOTE_HOST}:
 fi
+echo "sending $( stat -c '%s'  ../$PROJECT.tar.gz ) bytes"
 scp ../$PROJECT.tar.gz $REMOTE_USER@${REMOTE_HOST}:
+
 
