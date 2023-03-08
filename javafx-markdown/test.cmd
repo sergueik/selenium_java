bb47bbb0849a86c1f3aa52b9417b9e8e7d68c030
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-03-08 01:05:35 +0100

    intermediate revision - added check

--- javafx-markdown/test.cmd
+++ javafx-markdown/test.cmd
@@ -1,7 +1,9 @@
 @echo OFF
 REM do not name this script run.cmd
+where.exe mvn.cmd 2>NUL 1>NUL
+if errorlevel 1 goto :SKIP
 call mvn package
-
 echo done
+:SKIP
 java -cp target\example.javafx_markdown.jar;target\lib\* example.Example
 REM java -jar target\example.javafx_markdown.jar
