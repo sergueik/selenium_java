acc9e66f3ecdc1971c182826f8dbfcceb7116776
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-02 01:27:31 +0200

    itermediate revision - simplified script to deal with one directory and use a different filename for generated files

--- utils/resampler.cmd
+++ utils/resampler.cmd
@@ -1,8 +1,9 @@
 @echo off
 
-REM origin: https://raw.githubusercontent.com/melk0r64/Resampler-Script/main/ReSampler.bat
+REM based on: https://raw.githubusercontent.com/melk0r64/Resampler-Script/main/ReSampler.bat
+REM the original code scaned subbolders
 REM NOTE: the original code relied on Window title scan by tasklist.exe 
-REM and does not correctly wait for the processes to terminate
+REM and failed to discover the processes to wait for
 REM see also: flac downsampling
 REM see also: https://forums.stevehoffman.tv/threads/high-res-downsampling-for-dummies.1146789/
 REM see also: https://sox.sourceforge.net
@@ -11,68 +12,43 @@ REM see also: https://github.com/melk0r64/Resampler-Script
 REM see also: https://github.com/jniemann66/ReSampler
 REM see also: https://github.com/tchnmf/sox/tree/master/scripts
 
-setlocal EnableExtensions EnableDelayedExpansion
-set "SourcePath=%CD%\"
-set "TargetPath=%CD%\..\resampled"
-mkdir %TargetPath%
-rem If the batch file was started with a string as
-rem parameter, interpret this string as source path.
-if not "%~1" == "" set "SourcePath=%~1"
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-rem If the batch file was started with one more string
-rem as parameter, interpret this string as target path.
-if not "%~2" == "" set "TargetPath=%~2"
-
-rem Remove backslash at end of source and target path
-rem in case of being specified with a backslash at end.
-if "%SourcePath:~-1%" == "\" set "SourcePath=%SourcePath:~0,-1%"
-if "%TargetPath:~-1%" == "\" set "TargetPath=%TargetPath:~0,-1%"
-
-rem Determine length of source path by finding out at which
-rem position in source path there is no more character.
-set "PathLength=1"
-:GetPathLength
-if not "!SourcePath:~%PathLength%,1!" == "" (
-    set /A PathLength+=1
-    goto GetPathLength
-)
-
-rem Process each file not having hidden or system attribute set and
-rem decrypt it to the target path relative to source path. The relative
-rem path is determined by removing from full path of current file the
-rem first PathLength characters and the last character which is the
-rem directory separator (backslash).
+set "SOURCPATH=%CD%"
+if not "%~1" == "" set "SOURCPATH=%~1"
+set TIMEOUT=10
 set PATH=%PATH%;c:\tools\resampler
 set "MAX_PROCESSES=2"
 set "PROCESS_COUNTER=0"
 for /F "tokens=2 delims==" %%. in ('wmic.exe cpu get NumberOfCores /format:list') do set /A "MAX_PROCESSES=%%. * 2"
 if not "%3" == "" set "MAX_PROCESSES=%3"
 echo Will run !MAX_PROCESSES! in parallel
-REM seems to scan subbolders too
-for /R "%SourcePath%" %%I in (*.flac) do (
-    set "RelativePath=%%~dpI"
-    set "RelativePath=!RelativePath:~%PathLength%,-1!"
-    md "%TargetPath%!RelativePath!" 2>nul
-
-    set "COMMAND=ReSampler.exe -i "%%I" -o "%TargetPath%!RelativePath!\%%~nxI" -r 44100 -b 16 --dither 3 --minphase --relaxedLPF --showStages --tempDir %TEMP%"
-
-    rem Launch the resampler tool in a separate process
+cd /d "%SOURCPATH%"
+REM no need for "tokens=*" 
+REM set OPTIONS=-r 44100 -b 16 --minphase --relaxedLPF --showStages --tempDir "%TEMP%"
+set OPTIONS=-r 44100 -b 16 --dither 3 --minphase --relaxedLPF --showStages --tempDir "%TEMP%"
+for /R "%SOURCPATH%" %%_ in (*.flac) do (
+    set "INPUTFILE=%%_"
+    set "OUTPUTFILE=%%~dp_%%~n_ - 01%%~x_"
+    rem NODE dealing with quotes
+    set "COMMAND=ReSampler.exe -i "!INPUTFILE!" -o "!OUTPUTFILE!" !OPTIONS!"
     echo Launch the resampler tool in a separate process: !COMMAND!
-    START /B CMD /C "!COMMAND!"
+    start /B CMD /C "!COMMAND!"
 
     set /A PROCESS_COUNTER+=1
-    echo Started !PROCESS_COUNTER! processes
+    echo Running !PROCESS_COUNTER! processes
     if !PROCESS_COUNTER! geq !MAX_PROCESSES! (
         rem Wait for any of the launched processes to finish before launching more
-        CALL :WAIT_FOR_PROCESS
+        call :WAIT_FOR_PROCESS
     )
 )
 
 :WAIT_FOR_PROCESS
 echo Wait for process to finish
 rem Wait for one process to finish
-timeout.exe /T 2 /NOBREAK
-REM PING -n 2 127.0.0.1 >NUL
+Rem echo timeout.exe /T !TIMEOUT! /NOBREAK
+C:\Windows\System32\timeout.exe /T !TIMEOUT! /nobreak 
+
 for /f "tokens=1" %%_ in ('tasklist.exe /FI "IMAGENAME eq ReSampler.exe" /NH') do (
     if "%%_" == "ReSampler.exe" (
         rem There is still at least one instance of the ReSampler.exe tool running
