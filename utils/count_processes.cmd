0e1d717fd8b2912cfbd15277fde79efcae531c02
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-03 03:30:26 +0200

    intermediate revision - added alternative counter of running processes with the specified image name, not relying on 'find.exe' working as a poor man's 'wc'

--- utils/count_processes.cmd
+++ utils/count_processes.cmd
@@ -1,9 +1,28 @@
 @echo off
+
 REM use trick from https://devblogs.microsoft.com/oldnewthing/20110825-00/?p=9803
 SETLOCAL ENABLEDELAYEDEXPANSION
 set IMAGENAME=vivaldi.exe
+
 if not "%~1" == "" set "IMAGENAME=%~1"
+call :COUNT_RUNNING_PROCESSES1 !IMAGENAME!
+call :COUNT_RUNNING_PROCESSES2 !IMAGENAME!
+goto :EOF
+
+:COUNT_RUNNING_PROCESSES1
+set IMAGENAME=%1
 set /A RUNNING_PPOCESS_COUNT=0
-for /f "tokens=1" %%_ in ('tasklist.exe /FI "IMAGENAME eq !IMAGENAME!" /NH ^| C:\Windows\System32\find.exe "!IMAGENAME!" ^|C:\Windows\System32\find.exe/c /v "" ') do set /A RUNNING_PPOCESS_COUNT=%%_
+for /f "tokens=1" %%_ in ('tasklist.exe /FI "IMAGENAME eq !IMAGENAME!" /NH ^| C:\Windows\System32\find.exe /i "!IMAGENAME!" ^|C:\Windows\System32\find.exe/c /v "" ') do set /A RUNNING_PPOCESS_COUNT=%%_
+echo !RUNNING_PPOCESS_COUNT! !IMAGENAME! is running
+goto :EOF
+
+:COUNT_RUNNING_PROCESSES2
+set IMAGENAME=%1
+set /A RUNNING_PPOCESS_COUNT=0
+for /f "tokens=1" %%_ in ('tasklist.exe /FI "IMAGENAME eq !IMAGENAME!" /NH') do (
+  if /I "%%_" == "!IMAGENAME!" (
+      set /A RUNNING_PPOCESS_COUNT=!RUNNING_PPOCESS_COUNT! + 1
+  )
+)
 echo !RUNNING_PPOCESS_COUNT! !IMAGENAME! is running
 goto :EOF
