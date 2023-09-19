3151184125e4287f23742a1289d06f4ae8c9f1eb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-09-19 09:40:19 -0400

    added links to better performing alternatives

--- utils/save_image.sh
+++ utils/save_image.sh
@@ -3,6 +3,11 @@ IMAGE=${1:-lenovo40withminikube}
 DISK=${2:-/dev/sdc}
 # When copying larger ssd to a later useful is to provide count e.g.
 # count=64 bs=500M
+# for reducing the volumne of copy to the Space In-Use onl
+# see https://www.baeldung.com/linux/clone-space-in-use-from-disk
+# see also https://askubuntu.com/questions/260620/resize-dev-loop0-and-increase-space
+# for partclone see https://ru.linux-console.net/?p=11393#gsc.tab=0 (in Russian)
+# original forum: https://qna.habr.com/q/1306968 (in Russian) -  recommends preparing source partition with zero fills on sparse via dd if=/dev/zero of=$DISK/zero; rm $DISK/zero
 STAGING='/media/sergueik/Windows8_OS/Users/Serguei/Desktop/Private/z/'
 if [ ! -d $STAGING ] ;then
   exit 0
