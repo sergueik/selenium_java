c12271ec45545ece6b25488823f8fe22b5dd7c4f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-10-31 15:21:16 -0400

    intermediate revision - put OUTPUTFILE explicitly in the current drectory - observed sometimes not to be the case without explicit

--- utils/uuencode.ps1
+++ utils/uuencode.ps1
@@ -1,5 +1,4 @@
 # origin: https://stackoverflow.com/questions/42592518/encode-decode-exe-into-base64
-#
 param (
   [string] $inputfile,
   [string] $outputfile,
@@ -9,13 +8,16 @@ param (
 if ([bool]$psboundparameters['help'].ispresent) {
   write-host @'
   Usage: uuencode.ps1 [-decode] -inputfile [INPUTFILE] -outputfile [OUTPUTFILE]
+  The [INPUTFILE] and [OUTPUTFILE] are read and created in the current directory
 '@  
   exit 0
 }
+# Powershell found to create files under $env:USERPROFILE unless full path is specified
+$outputfilepath = (( resolve-path -path '.').path + '\' + $outputfile )
 if ([bool]$psboundparameters['decode'].ispresent) {
   $o = get-content -encoding ascii -raw -path $inputfile
-  [IO.File]::WriteAllBytes($outputfile, [Convert]::FromBase64String($o))
+  [IO.File]::WriteAllBytes($outputfilepath, [Convert]::FromBase64String($o))
 } else  {
   $o = [Convert]::ToBase64String([IO.File]::ReadAllBytes($inputfile))
-  out-file -encoding ascii -filepath $outputfile -inputobject $o
+  out-file -encoding ascii -filepath $outputfilepath -inputobject $o
 }
