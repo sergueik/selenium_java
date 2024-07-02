5169c9479437f32bed94cb9ca7a3e5b170ccbbbd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-07-02 02:22:21 +0200

    internediate revision - removed unneded options from "check_remaining_battery.sh"

--- utils/check_remaining_battery.sh
+++ utils/check_remaining_battery.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-OPTS=`getopt -o dhb: --long debug,help,battery: -h 'parse-options' -- "$@"`
+# based on: https://www.cyberciti.biz/faq/linux-laptop-battery-status-temperature/ 
+
+OPTS=`getopt -o hb: --long help,battery: -h 'parse-options' -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 HELP=false
-BATTERY_CHECK=false
-SIZE=5
+BATTERY_SIZE=5
 
 while true; do
   case "$1" in
-    -d | --debug )     DEBUG=true; shift ;;
     -h | --help )      HELP=true; shift ;;
-    -b | --battery )   BATTERY_CHECK=true; SIZE="$2"; shift; shift ;;
+    -b | --battery )   BATTERY_SIZE="$2"; shift; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -18,75 +18,65 @@ done
 if [[ "${HELP}" = "true" ]]; then
 cat <<EOF
 Usage:
-  $0 [-d|--debug] [-h|--help] [-b|--battery] THRESHOLD
+  $0 [-h|--help] [-b|--battery] THRESHOLD
 EOF
 exit 0
 fi
-if [[ "${DEBUG}" = "true" ]]; then
-  echo HELP=$HELP
-  echo SIZE=$SIZE
-  echo BATTERY_CHECK=$BATTERY_CHECK
-fi
+echo BATTERY_SIZE=$BATTERY_SIZE
 
-
-# based on: https://www.cyberciti.biz/faq/linux-laptop-battery-status-temperature/ 
 
 check_remaining_battery () {
 STATE=$(upower -i $(upower -e | grep -i battery | head -1)| grep -E 'state: *' | awk '{print $2}')
 HOURS=$(upower -i $(upower -e | grep -i battery | head -1)| grep 'time to empty' | grep hours | awk '{print $4}')
 MINUTES=$(upower -i $(upower -e | grep -i battery | head -1)| grep 'time to empty' | grep minutes | awk '{print $4}' | sed 's|\.[0-9][0-9]*||')
-if [[ "${DEBUG}" = 'true' ]]; then
-  echo "Battery state: \"${STATE}\""
-  if [ ! -z $MINUTES ] ;  then
-    echo "Battery empty time: \"${MINUTES}\" minutes"
-  fi
-  if [ ! -z $HOURS ] ;  then
-    echo "Battery empty time: \"${HOURS}\" hours"
-  fi
+echo "Battery state: \"${STATE}\""
+if [ ! -z $MINUTES ] ;  then
+  echo "Battery empty time: \"${MINUTES}\" minutes"
+fi
+if [ ! -z $HOURS ] ;  then
+  echo "Battery empty time: \"${HOURS}\" hours"
 fi
 if [[ "${STATE}" -eq 'discharging' ]]
 # when the battery is charging the time to empty is meaningless
 # NOTE: == is better than -eq , since -ne does not work
 then
   if [ ! -z $MINUTES ] ;  then	  
-    if [[ "${DEBUG}" = 'true' ]]; then
-      echo "Comparing Battery empty time: ${MINUTES} to ${SIZE}"
-    fi
-    if [ $MINUTES -lt $SIZE ]
+    echo "Comparing Battery empty time: ${MINUTES} to ${BATTERY_SIZE}"
+    if [ $MINUTES -lt $BATTERY_SIZE ]
     then
       echo 'Too little battery left - will abort'
       # exit 0
     fi
   fi
+else 
+  echo "Battery state is: \"${STATE}\""
 fi
 if [[ "${STATE}" != 'charging' ]]
 # when the battery is charging the time to empty is meaningless
 then
   if [ ! -z $MINUTES ] ;  then
-    if [[ "${DEBUG}" = 'true' ]]; then
-      echo "Comparing Battery empty time: ${MINUTES} to ${SIZE}"
-    fi
-    if [ $MINUTES -lt $SIZE ]
-    then
+    echo "Comparing Battery empty time: ${MINUTES} to ${BATTERY_SIZE}"
+    if [ $MINUTES -lt $BATTERY_SIZE ] ; then
       echo 'Too little battery left - will abort'
      #  exit 0
     fi
   fi
+else 
+  echo "Battery state is: \"${STATE}\""
 fi
 # TODO: -ne does not work
 if [[ "${STATE}" -ne 'charging' ]]
 # when the battery is charging the time to empty is meaningless
 then
   if [ ! -z $MINUTES ] ;  then
-    if [[ "${DEBUG}" = 'true' ]]; then
-      echo "Comparing Battery empty time: ${MINUTES} to ${SIZE}"
-    fi
-    if [ $MINUTES -lt $SIZE ]
-    then
+    echo "Comparing Battery empty time: ${MINUTES} to ${BATTERY_SIZE}"
+    if [ $MINUTES -lt $BATTERY_SIZE ] ; then
       echo 'Too little battery left - will abort'
      #  exit 0
     fi
   fi
+else 
+  echo "Battery state is: \"${STATE}\""
 fi
 }
 
