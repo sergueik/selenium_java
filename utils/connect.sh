79548a18e499e218b22f7a8c287159b9759dae9b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-08-07 04:42:13 +0200

    intermediate revision - make the $REMOTE_DIR directory if not exists

--- utils/connect.sh
+++ utils/connect.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # origin: https://serverfault.com/questions/167416/how-can-i-automatically-change-directory-on-ssh-login 
 # see also https://serverfault.com/questions/499565/change-default-directory-when-i-ssh-to-server
+# NOTE: `whoami` in Windows in git bash can be different from the user name on Linux box
+DEFAULT_HOST='192.168.0.64'
+DEFAULT_USER='sergueik'
 # about customizing ~/.ssh/authorized_keys and ~/.ssh/config
 env | grep -q 'OS='
 if [ $? = 0 ] ; then
-  PROJECTS_DIR='/c/developer/sergueik'
+  PROJECTS_DIR="/c/developer/${DEFAULT_USER}"
 else
   PROJECTS_DIR="${HOME}/src"
 fi
 TARGET_PROJECTS_DIR='src'
 
 PROJECT_DIR=$(pwd | sed "s|^${PROJECTS_DIR}/||") # current directory with swapped base dir
-# NOTE: `whoami` in Windows in git bash can be different from
-REMOTE_HOST=${1:-192.168.0.64}
-REMOTE_USER=${2:-sergueik}
-
-ssh -t $REMOTE_USER@${REMOTE_HOST} "cd ~/$TARGET_PROJECTS_DIR/$PROJECT_DIR; exec \$SHELL --login"
+REMOTE_HOST=${1:-$DEFAULT_HOST}
+REMOTE_USER=${2:-$DEFAULT_USER}
+REMOTE_DIR="${TARGET_PROJECTS_DIR}/${PROJECT_DIR}"
+ssh -t $REMOTE_USER@${REMOTE_HOST} "test -d ~/$REMOTE_DIR || mkdir ~/$REMOTE_DIR; cd ~/$REMOTE_DIR; exec \$SHELL --login"
